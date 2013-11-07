7fc7e3d79b2aed3f3dae7ad7acd9a42e8f20fdfa
Paulmichael Blasucci <pblasucci@gmail.com>
2013-11-06 22:01:49 -0500

    tweaks to narrative docs

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -54,16 +54,16 @@ Contributing and copyright
 
 The project is hosted on [GitHub][gh] where you can [report issues][issues], fork 
 the project and submit pull requests. If you're adding new public API, please also 
-consider adding [samples][samples] that can be turned into a documentation. You might
-also want to read [library design notes](design.html) to understand how it works.
+consider adding [samples][content] that can be turned into a documentation. You might
+also want to read [library design notes][readme] to understand how it works.
 
 The library is available under Public Domain license, which allows modification and 
 redistribution for both commercial and non-commercial purposes. For more information see the 
 [License file][license] in the GitHub repository. 
 
-  [content]: https://github.com/fsharp/FSharp.ProjectScaffold/tree/master/docs/content
-  [gh]: https://github.com/fsharp/FSharp.ProjectScaffold
-  [issues]: https://github.com/fsharp/FSharp.ProjectScaffold/issues
-  [readme]: https://github.com/fsharp/FSharp.ProjectScaffold/blob/master/README.md
-  [license]: https://github.com/fsharp/FSharp.ProjectScaffold/blob/master/LICENSE.md
+  [content]: https://github.com/pblasucci/FSharp.ProjectScaffold/tree/master/docs/content
+  [gh]: https://github.com/pblasucci/FSharp.ProjectScaffold
+  [issues]: https://github.com/pblasucci/FSharp.ProjectScaffold/issues
+  [readme]: https://github.com/pblasucci/FSharp.ProjectScaffold/blob/master/README.md
+  [license]: https://github.com/pblasucci/FSharp.ProjectScaffold/blob/master/LICENSE.md
 *)
