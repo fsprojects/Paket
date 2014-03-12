4bc9b93ee5e9e5340a3e2e683a6ae24ea6ab6c5e
Sergey Tihon <sergey.tihon@gmail.com>
2014-03-12 18:22:27 +0300

    generate.fsx was updated to use FSharp.Formatting 2.4.1 and FSharp.Compiler.Service 0.0.33

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -22,9 +22,9 @@ let info =
 // For typical project, no changes are needed below
 // --------------------------------------------------------------------------------------
 
-#I "../../packages/FSharp.Formatting.2.3.5-beta/lib/net40"
+#I "../../packages/FSharp.Formatting.2.4.1/lib/net40"
 #I "../../packages/RazorEngine.3.3.0/lib/net40"
-#I "../../packages/FSharp.Compiler.Service.0.0.11-alpha/lib/net40"
+#I "../../packages/FSharp.Compiler.Service.0.0.33/lib/net40"
 #r "../../packages/Microsoft.AspNet.Razor.2.0.30506.0/lib/net40/System.Web.Razor.dll"
 #r "../../packages/FAKE/tools/FakeLib.dll"
 #r "RazorEngine.dll"
@@ -51,7 +51,7 @@ let content    = __SOURCE_DIRECTORY__ @@ "../content"
 let output     = __SOURCE_DIRECTORY__ @@ "../output"
 let files      = __SOURCE_DIRECTORY__ @@ "../files"
 let templates  = __SOURCE_DIRECTORY__ @@ "templates"
-let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting.2.3.5-beta/"
+let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting.2.4.1/"
 let docTemplate = formatting @@ "templates/docpage.cshtml"
 
 // Where to look for *.csproj templates (in this order)
