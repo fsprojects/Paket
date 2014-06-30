7a6f1bd60e1e544b037086d5af59c3731935abe1
Peter Strøiman <peter@stroiman.com>
2014-06-30 21:36:09 +0200

    Removed temp generated fsharpi file
    First, it seems that this is not necessary, probably because FAKE 3.0 uses the
    F# compiler services instead of the fsharpi executable.
    
    Second, because the FAKE command was not the last command in the script, any
    failure from the build script would not cause the 'build.sh' shell script to
    return an error message, and e.g. a build server would see the build outcome as
    a success.

--- build.sh
+++ build.sh
@@ -3,12 +3,4 @@ if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
   mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
 fi
 #workaround assembly resolution issues in build.fsx
-export FSHARPI=`which fsharpi`
-cat - > fsharpi <<"EOF"
-#!/bin/bash
-libdir=$PWD/packages/FAKE/tools/
-$FSHARPI --lib:$libdir $@
-EOF
-chmod +x fsharpi
 mono packages/FAKE/tools/FAKE.exe build.fsx $@
-rm fsharpi
