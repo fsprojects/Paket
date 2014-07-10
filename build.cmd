bc181abf74bddff054249556691a61a104639605
Tony Williams <tony.williams@thap.co.uk>
2014-07-10 20:44:20 +0100

    Added a source link target

--- build.cmd
+++ build.cmd
@@ -1,5 +1,6 @@
 @echo off
 cls
 .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
+.nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 packages\FAKE\tools\FAKE.exe build.fsx %*
 pause
