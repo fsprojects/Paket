c166438c8050d5df817745a609bd3e907a34411b
Don Syme <dsyme@users.noreply.github.com>
2014-07-15 23:25:41 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -3,4 +3,3 @@ cls
 .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
 .nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 packages\FAKE\tools\FAKE.exe build.fsx %*
-pause
