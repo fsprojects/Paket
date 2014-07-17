ae6cd3ebbb855e758faee5c07d68edb56582d8f0
Christopher Atkins <christopher316@gmail.com>
2014-07-16 22:55:02 -0400

    updated build.cmd to fetch nuget.core

--- build.cmd
+++ build.cmd
@@ -1,7 +1,14 @@
 @echo off
 cls
+
+IF NOT EXIST packages\Nuget.Core\lib\net40-Client\NuGet.Core.dll (
+  .nuget\nuget.exe install Nuget.Core -OutputDirectory packages -ExcludeVersion
+)
+
+
 .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
 .nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
+
 IF NOT EXIST build.fsx (
   packages\FAKE\tools\FAKE.exe init.fsx
 )
