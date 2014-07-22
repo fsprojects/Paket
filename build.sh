4fbd760c6991c66ee522989919459c9733569d8d
Tomas Petricek <tomas@tomasp.net>
2014-07-22 14:34:56 -0500

    Move NUnit dependency and invoke nuget always in cmd script

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ ! -e packages/FAKE/tools/FAKE.exe ]; then 
-  mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion -version "3.2.1"
-fi
-
-if [ ! -e packages/SourceLink.Fake/Tools/Fake.fsx ]; then
-  mono .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
-fi
+mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+mono .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 
 if [ ! -e build.fsx ]; then 
   mono packages/FAKE/tools/FAKE.exe init.fsx
