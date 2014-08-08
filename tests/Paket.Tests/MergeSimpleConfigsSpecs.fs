09c2bb3a54e1c2b498fe231216277b0779817364
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-08-08 16:42:31 +0200

    Put the tests where they belong

--- tests/Paket.Tests/MergeSimpleConfigsSpecs.fs
+++ tests/Paket.Tests/MergeSimpleConfigsSpecs.fs
@@ -1,4 +1,4 @@
-module Paket.LoadConfigSpecs
+module Paket.MergeSimpleConfigsSpecs
 
 open Paket
 open NUnit.Framework
@@ -12,21 +12,22 @@ nuget "Castle.Windsor-log4net" "~> 3.2"
 nuget "Rx-Main" "~> 2.0"
 """
 
-[<Test>]
-let ``should read simple config1`` () =
-    let cfg = FromCode config1
-    cfg.["Rx-Main"].Version |> shouldEqual "~> 2.0"    
-    cfg.["Castle.Windsor-log4net"].Version |> shouldEqual "~> 3.2"
-
 let config2 = """
 source "http://nuget.org/api/v2"
 
 nuget "FAKE" "~> 3.0"
 nuget "Rx-Main" "~> 2.2"
+nuget "Castle.Windsor-log4net" "~> 1.2"
 """
 
 [<Test>]
-let ``should read simple config2`` () =
-    let cfg = FromCode config2
+let ``should merge simple configs`` () =
+    let cfg1 = FromCode config1
+    let cfg2 = FromCode config2
+
+    let cfg = merge cfg1 cfg2
+
     cfg.["Rx-Main"].Version |> shouldEqual "~> 2.2"    
+    cfg.["Castle.Windsor-log4net"].Version |> shouldEqual "~> 3.2"    
     cfg.["FAKE"].Version |> shouldEqual "~> 3.0"
+
