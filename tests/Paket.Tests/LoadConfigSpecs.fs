fbfc7381ea5717726be070110269442ec0344e81
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-08-08 15:42:20 +0200

    Add another loadconfig test

--- tests/Paket.Tests/LoadConfigSpecs.fs
+++ tests/Paket.Tests/LoadConfigSpecs.fs
@@ -8,13 +8,25 @@ open FsUnit
 let config1 = """
 source "http://nuget.org/api/v2"
 
-printfn "hello world from config"
-
 nuget "Castle.Windsor-log4net" "~> 3.2"
 nuget "Rx-Main" "~> 2.0"
 """
 
 [<Test>]
-let ``should read easy config`` () =
+let ``should read easy config1`` () =
     let cfg = FromCode config1
-    cfg.["Rx-Main"].Version |> shouldEqual "~> 2.0"
+    cfg.["Rx-Main"].Version |> shouldEqual "~> 2.0"    
+    cfg.["Castle.Windsor-log4net"].Version |> shouldEqual "~> 3.2"
+
+let config2 = """
+source "http://nuget.org/api/v2"
+
+nuget "FAKE" "~> 3.0"
+nuget "Rx-Main" "~> 2.2"
+"""
+
+[<Test>]
+let ``should read easy config2`` () =
+    let cfg = FromCode config2
+    cfg.["Rx-Main"].Version |> shouldEqual "~> 2.2"    
+    cfg.["FAKE"].Version |> shouldEqual "~> 3.0"
