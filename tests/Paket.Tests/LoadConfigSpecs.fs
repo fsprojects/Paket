8668651a39b5e64cb9ba67a2c9b724d6d9f554d1
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-08-09 19:11:23 +0200

    Tests for SpecificVersion

--- tests/Paket.Tests/LoadConfigSpecs.fs
+++ tests/Paket.Tests/LoadConfigSpecs.fs
@@ -10,6 +10,8 @@ source "http://nuget.org/api/v2"
 
 nuget "Castle.Windsor-log4net" "~> 3.2"
 nuget "Rx-Main" "~> 2.0"
+nuget "FAKE" "1.1"
+nuget "SignalR" "3.3.2"
 """
 
 [<Test>]
@@ -18,6 +20,10 @@ let ``should read simple config1`` () =
     cfg.["Rx-Main"].Version |> shouldEqual (VersionRange { Min = "2.0"; Max = "3.0" })
     cfg.["Rx-Main"].Source |> shouldEqual "src1"    
     cfg.["Castle.Windsor-log4net"].Version |> shouldEqual (VersionRange { Min = "3.2"; Max = "4.0" })
+
+    cfg.["FAKE"].Version |> shouldEqual (SpecificVersion "1.1")
+    cfg.["SignalR"].Version |> shouldEqual (SpecificVersion "3.3.2")
+
     
 let config2 = """
 source "http://nuget.org/api/v2"
