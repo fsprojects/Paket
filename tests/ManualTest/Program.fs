e0e16cf8876c6b8250e490b9002daa95987be3d4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-08-27 16:24:31 +0200

    Use TestHelper in ManualTest

--- tests/ManualTest/Program.fs
+++ tests/ManualTest/Program.fs
@@ -1,4 +1,5 @@
 open Paket
+open TestHelpers
 
 let completeConfig = Config.ReadFromFile "myConfig.fsx"
 
@@ -6,21 +7,6 @@ let graph = [
     "Castle.Windsor-log4net","3.2",[]
     "Rx-Main","2.0",[]
 ]
-
-// TODO: Remove me
-let DictionaryDiscovery(graph : seq<string * string * (string * VersionRange) list>) = 
-    { new IDiscovery with
-          member __.GetDirectDependencies(sourceType, source, package, version) = 
-              graph
-              |> Seq.filter (fun (p,v,_) -> p = package && v = version) 
-              |> Seq.map (fun (_,_,d) -> d) 
-              |> Seq.head 
-              |> List.map (fun (p,v) -> { Name = p; VersionRange = v; SourceType = sourceType; Source = source})
-          member __.GetVersions package = 
-              graph              
-              |> Seq.filter (fun (p,_,_) -> p = package)
-              |> Seq.map (fun (_,v,_) -> v) }
-
 
 let discovery = DictionaryDiscovery graph
 
