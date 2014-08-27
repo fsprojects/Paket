7c6fdaa05e13088597a627e17779753d9b9c7cab
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-08-27 15:51:41 +0200

    adding build.cmd to solution

--- build.cmd
+++ build.cmd
@@ -4,7 +4,4 @@ cls
 .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
 .nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 
-IF NOT EXIST build.fsx (
-  packages\FAKE\tools\FAKE.exe init.fsx
-)
 packages\FAKE\tools\FAKE.exe build.fsx %*
--- build.sh
+++ build.sh
@@ -3,8 +3,4 @@
 mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
 mono .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 
-if [ ! -e build.fsx ]; then 
-  mono packages/FAKE/tools/FAKE.exe init.fsx
-fi
-
 mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
