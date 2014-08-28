f6336f9ec7c0bdc877ff159350ef0ebffda8e3aa
Christoph Ruegg <git@cdrnet.ch>
2014-08-28 20:58:22 +0200

    More compact formatting of build.sh w/o tabs

--- build.sh
+++ build.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 if test "$OS" = "Windows_NT"
 then
-
-	# use .Net
-	.nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
-	.nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
-
-	[ ! -e build.fsx ] && packages/FAKE/tools/FAKE.exe init.fsx
-
-	packages/FAKE/tools/FAKE.exe build.fsx $@ 
-
+  # use .Net
+  .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
+  [ ! -e build.fsx ] && packages/FAKE/tools/FAKE.exe init.fsx
+  packages/FAKE/tools/FAKE.exe build.fsx $@
 else
-
-	# use mono
-	mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
-	mono .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
-
-	[ ! -e build.fsx ] && mono packages/FAKE/tools/FAKE.exe init.fsx
-
-	mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
-
+  # use mono
+  mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  mono .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
+  [ ! -e build.fsx ] && mono packages/FAKE/tools/FAKE.exe init.fsx
+  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
