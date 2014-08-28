b14e3255fdd7aa791c10c7450209368d00ed465e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-08-28 11:15:49 +0200

    Use real nuget config in manual test

--- tests/ManualTest/Program.fs
+++ tests/ManualTest/Program.fs
@@ -2,11 +2,6 @@ open Paket
 
 let completeConfig = Config.ReadFromFile "myConfig.fsx"
 
-let graph = [
-    "Castle.Windsor-log4net","3.2",[]
-    "Rx-Main","2.0",[]
-]
-
 for x in completeConfig.Resolve(Nuget.NugetDiscovery()) do
     printfn "%A" x.Value
 System.Console.ReadKey() |> ignore
