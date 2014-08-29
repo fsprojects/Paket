b24d61f36a8e718ed3de68a8a7d9e9599856505b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-08-29 14:54:31 +0200

    Adding introduction to the docs

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -1,50 +1,20 @@
-(*** hide ***)
-// This block of code is omitted in the generated HTML documentation. Use 
-// it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
 
 (**
-Paket
-=====
+What is Paket?
+==============
 
-Documentation
+Paket is a package manager for .NET and mono projects. It tracks your NuGet packages and installs the exact package versions that are needed. 
 
-<div class="row">
-  <div class="span1"></div>
-  <div class="span6">
-    <div class="well well-small" id="nuget">
-      Paket can be <a href="https://nuget.org/packages/Paket">installed from NuGet</a>:
-      <pre>PM> Install-Package Paket</pre>
-    </div>
-  </div>
-  <div class="span1"></div>
-</div>
-
-Example
--------
+Getting Started
+---------------
 
 This example demonstrates using a function defined in this sample library.
 
 *)
-#r "Paket.dll"
-open Paket
 
 
 (**
-Some more info
-
-Samples & documentation
------------------------
-
-The library comes with comprehensible documentation. 
-It can include a tutorials automatically generated from `*.fsx` files in [the content folder][content]. 
-The API reference is automatically generated from Markdown comments in the library implementation.
-
- * [Tutorial](tutorial.html) contains a further explanation of this sample library.
-
- * [API Reference](reference/index.html) contains automatically generated documentation for all types, modules
-   and functions in the library. This includes additional brief samples on using most of the
-   functions.
  
 Contributing and copyright
 --------------------------
