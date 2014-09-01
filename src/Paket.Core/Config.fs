5a85028f04b2fb8c8464eae093b96a29725d2202
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-01 15:34:33 +0200

    More docs

--- src/Paket.Core/Config.fs
+++ src/Paket.Core/Config.fs
@@ -6,7 +6,8 @@ open System.Collections.Generic
 open Microsoft.FSharp.Compiler.Interactive.Shell
 open Paket
 
-module ConfigHelpers =
+// [omit]
+module ConfigHelpers = 
     let initialCode = """
 open System.Collections.Generic
 let __nuget = new Dictionary<string,string*string*string>()
@@ -16,7 +17,7 @@ let source url = __nugetSource <- url
 
 let nuget package version = __nuget.Add(package,("nuget",__nugetSource,version))
 """
-
+    
     let parseVersionRange (text : string) : VersionRange = 
         // TODO: Make this pretty
         if text.StartsWith "~> " then 
@@ -32,12 +33,12 @@ let nuget package version = __nuget.Add(package,("nuget",__nugetSource,version))
         else if text.StartsWith "= " then VersionRange.Exactly(text.Replace("= ", ""))
         else VersionRange.AtLeast(text)
     
-    let formatVersionRange (version: VersionRange) =
+    let formatVersionRange (version : VersionRange) = 
         match version with
         | Minimum v -> ">= " + v.ToString()
         | Specific v -> v.ToString()
-        | Range(v1,v2) -> ">= " + v1.ToString() + ", < " + v2.ToString()
-
+        | Range(v1, v2) -> ">= " + v1.ToString() + ", < " + v2.ToString()
+    
     let executeInScript (executeInScript : FsiEvaluationSession -> unit) = 
         let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
         let commonOptions = [| "fsi.exe"; "--noninteractive" |]
@@ -53,21 +54,27 @@ let nuget package version = __nuget.Add(package,("nuget",__nugetSource,version))
                 executeInScript session
                 match session.EvalExpression "__nuget" with
                 | Some value -> 
-                    let dependencies =
-                        value.ReflectionValue :?> Dictionary<string,string*string*string>
+                    let dependencies = 
+                        value.ReflectionValue :?> Dictionary<string, string * string * string> 
                         |> Seq.map (fun x -> 
-                            let sourceType,source,version = x.Value
-                            { Name = x.Key; VersionRange = parseVersionRange version; SourceType = sourceType; Source = source })
+                               let sourceType, source, version = x.Value
+                               { Name = x.Key
+                                 VersionRange = parseVersionRange version
+                                 SourceType = sourceType
+                                 Source = source })
                     dependencies
                 | _ -> failwithf "Error: %s" <| sbErr.ToString()
             with _ -> failwithf "Error: %s" <| sbErr.ToString()
         with exn -> failwithf "FsiEvaluationSession could not be created. %s" <| sbErr.ToString()
 
-type Config(packages : Package seq) =
+/// Allows to parse and analyze packages.fsx files.
+type Config(packages : Package seq) = 
     let packages = packages |> Seq.toList
-    let dependencyMap = Map.ofSeq (packages |> Seq.map (fun p -> p.Name,p.VersionRange))
+    let dependencyMap = Map.ofSeq (packages |> Seq.map (fun p -> p.Name, p.VersionRange))
     member __.DirectDependencies = dependencyMap
     member __.Packages = packages
     member __.Resolve(discovery : IDiscovery) = Resolver.Resolve(discovery, packages)
-    static member FromCode code : Config = Config(ConfigHelpers.executeInScript (fun session -> session.EvalExpression code |> ignore))
-    static member ReadFromFile fileName : Config = Config(ConfigHelpers.executeInScript (fun session -> session.EvalScript fileName))
+    static member FromCode code : Config = 
+        Config(ConfigHelpers.executeInScript (fun session -> session.EvalExpression code |> ignore))
+    static member ReadFromFile fileName : Config = 
+        Config(ConfigHelpers.executeInScript (fun session -> session.EvalScript fileName))
