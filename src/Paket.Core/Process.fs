a9e6a47820f67068d6725b6a3a8a67aa28e8fc76
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-01 15:47:46 +0200

    Docs for Process

--- src/Paket.Core/Process.fs
+++ src/Paket.Core/Process.fs
@@ -1,8 +1,10 @@
-﻿module Paket.Process
+﻿/// Contains methods for the install and update process.
+module Paket.Process
 
 open System.IO
 
-let ExtractPackages force (packages : Package seq) = 
+/// Downloads and extracts all package.
+let ExtractPackages(force, packages : Package seq) = 
     packages |> Seq.map (fun package -> 
                     let version = 
                         match package.VersionRange with
@@ -10,22 +12,18 @@ let ExtractPackages force (packages : Package seq) =
                         | v -> failwithf "Version error in lockfile for %s %A" package.Name v
                     match package.SourceType with
                     | "nuget" -> 
-                        async {
-                            let! packageFile = Nuget.DownloadPackage(package.Source, package.Name, version.ToString(), force)
-                            return! Nuget.ExtractPackage(packageFile,package.Name, version.ToString(), force) }
+                        async { let! packageFile = Nuget.DownloadPackage
+                                                       (package.Source, package.Name, version.ToString(), force)
+                                return! Nuget.ExtractPackage(packageFile, package.Name, version.ToString(), force) }
                     | _ -> failwithf "Can't download from source type %s" package.SourceType)
 
-let Install regenerate force packageFile =
-    let lockfile =
+/// Installs the given packageFile.
+let Install(regenerate, force, packageFile) = 
+    let lockfile = 
         let fi = FileInfo(packageFile)
-        FileInfo(fi.Directory.FullName + Path.DirectorySeparatorChar.ToString() + fi.Name.Replace(fi.Extension,".lock"))
-
-    if regenerate || (not lockfile.Exists) then
-        LockFile.Update(packageFile,lockfile.FullName)
-
-    File.ReadAllLines lockfile.FullName
-    |> LockFile.Parse
-    |> ExtractPackages force
+        FileInfo(Path.Combine(fi.Directory.FullName + fi.Name.Replace(fi.Extension, ".lock")))
+    if regenerate || (not lockfile.Exists) then LockFile.Update(packageFile, lockfile.FullName)
+    ExtractPackages(force, File.ReadAllLines lockfile.FullName |> LockFile.Parse)
     |> Async.Parallel
     |> Async.RunSynchronously
     |> ignore
