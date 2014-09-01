f5791217e0355b4d827adcd4d4a9bc12814ba468
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-01 19:03:02 +0200

    LoadReferencedPackages

--- src/Paket/ProjectFile.fs
+++ src/Paket/ProjectFile.fs
@@ -1,6 +1,14 @@
-﻿module Paket.ProjectFile
+﻿/// Contains methods to read and manipulate project files.
+module Paket.ProjectFile
 
 open System.Xml
+open System.IO
+
+/// Reads the packages file which sits next to the projectFile.
+let LoadReferencedPackages (projectFileName:string) = 
+    let fi = FileInfo(projectFileName)
+    let packagesDef = FileInfo(fi.Directory.FullName + Path.DirectorySeparatorChar.ToString() + "packages")
+    if packagesDef.Exists then File.ReadAllLines(packagesDef.FullName) else [||]
 
 let getProject (text:string) =
     let xmlDocument = new XmlDocument()
