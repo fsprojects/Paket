363f7a3ca0acc43130c2c465c2ff1efadc4dcda5
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-01 17:46:52 +0200

    Starting to implement ProjectFile

---
+++ src/Paket.Core/ProjectFile.fs
@@ -0,0 +1,8 @@
+﻿module Paket.ProjectFile
+
+open System.Xml
+
+let getProject (text:string) =
+    let xmlDocument = new XmlDocument()
+    xmlDocument.LoadXml text
+    xmlDocument
---
+++ tests/Paket.Tests/ProjectFileSpecs.fs
@@ -0,0 +1,5 @@
+﻿module Paket.ProjectFileSpecs
+
+open Paket
+open NUnit.Framework
+open FsUnit
