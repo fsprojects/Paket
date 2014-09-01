9231cd336319a5244344fc61b925b1e00286a213
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-01 19:59:43 +0200

    Can detect reference nodes in projectfile

--- src/Paket/ProjectFile.fs
+++ src/Paket/ProjectFile.fs
@@ -10,7 +10,18 @@ let LoadReferencedPackages (projectFileName:string) =
     let packagesDef = FileInfo(fi.Directory.FullName + Path.DirectorySeparatorChar.ToString() + "packages")
     if packagesDef.Exists then File.ReadAllLines(packagesDef.FullName) else [||]
 
-let getProject (text:string) =
-    let xmlDocument = new XmlDocument()
-    xmlDocument.LoadXml text
-    xmlDocument
+let getProject (fileName:string) =
+    let doc = new XmlDocument()
+    doc.Load fileName
+    let manager = new XmlNamespaceManager(doc.NameTable)
+    manager.AddNamespace("ns", "http://schemas.microsoft.com/developer/msbuild/2003")
+    doc,manager
+
+type ReferenceNode = {
+    DLLName : string
+    Node: XmlNode
+    }
+
+let getReferences (doc : XmlDocument, manager) = 
+    [ for node in doc.SelectNodes("//ns:Project/ns:ItemGroup/ns:Reference", manager) do
+          yield { DLLName = node.Attributes.["Include"].InnerText.Split(',').[0]; Node = node} ]
--- tests/Paket.Tests/ProjectFileSpecs.fs
+++ tests/Paket.Tests/ProjectFileSpecs.fs
@@ -3,3 +3,15 @@
 open Paket
 open NUnit.Framework
 open FsUnit
+
+[<Test>]
+let ``should detect reference nodes``() =
+    let references =
+        ProjectFile.getProject "./TestData/Project1.fsproj"
+        |> ProjectFile.getReferences
+
+    references.Length |> shouldEqual 4
+    references.[0].DLLName |> shouldEqual "mscorlib"
+    references.[1].DLLName |> shouldEqual "FSharp.Core"
+    references.[2].DLLName |> shouldEqual "nunit.framework"
+    references.[3].DLLName |> shouldEqual "System"
