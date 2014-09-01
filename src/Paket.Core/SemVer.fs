a69a70f63fe87206ee981eb3e10574eb99127df4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-01 15:28:00 +0200

    Adding some SemVer docs

--- src/Paket.Core/SemVer.fs
+++ src/Paket.Core/SemVer.fs
@@ -4,84 +4,99 @@ namespace Paket
 open System
 open System.Text.RegularExpressions
 
+/// Information about PreRelease packages.
 [<CustomEquality; CustomComparison>]
 type PreRelease = 
-    { Origin: string
-      Name: string
-      Number: int option }
+    { Origin : string
+      Name : string
+      Number : int option }
+    
     static member TryParse str = 
         let m = Regex("^(?<name>[a-zA-Z]+)(?<number>\d*)$").Match(str)
         match m.Success, m.Groups.["name"].Value, m.Groups.["number"].Value with
-        | true, name, "" -> Some { Origin = str; Name = name; Number = None }
-        | true, name, number -> Some { Origin = str; Name = name; Number = Some (int number) }
+        | true, name, "" -> 
+            Some { Origin = str
+                   Name = name
+                   Number = None }
+        | true, name, number -> 
+            Some { Origin = str
+                   Name = name
+                   Number = Some(int number) }
         | _ -> None
-    override x.Equals(yobj) =
+    
+    override x.Equals(yobj) = 
         match yobj with
         | :? PreRelease as y -> x.Origin = y.Origin
         | _ -> false
+    
     override x.GetHashCode() = hash x.Origin
     interface System.IComparable with
-        member x.CompareTo yobj =
+        member x.CompareTo yobj = 
             match yobj with
-            | :? PreRelease as y ->
-                if x.Name <> y.Name then compare x.Name y.Name else
-                compare x.Number y.Number
+            | :? PreRelease as y -> 
+                if x.Name <> y.Name then compare x.Name y.Name
+                else compare x.Number y.Number
             | _ -> invalidArg "yobj" "cannot compare values of different types"
 
 /// Contains the version information.
 [<CustomEquality; CustomComparison>]
-type SemVerInfo =
+type SemVerInfo = 
     { /// MAJOR version when you make incompatible API changes.
-      Major: int
+      Major : int
       /// MINOR version when you add functionality in a backwards-compatible manner.
-      Minor: int
+      Minor : int
       /// PATCH version when you make backwards-compatible bug fixes.
-      Patch: int
+      Patch : int
       /// The optional PreRelease version
       PreRelease : PreRelease option
       /// The optional build no.
-      Build: string;
+      Build : string
       // The original version text
-      Original: string option }
-    override x.ToString() =
+      Original : string option }
+    
+    override x.ToString() = 
         match x.Original with
         | Some version -> version
-        | None ->
-            sprintf "%d.%d.%d" x.Major x.Minor x.Patch +
-             (match x.PreRelease, String.IsNullOrEmpty x.Build |> not with
-              | Some preRelease, _ -> "-" + preRelease.Name 
-              | None, true -> "-"
-              | _ -> "") +
-             (if String.IsNullOrEmpty x.Build |> not then "." + x.Build else "")
-
-    override x.Equals(yobj) =
+        | None -> 
+            let build = 
+                if String.IsNullOrEmpty x.Build |> not then "." + x.Build
+                else ""
+            
+            let pre = 
+                match x.PreRelease, String.IsNullOrEmpty x.Build |> not with
+                | Some preRelease, _ -> "-" + preRelease.Name
+                | None, true -> "-"
+                | _ -> ""
+            
+            sprintf "%d.%d.%d" x.Major x.Minor x.Patch + pre + build
+    
+    override x.Equals(yobj) = 
         match yobj with
         | :? SemVerInfo as y -> 
-            (x.Minor,x.Minor,x.Patch,x.PreRelease,x.Build) = 
-              (y.Minor,y.Minor,y.Patch,y.PreRelease,y.Build)
+            (x.Minor, x.Minor, x.Patch, x.PreRelease, x.Build) = (y.Minor, y.Minor, y.Patch, y.PreRelease, y.Build)
         | _ -> false
- 
-    override x.GetHashCode() = hash (x.Minor,x.Minor,x.Patch,x.PreRelease,x.Build)
+    
+    override x.GetHashCode() = hash (x.Minor, x.Minor, x.Patch, x.PreRelease, x.Build)
     interface System.IComparable with
-        member x.CompareTo yobj =
+        member x.CompareTo yobj = 
             match yobj with
-            | :? SemVerInfo as y ->
-                if x.Major <> y.Major then compare x.Major y.Major else
-                if x.Minor <> y.Minor then compare x.Minor y.Minor else
-                if x.Patch <> y.Patch then compare x.Patch y.Patch else
-                if x.PreRelease = y.PreRelease && x.Build = y.Build  then 0 else
-                if x.PreRelease.IsNone && x.Build = "" then 1 else
-                if y.PreRelease.IsNone && y.Build = "" then -1 else
-                if x.PreRelease <> y.PreRelease then compare x.PreRelease y.PreRelease else
-                if x.Build <> y.Build then 
+            | :? SemVerInfo as y -> 
+                if x.Major <> y.Major then compare x.Major y.Major
+                else if x.Minor <> y.Minor then compare x.Minor y.Minor
+                else if x.Patch <> y.Patch then compare x.Patch y.Patch
+                else if x.PreRelease = y.PreRelease && x.Build = y.Build then 0
+                else if x.PreRelease.IsNone && x.Build = "" then 1
+                else if y.PreRelease.IsNone && y.Build = "" then -1
+                else if x.PreRelease <> y.PreRelease then compare x.PreRelease y.PreRelease
+                else if x.Build <> y.Build then 
                     match Int32.TryParse x.Build, Int32.TryParse y.Build with
-                    | (true,b1),(true,b2) -> compare b1 b2
-                    | _ -> compare x.Build y.Build 
-                else
-                    0
+                    | (true, b1), (true, b2) -> compare b1 b2
+                    | _ -> compare x.Build y.Build
+                else 0
             | _ -> invalidArg "yobj" "cannot compare values of different types"
 
-module SemVer =
+/// Parser for [SemVer docs](http://semver.org/).
+module SemVer = 
     /// Parses the given version string into a SemVerInfo which can be printed using ToString() or compared
     /// according to the rules described in the [SemVer docs](http://semver.org/).
     /// ## Sample
@@ -91,19 +106,26 @@ module SemVer =
     ///     parse "1.2.3-alpha2"   > parse "1.2.3-alpha"    // true
     ///     parse "1.2.3-alpha002" > parse "1.2.3-alpha1"   // true
     ///     parse "1.5.0-beta.2"   > parse "1.5.0-rc.1"     // false
-    let parse (version:string) =
+    let parse (version : string) = 
         let splitted = version.Split '.'
         let l = splitted.Length
-        let patch,preRelease =
-            if l <= 2 then 0,"" else
-            let splitted' = splitted.[2].Split '-'
-            Int32.Parse splitted'.[0], if splitted'.Length > 1 then splitted'.[1] else ""
-
-
-        { Major = if l > 0 then Int32.Parse splitted.[0] else 0
-          Minor = if l > 1 then Int32.Parse splitted.[1] else 0
+        
+        let patch, preRelease = 
+            if l <= 2 then 0, ""
+            else 
+                let splitted' = splitted.[2].Split '-'
+                Int32.Parse splitted'.[0], 
+                if splitted'.Length > 1 then splitted'.[1]
+                else ""
+        { Major = 
+              if l > 0 then Int32.Parse splitted.[0]
+              else 0
+          Minor = 
+              if l > 1 then Int32.Parse splitted.[1]
+              else 0
           Patch = patch
           PreRelease = PreRelease.TryParse preRelease
-          Build = if l > 3 then splitted.[3] else ""
-          Original = Some version
-        }
+          Build = 
+              if l > 3 then splitted.[3]
+              else ""
+          Original = Some version }
