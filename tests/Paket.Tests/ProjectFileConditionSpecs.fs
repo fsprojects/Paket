4b3db26155f7241db379098532781373a545ce5f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-04 20:01:54 +0200

    Temporary hack to detect net40-client - closes #19

--- tests/Paket.Tests/ProjectFileConditionSpecs.fs
+++ tests/Paket.Tests/ProjectFileConditionSpecs.fs
@@ -13,4 +13,10 @@ let ``should detect framework version from path``() =
     FramworkCondition.DetectFromPath("../Rx-Main/lib/net35/Rx.dll").Framework |> shouldEqual (Framework "v3.5")
     FramworkCondition.DetectFromPath(@"..\NUnit\lib\NUnit.dll").Framework |> shouldEqual All
     FramworkCondition.DetectFromPath("../NUnit/lib/NUnit.dll").Framework |> shouldEqual All
-    
+
+
+[<Test>]
+let ``should detect client framework version from path``() =
+    // this is a temporary hack
+    FramworkCondition.DetectFromPath(@"..\packages\Castle.Core\lib\net40-client\Castle.Core.dll").Framework |> shouldEqual (Framework "v4.0")
+   
