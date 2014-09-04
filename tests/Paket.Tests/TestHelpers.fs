d5529b42f74c9562a375fc50df223c066237ca5d
isaacabraham <isaac.abraham@gmail.com>
2014-09-04 17:28:36 +0100

    Refactored LockFile parsing with idiomatic F# folding.
    Small changes to the Package record to remove some conditional cases that now can never occur.

--- tests/Paket.Tests/TestHelpers.fs
+++ tests/Paket.Tests/TestHelpers.fs
@@ -17,7 +17,7 @@ let DictionaryDiscovery(graph : seq<string * string * (string * VersionRange) li
                                 { Name = p
                                   VersionRange = v
                                   SourceType = sourceType
-                                  DirectDependencies = None
+                                  DirectDependencies = []
                                   ResolverStrategy = resolverStrategy
                                   Source = source })
                   return "",dependencies
@@ -31,7 +31,7 @@ let DictionaryDiscovery(graph : seq<string * string * (string * VersionRange) li
               } }
 
 let resolve graph (dependencies: (string * VersionRange) seq) =
-    let packages = dependencies |> Seq.map (fun (n,v) -> { Name = n; VersionRange = v; SourceType = ""; Source = ""; DirectDependencies = None; ResolverStrategy = ResolverStrategy.Max })
+    let packages = dependencies |> Seq.map (fun (n,v) -> { Name = n; VersionRange = v; SourceType = Nuget; Source = ""; DirectDependencies = []; ResolverStrategy = ResolverStrategy.Max })
     Resolver.Resolve(true, DictionaryDiscovery graph, packages).ResolvedVersionMap
 
 let getVersion resolved =
