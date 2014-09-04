a69ed17d95a3b1d4e1c51f47df77ac4a5d004a16
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-04 17:01:19 +0200

    Detect if not framework condition is needed - references #19

--- tests/Paket.Tests/ProjectFileConditionSpecs.fs
+++ tests/Paket.Tests/ProjectFileConditionSpecs.fs
@@ -6,8 +6,11 @@ open FsUnit
 
 [<Test>]
 let ``should detect framework version from path``() =
-    FramworkCondition.DetectFromPath(@"..\Rx-Main\lib\net40\Rx.dll").Framework |> shouldEqual "v4.0"
-    FramworkCondition.DetectFromPath(@"..\Rx-Main\lib\net45\Rx.dll").Framework |> shouldEqual "v4.5"
-    FramworkCondition.DetectFromPath(@"..\Rx-Main\lib\net20\Rx.dll").Framework |> shouldEqual "v2.0"
-    FramworkCondition.DetectFromPath(@"..\Rx-Main\lib\net35\Rx.dll").Framework |> shouldEqual "v3.5"
+    FramworkCondition.DetectFromPath(@"..\Rx-Main\lib\net40\Rx.dll").Framework |> shouldEqual (Framework "v4.0")
+    FramworkCondition.DetectFromPath(@"..\Rx-Main\lib\net45\Rx.dll").Framework |> shouldEqual (Framework "v4.5")
+    FramworkCondition.DetectFromPath(@"..\Rx-Main\lib\net20\Rx.dll").Framework |> shouldEqual (Framework "v2.0")
+    FramworkCondition.DetectFromPath(@"..\Rx-Main\lib\net35\Rx.dll").Framework |> shouldEqual (Framework "v3.5")
+    FramworkCondition.DetectFromPath("../Rx-Main/lib/net35/Rx.dll").Framework |> shouldEqual (Framework "v3.5")
+    FramworkCondition.DetectFromPath(@"..\NUnit\lib\NUnit.dll").Framework |> shouldEqual All
+    FramworkCondition.DetectFromPath("../NUnit/lib/NUnit.dll").Framework |> shouldEqual All
     
