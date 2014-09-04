f503d70b772da19c8d98b3bbbe26c359eec29a57
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-04 19:07:07 +0200

    Install SourceLink.Fake via Paket

--- build.cmd
+++ build.cmd
@@ -2,7 +2,6 @@
 cls
 
 .nuget\nuget.exe install Paket -OutputDirectory packages -Prerelease -ExcludeVersion
-.nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 
 packages\Paket\tools\Paket.exe install
 packages\FAKE\tools\FAKE.exe build.fsx %*
--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mono .nuget/NuGet.exe install Paket -OutputDirectory packages -Prerelease -ExcludeVersion
-mono .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 
 mono packages/Paket/tools/Paket.exe install
 mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
