2fa175a69a6ca03b98f748790e4d01b66bfad8de
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-05 11:33:06 +0200

    Document bug in #55

---
+++ tests/Paket.Tests/PessimisticResolverSpecs.fs
@@ -0,0 +1,54 @@
+module Paket.PessimisticResolverSpecs
+
+open Paket
+open NUnit.Framework
+open FsUnit
+open TestHelpers
+
+let graph = [
+    "Nancy.Bootstrappers.Windsor","0.23",["Castle.Windsor",VersionRange.AtLeast "3.2.1"]
+    "Castle.Windsor","3.2.1",[]
+    "Castle.Windsor","3.3.0",[]
+]
+
+let config1 = """
+source "http://nuget.org/api/v2"
+
+nuget "Nancy.Bootstrappers.Windsor" "!~> 0.23"
+"""
+
+[<Test>]
+let ``should resolve simple config1``() = 
+    let cfg = Config.FromCode config1
+    let resolved = cfg.Resolve(true, DictionaryDiscovery graph).ResolvedVersionMap
+    getVersion resolved.["Castle.Windsor"] |> shouldEqual "3.2.1"
+    getVersion resolved.["Nancy.Bootstrappers.Windsor"] |> shouldEqual "0.23"
+
+let config2 = """
+source "http://nuget.org/api/v2"
+
+nuget "Castle.Windsor" "!>= 0"
+nuget "Nancy.Bootstrappers.Windsor" "!~> 0.23"
+"""
+
+[<Test>]
+let ``should resolve simple config2``() = 
+    let cfg = Config.FromCode config2
+    let resolved = cfg.Resolve(true, DictionaryDiscovery graph).ResolvedVersionMap
+    getVersion resolved.["Castle.Windsor"] |> shouldEqual "3.2.1"
+    getVersion resolved.["Nancy.Bootstrappers.Windsor"] |> shouldEqual "0.23"
+
+
+let config3 = """
+source "http://nuget.org/api/v2"
+
+nuget "Nancy.Bootstrappers.Windsor" "!~> 0.23"
+nuget "Castle.Windsor" "!>= 0"
+"""
+
+[<Test>]
+let ``should resolve simple config3``() = 
+    let cfg = Config.FromCode config3
+    let resolved = cfg.Resolve(true, DictionaryDiscovery graph).ResolvedVersionMap
+    getVersion resolved.["Castle.Windsor"] |> shouldEqual "3.2.1"
+    getVersion resolved.["Nancy.Bootstrappers.Windsor"] |> shouldEqual "0.23"
