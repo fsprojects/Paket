b1f10893d7333db374f07cc217b06fcf85cd7b5b
F# Software Foundation <fsgit@users.noreply.github.com>
2014-09-07 18:30:35 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,8 +1,10 @@
 @echo off
 cls
 
-.nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
-.nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
+IF NOT EXIST packages\FAKE\tools\FAKE.exe  (
+  .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  .nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
+)
 
 IF NOT EXIST build.fsx (
   packages\FAKE\tools\FAKE.exe init.fsx
