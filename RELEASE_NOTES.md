a14c0c217a8ea6726ed38cc6087c77ee0020b6ee
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-10 09:09:38 +0200

    Bump version to 0.0.6-alpha001

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 0.0.6-alpha001 - 10.09.2014
+* Allow path sources
+
 #### 0.0.5-alpha004 - 09.09.2014
 * Dogfooding works
--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Paket")>]
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET")>]
-[<assembly: AssemblyVersionAttribute("0.0.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.5")>]
+[<assembly: AssemblyVersionAttribute("0.0.6")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.5"
+    let [<Literal>] Version = "0.0.6"
