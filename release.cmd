6f4ebfd5c058f4d20628e01a1fc4913035f434b1
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-14 00:13:46 +0200

    Update generated documentation for version 0.1.1

--- release.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cls
-
-.nuget\nuget.exe install Paket -OutputDirectory packages -Prerelease -ExcludeVersion
-
-packages\Paket\tools\paket.exe install
-packages\FAKE\tools\FAKE.exe build.fsx Release "NugetKey=6cfcf909-7b5a-417d-a706-d1a1ab9b4b9e"
