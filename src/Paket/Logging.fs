b9147514f75d12f89f1db004c1bb8ffd355c484a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-20 00:25:03 +0200

    Extract Logging

---
+++ src/Paket/Logging.fs
@@ -0,0 +1,28 @@
+﻿[<AutoOpen>]
+module internal Paket.Logging
+
+open System
+
+/// [omit]
+let monitor = new Object()
+
+/// [omit]
+let trace (s : string) = lock monitor (fun () -> printfn "%s" s)
+
+/// [omit]
+let tracefn fmt = Printf.ksprintf trace fmt
+
+/// [omit]
+let traceColored color (s: string) = 
+    lock monitor 
+        (fun () ->
+            let curColor = Console.ForegroundColor
+            if curColor <> color then Console.ForegroundColor <- color
+            printfn "%s" s
+            if curColor <> color then Console.ForegroundColor <- curColor)
+
+/// [omit]
+let traceError = traceColored ConsoleColor.Red
+
+/// [omit]
+let traceWarn = traceColored ConsoleColor.Yellow
--- src/Paket/Utils.fs
+++ src/Paket/Utils.fs
@@ -8,30 +8,6 @@ open System.Net
 open System.Xml
 
 /// [omit]
-let monitor = new Object()
-
-/// [omit]
-let trace (s : string) = lock monitor (fun () -> printfn "%s" s)
-
-/// [omit]
-let tracefn fmt = Printf.ksprintf trace fmt
-
-/// [omit]
-let traceColored color (s: string) = 
-    lock monitor 
-        (fun () ->
-            let curColor = Console.ForegroundColor
-            if curColor <> color then Console.ForegroundColor <- color
-            printfn "%s" s
-            if curColor <> color then Console.ForegroundColor <- curColor)
-
-/// [omit]
-let traceError = traceColored ConsoleColor.Red
-
-/// [omit]
-let traceWarn = traceColored ConsoleColor.Yellow
-
-/// [omit]
 let addAttribute name value (node:XmlElement) =
     node.SetAttribute(name, value) |> ignore
     node
