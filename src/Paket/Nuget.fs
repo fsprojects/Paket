8ed597530f6a55603cf0b3c9c5e3fa4f80ab6024
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-22 18:40:09 +0200

    Cleaning nuget cache

--- src/Paket/Nuget.fs
+++ src/Paket/Nuget.fs
@@ -7,7 +7,6 @@ open System.Net
 open Newtonsoft.Json
 open Ionic.Zip
 open System.Xml
-open System.Collections.Generic
 open System.Text.RegularExpressions
 open Paket.Logging
 
@@ -31,40 +30,26 @@ let getAllVersionsFromNugetOData (nugetURL, package) =
                        yield node.InnerText
                }
     }
-    
-let private versionDataCache = Dictionary<_, _>()
 
-/// Gets versions of the given package.
-/// Caches calls in RAM - do not cache on disk!
-let getAllVersions (nugetURL, package) =     
+/// Gets all versions no. of the given package.
+let getAllVersions (nugetURL, package) = 
     // we cannot cache this
     async { 
-        let key = nugetURL + package
-        match versionDataCache.TryGetValue key with
-        | true, data -> return data
-        | _ -> 
-            let! raw = sprintf "%s/package-versions/%s" nugetURL package |> safeGetFromUrl
-
-            match raw with
-            | None -> 
-                let! result = getAllVersionsFromNugetOData (nugetURL, package)
-                versionDataCache.Add(key, result)
-                return result
-            | Some data -> 
-                try
-                    try
-                        let result = JsonConvert.DeserializeObject<string []>(data) |> Array.toSeq
-                        versionDataCache.Add(key, result)
-                        return result
-                    with
-                    | _ ->
-                        let! result = getAllVersionsFromNugetOData (nugetURL, package)
-                        versionDataCache.Add(key, result)
-                        return result
-                with 
-                | exn -> 
-                    failwithf "Could not get data from %s for package %s.%s Message: %s" nugetURL package Environment.NewLine exn.Message
-                    return Seq.empty
+        let! raw = sprintf "%s/package-versions/%s" nugetURL package |> safeGetFromUrl
+        match raw with
+        | None -> let! result = getAllVersionsFromNugetOData (nugetURL, package)
+                  return result
+        | Some data -> 
+            try 
+                try 
+                    let result = JsonConvert.DeserializeObject<string []>(data) |> Array.toSeq
+                    return result
+                with _ -> let! result = getAllVersionsFromNugetOData (nugetURL, package)
+                          return result
+            with exn -> 
+                failwithf "Could not get data from %s for package %s.%s Message: %s" nugetURL package 
+                    Environment.NewLine exn.Message
+                return Seq.empty
     }
 
 /// Gets versions of the given package from local Nuget feed.
