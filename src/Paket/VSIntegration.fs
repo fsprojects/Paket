72a16de3830669d8ec2b079ecd49f48f16c7992c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-24 09:56:39 +0200

    Donwload latest in InitAutoRestore

--- src/Paket/VSIntegration.fs
+++ src/Paket/VSIntegration.fs
@@ -1,20 +1,25 @@
 ﻿module Paket.VSIntegration
 
-open System
 open System.IO
 open Logging
 
 let InitAutoRestore() = 
     if not <| Directory.Exists(".paket") then Directory.CreateDirectory(".paket") |> ignore
-    let version = AssemblyVersionInformation.Version
     CleanDir(".paket")
     use client = createWebClient()
+
+    let releasesUrl = "https://api.github.com/repos/fsprojects/Paket/releases";
+    let data = client.DownloadString(releasesUrl)
+    let start = data.IndexOf("tag_name") + 11
+    let end' = data.IndexOf("\"", start)
+    let latestVersion = data.Substring(start, end' - start);
+    
     for file in ["paket.targets"; "paket.bootstrapper.exe"] do
         try 
-            client.DownloadFile(sprintf "https://github.com/fsprojects/Paket/releases/download/%s/%s" version file, 
+            client.DownloadFile(sprintf "https://github.com/fsprojects/Paket/releases/download/%s/%s" latestVersion file, 
                         Path.Combine(".paket", file))
             tracefn "Downloaded %s" file
-        with _ -> traceErrorfn "Unable to download %s for version %s" file version
+        with _ -> traceErrorfn "Unable to download %s for version %s" file latestVersion
 
     for proj in ProjectFile.FindAllProjects(".") do
         let project = ProjectFile.Load(proj.FullName)
