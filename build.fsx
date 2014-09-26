71b26011a189bbe75813f204abadb8208263a66e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-26 15:41:50 +0200

    Ocotokit was moved

--- build.fsx
+++ build.fsx
@@ -230,7 +230,7 @@ Target "ReleaseDocs" (fun _ ->
     Branches.push tempDocsDir
 )
 
-#load "paket-files/fsharp/FAKE/modules/Octokit.fsx"
+#load "paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx"
 open Octokit
 
 Target "Release" (fun _ ->
