34fc042be1eab6760ffb62533c811c74a98c845c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-01 14:35:23 +0200

    Extracting a install model type

--- tests/Paket.Tests/ProjectFile/InstallModelSpecs.fs
+++ tests/Paket.Tests/ProjectFile/InstallModelSpecs.fs
@@ -10,48 +10,50 @@ type InstallOption = {
     FileName: string
     Framework: FrameworkIdentifier }
 
-let extractFrameworksFromPaths libs =
+type InstallModell = InstallOption Set
+
+let getFrameworks model = model |> Set.map (fun x -> x.Framework)
+
+
+let extractFrameworksFromPaths libs : InstallModell =
     libs
     |> List.map (fun lib ->     
             match FrameworkIdentifier.DetectFromPath lib with
             | Some framework -> Some {FileName = lib; Framework = framework}
             | None -> None)            
-    |> List.choose id    
+    |> List.choose id
+    |> Set.ofList
 
-let addMissingFrameworks libs =
+let addMissingFrameworks (model:InstallModell) =
     let frameworks =
-        libs
-        |> List.map (fun x -> match x.Framework with | DotNetFramework(Framework v,_) -> Some(v,x.FileName) | _ -> None)
-        |> List.choose id
+        model
+        |> Seq.map (fun x -> match x.Framework with | DotNetFramework(Framework v,_) -> Some(v,x.FileName) | _ -> None)
+        |> Seq.choose id
+        |> Set.ofSeq
 
-    libs
-    |> List.append [
-        { FileName = ""; Framework = DotNetFramework(Framework "v3.5",Full)}
-        { FileName = ""; Framework = DotNetFramework(Framework "v4.5",Full)}
-        { FileName = ""; Framework = DotNetFramework(Framework "v4.5.1",Full)}
-          ]
+    model
+    |> Set.add { FileName = ""; Framework = DotNetFramework(Framework "v3.5",Full)}
+    |> Set.add { FileName = ""; Framework = DotNetFramework(Framework "v4.5",Full)}
+    |> Set.add { FileName = ""; Framework = DotNetFramework(Framework "v4.5.1",Full)}          
     
 [<Test>]
 let ``should understand net40 and net45``() =
-    let options =
+    let model =
         [@"..\Rx-Main\lib\net40\Rx.dll"; @"..\Rx-Main\lib\net45\Rx.dll"]
         |> extractFrameworksFromPaths
     
-    options
-    |> List.map (fun x -> x.Framework)
-    |> shouldEqual [DotNetFramework(Framework "v4.0",Full); DotNetFramework(Framework "v4.5",Full)]
+    getFrameworks model |> shouldContain (DotNetFramework(Framework "v4.0",Full))
+    getFrameworks model |> shouldContain (DotNetFramework(Framework "v4.5",Full))
 
 [<Test>]
 let ``should add net35 if we have net20 and net40``() =
-    let options =
+    let model =
         [@"..\Rx-Main\lib\net20\Rx.dll"; @"..\Rx-Main\lib\net40\Rx.dll"]
         |> extractFrameworksFromPaths 
         |> addMissingFrameworks
 
-    let frameworks = options|> List.map (fun x -> x.Framework)
-
-    frameworks |> shouldContain (DotNetFramework(Framework "v2.0",Full))
-    frameworks |> shouldContain (DotNetFramework(Framework "v3.5",Full))
-    frameworks |> shouldContain (DotNetFramework(Framework "v4.0",Full))
-    frameworks |> shouldContain (DotNetFramework(Framework "v4.5",Full))
-    frameworks |> shouldContain (DotNetFramework(Framework "v4.5.1",Full))
+    getFrameworks model |> shouldContain (DotNetFramework(Framework "v2.0",Full))
+    getFrameworks model |> shouldContain (DotNetFramework(Framework "v3.5",Full))
+    getFrameworks model |> shouldContain (DotNetFramework(Framework "v4.0",Full))
+    getFrameworks model |> shouldContain (DotNetFramework(Framework "v4.5",Full))
+    getFrameworks model |> shouldContain (DotNetFramework(Framework "v4.5.1",Full))
