327ca721e280dbd66193f24bab973fa2de26898f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-10 18:30:15 +0200

    Bump version to 0.5.15

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub files.")]
-[assembly: AssemblyVersionAttribute("0.5.14")]
-[assembly: AssemblyFileVersionAttribute("0.5.14")]
+[assembly: AssemblyVersionAttribute("0.5.15")]
+[assembly: AssemblyFileVersionAttribute("0.5.15")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.5.14";
+        internal const string Version = "0.5.15";
     }
 }
