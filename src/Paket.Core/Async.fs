ad84519daf724fdf98ff6273aa5a2550d9279798
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-10 11:25:58 +0200

    Document Async

--- src/Paket.Core/Async.fs
+++ src/Paket.Core/Async.fs
@@ -1,9 +1,11 @@
 namespace FSharp.Control
 
+/// Extensions for async workflows.
 [<AutoOpen>]
 module AsyncExtensions = 
 
   type Microsoft.FSharp.Control.Async with 
+     /// Runs both computations in parallel and returns the result as a tuple.
     static member Parallel (a : Async<'a>, b : Async<'b>) : Async<'a * 'b> =
         async {
             let! a' = Async.StartChild a
