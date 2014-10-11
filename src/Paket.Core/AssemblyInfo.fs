6d2d774684b731aaa8a767c3d2b059979e28219b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-11 14:16:12 +0200

    Bump version to 0.6.2

--- src/Paket.Core/AssemblyInfo.fs
+++ src/Paket.Core/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Paket.Core")>]
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub files.")>]
-[<assembly: AssemblyVersionAttribute("0.6.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.6.1")>]
+[<assembly: AssemblyVersionAttribute("0.6.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.6.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.6.1"
+    let [<Literal>] Version = "0.6.2"
