bb5dedccf8f57c0739752d5cab9db7b0b9f97c3d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-11 13:56:08 +0200

    Bump version to 0.6.1

--- build.cmd
+++ build.cmd
@@ -6,7 +6,7 @@ if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-.paket\paket.exe install -v
+.paket\paket.exe restore -v
 if errorlevel 1 (
   exit /b %errorlevel%
 )
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ $exit_code -ne 0 ]; then
 	exit $exit_code
 fi
 
-mono .paket/paket.exe install -v
+mono .paket/paket.exe restore -v
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
 	exit $exit_code
