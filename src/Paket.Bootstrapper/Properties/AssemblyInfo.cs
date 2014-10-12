1ba6179073c0e8c01d411ce9c07b542309cc8181
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-12 18:03:45 +0200

    Bump version to 0.6.9

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub files.")]
-[assembly: AssemblyVersionAttribute("0.6.8")]
-[assembly: AssemblyFileVersionAttribute("0.6.8")]
+[assembly: AssemblyVersionAttribute("0.6.9")]
+[assembly: AssemblyFileVersionAttribute("0.6.9")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.6.8";
+        internal const string Version = "0.6.9";
     }
 }
