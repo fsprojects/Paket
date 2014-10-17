2ae2f8257769bc2649f57f1cb1af3270cb917c07
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-17 16:08:50 +0200

    Create a basic DependencyModel

---
+++ src/Paket.Core/DependencyModel.fs
@@ -0,0 +1,6 @@
+﻿module Paket.DependencyModel
+
+/// Calculates the used dependencies for a project.
+let CalcDependencies(dependenciesFile:DependenciesFile,references) = 
+    dependenciesFile.DirectDependencies
+    |> Map.filter (fun name _ -> references |> List.exists (fun (reference:string) -> name.ToLower() = reference.ToLower()))
--- tests/Paket.Tests/DependencyModel/ProjectDependencySpecs.fs
+++ tests/Paket.Tests/DependencyModel/ProjectDependencySpecs.fs
@@ -4,6 +4,20 @@ open Paket
 open NUnit.Framework
 open FsUnit
 
+let config1 = """
+source "http://nuget.org/api/v2"  username: "user" password: "pass"
+
+nuget "Castle.Windsor-log4net" "~> 3.2"
+"""
+
 [<Test>]
-let ``can calc project references``() = 
-    () // TODo:
+let ``project references for empty project should be empty``() = 
+    let cfg = DependenciesFile.FromCode(config1)
+    DependencyModel.CalcDependencies(cfg,[])
+    |> shouldEqual Map.empty
+
+[<Test>]
+let ``project reference for single dependency should be found``() = 
+    let cfg = DependenciesFile.FromCode(config1)
+    let model = DependencyModel.CalcDependencies(cfg,["Castle.Windsor-log4net"])
+    model.["Castle.Windsor-log4net"].Range |> shouldEqual (VersionRange.Between("3.2", "4.0"))
