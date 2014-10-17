e09fc9f97284122de29c01c228dbd12ce0a9a60e
Isaac Abraham <isaac.abraham@gmail.com>
2014-10-17 23:11:20 +0100

    Refactored some code on binding redirects and made some helper functions to make it easier to consume.

--- src/Paket.Core/BindingRedirects.fs
+++ src/Paket.Core/BindingRedirects.fs
@@ -2,11 +2,13 @@
 
 open System
 open System.Xml.Linq
+open System.IO
 
 [<AutoOpen>]
 module private Helpers =
     let asOption = function | null -> None | x -> Some x
     let tryGetElement name (xe:XContainer) = xe.Element(XName.Get name) |> asOption
+    let getElements name (xe:XContainer) = xe.Elements(XName.Get name)
     let tryGetAttribute name (xe:XElement) = xe.Attribute(XName.Get name) |> asOption
     let createElement name attributes = XElement(XName.Get name, attributes |> Seq.map(fun (name,value) -> XAttribute(XName.Get name, value)))    
     let ensurePathExists (xpath:string) (item:XContainer) =
@@ -27,33 +29,42 @@ type BindingRedirect =
         Culture : string option }
 
 /// Updates the supplied MSBuild document with the supplied binding redirect.
-let setRedirect bindingRedirect (doc:XDocument) =
+let setRedirect (doc:XDocument) bindingRedirect =
     let assemblyBinding = doc |> ensurePathExists "/configuration/runtime/assemblyBinding"
     let dependentAssembly =
-        assemblyBinding.Elements <| XName.Get "dependentAssembly"
+        assemblyBinding |> getElements "dependentAssembly"
         |> Seq.tryFind(fun dependentAssembly ->
             defaultArg
                 (dependentAssembly
                  |> tryGetElement "assemblyIdentity"
-                 |> Option.bind (tryGetAttribute "name")
+                 |> Option.bind(tryGetAttribute "name")
                  |> Option.map(fun attribute -> attribute.Value = bindingRedirect.AssemblyName))
                 false)
         |> function
            | Some dependentAssembly -> dependentAssembly
            | None ->
                 let dependentAssembly = createElement "dependentAssembly" []
-                let assemblyIdentity =
-                    createElement "assemblyIdentity" ([ "name", Some bindingRedirect.AssemblyName
-                                                        "publicKeyToken", bindingRedirect.PublicKeyToken
-                                                        "culture", bindingRedirect.Culture ]
-                                                      |> Seq.choose(fun (key,value) -> match value with Some v -> Some(key, v) | None -> None))
-
-                dependentAssembly.Add assemblyIdentity
-                assemblyBinding.Add dependentAssembly
+                dependentAssembly.Add(createElement "assemblyIdentity" ([ "name", Some bindingRedirect.AssemblyName
+                                                                          "publicKeyToken", bindingRedirect.PublicKeyToken
+                                                                          "culture", bindingRedirect.Culture ]
+                                                                        |> Seq.choose(fun (key,value) -> match value with Some v -> Some(key, v) | None -> None)))
+                assemblyBinding.Add(dependentAssembly)
                 dependentAssembly
                 
-    let newRedirect = createElement "bindingRedirect" [ "oldVersion", sprintf "0.0.0.0-%s" bindingRedirect.Version; "newVersion", bindingRedirect.Version ]
+    let newRedirect = createElement "bindingRedirect" [ "oldVersion", sprintf "0.0.0.0-%s" bindingRedirect.Version
+                                                        "newVersion", bindingRedirect.Version ]
     match dependentAssembly |> tryGetElement "bindingRedirect" with
     | Some redirect -> redirect.ReplaceWith(newRedirect)
     | None -> dependentAssembly.Add(newRedirect)
     doc
+
+/// Applies a set of binding redirects to a single configuration file.
+let applyBindingRedirects bindingRedirects (configFilePath:string) =
+    let config = XDocument.Load configFilePath
+    let config = Seq.fold setRedirect config bindingRedirects
+    config.Save configFilePath
+
+/// Applies a set of binding redirects to all .config files in a specific folder.
+let applyBindingRedirectsToFolder bindingRedirects rootPath =
+    Directory.GetFiles(rootPath, "*.config", SearchOption.AllDirectories)
+    |> Seq.iter (applyBindingRedirects bindingRedirects)
