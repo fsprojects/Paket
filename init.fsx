06c183c3582b394f37ce012ee8fd391ce3ae9892
bartelink <ruben@bartelink.com>
2014-10-20 12:16:11 +0100

    Minor spelling corrections

--- init.fsx
+++ init.fsx
@@ -179,4 +179,3 @@ let generate templatePath generatedFilePath =
 generate (localFile "build.template") (localFile "build.fsx")
 generate (localFile "docs/tools/generate.template") (localFile "docs/tools/generate.fsx")
 File.Delete "init.fsx"
-
