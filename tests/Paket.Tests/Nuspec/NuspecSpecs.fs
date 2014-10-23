d0f43d9764e047046dfbe6b60e9242bab4003936
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-23 08:58:40 +0200

    Add failing test for #287

---
+++ tests/Paket.Tests/Nuspec/NuspecSpecs.fs
@@ -0,0 +1,60 @@
+﻿module Paket.NuspecSpecs
+
+open Paket
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``can detect explicit references``() = 
+    Nuspec.Load("Nuspec/FSharp.Data.nuspec").References
+    |> shouldEqual (NuspecReferences.Explicit ["FSharp.Data.dll"])
+
+[<Test>]
+let ``can detect explicit in self made nuspec``() = 
+    Nuspec.Load("Nuspec/FSharp.Data.Prerelease.nuspec").References
+    |> shouldEqual (NuspecReferences.Explicit ["FSharp.Data.dll"])
+
+[<Test>]
+let ``can detect all references``() = 
+    Nuspec.Load("Nuspec/Octokit.nuspec").References
+    |> shouldEqual NuspecReferences.All
+
+[<Test>]
+let ``can detect all references for log4net``() = 
+    Nuspec.Load("Nuspec/log4net.nuspec").References
+    |> shouldEqual NuspecReferences.All
+
+[<Test>]
+let ``if nuspec is not found we assume all references``() = 
+    Nuspec.Load("Nuspec/blablub.nuspec").References
+    |> shouldEqual NuspecReferences.All
+
+[<Test>]
+let ``can detect explicit references for Fantomas``() = 
+    Nuspec.Load("Nuspec/Fantomas.nuspec").References
+    |> shouldEqual (NuspecReferences.Explicit ["FantomasLib.dll"])
+
+[<Test>]
+let ``can detect no framework assemblies for Fantomas``() = 
+    Nuspec.Load("Nuspec/Fantomas.nuspec").FrameworkAssemblyReferences
+    |> shouldEqual []
+
+[<Test>]
+let ``if nuspec is not found we assume no framework references``() = 
+    Nuspec.Load("Nuspec/blablub.nuspec").FrameworkAssemblyReferences
+    |> shouldEqual []
+
+[<Test>]
+let ``can detect framework assemblies for Microsoft.Net.Http``() = 
+    Nuspec.Load("Nuspec/Microsoft.Net.Http.nuspec").FrameworkAssemblyReferences
+    |> shouldEqual 
+        [{ AssemblyName = "System.Net.Http"; TargetFramework = DotNetFramework(FrameworkVersion.V4_5) }
+         { AssemblyName = "System.Net.Http.WebRequest"; TargetFramework = DotNetFramework(FrameworkVersion.V4_5) }
+         { AssemblyName = "System.Net.Http"; TargetFramework = MonoTouch }
+         { AssemblyName = "System.Net.Http"; TargetFramework = MonoAndroid } ]
+
+[<Test>]
+let ``can detect framework assemblies for Octokit``() = 
+    Nuspec.Load("Nuspec/Octokit.nuspec").FrameworkAssemblyReferences
+    |> shouldEqual 
+        [{ AssemblyName = "System.Net.Http"; TargetFramework = DotNetFramework(FrameworkVersion.V4_5) }]
