5663e1324a43328d3f21715f231faf79b9139c62
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-23 15:56:14 +0200

    Adding some VersionRequirementSpecs as regression tests

---
+++ tests/Paket.Tests/DependenciesFile/VersionRequirementSpecs.fs
@@ -0,0 +1,55 @@
+module paket.dependenciesFile.VersionRequirementSpecs
+
+open Paket
+open NUnit.Framework
+open FsUnit
+open Paket.Requirements
+
+let parse text = DependenciesFileParser.parseVersionRequirement(text)
+
+let require packageName strategy text : PackageRequirement = 
+    { Name = packageName
+      VersionRequirement = parse text
+      ResolverStrategy = strategy
+      Parent = PackageRequirementSource.DependenciesFile ""
+      Sources = [] }
+
+[<Test>]
+let ``can order simple at least requirements``() = 
+    parse ">= 2.2" |> shouldBeSmallerThan (parse ">= 2.3")
+    parse ">= 1.2" |> shouldBeSmallerThan (parse ">= 2.3")
+
+[<Test>]
+let ``can order twiddle-wakka``() = 
+    parse "~> 2.2" |> shouldBeSmallerThan (parse "~> 2.3")
+    parse "~> 1.2" |> shouldBeSmallerThan (parse "~> 2.3")
+
+[<Test>]
+let ``can order simple at least requirements in package requirement``() = 
+    require "A" ResolverStrategy.Max ">= 2.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Max ">= 2.3")
+    require "A" ResolverStrategy.Max ">= 1.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Max ">= 2.3")
+    
+    require "A" ResolverStrategy.Min ">= 2.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Min ">= 2.3")
+    require "A" ResolverStrategy.Min ">= 1.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Min ">= 2.3")
+
+[<Test>]
+let ``can order alphabetical if everything else is equal``() = 
+    require "B" ResolverStrategy.Max ">= 2.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Max ">= 2.2")
+    require "B" ResolverStrategy.Max ">= 1.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Max ">= 1.2")
+    
+    require "C" ResolverStrategy.Min ">= 2.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Min ">= 2.2")
+    require "C" ResolverStrategy.Min ">= 1.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Min ">= 1.2")
+
+[<Test>]
+let ``can order twiddle-wakka in package requirement``() = 
+    require "A" ResolverStrategy.Max "~> 2.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Max "~> 2.3")
+    require "A" ResolverStrategy.Max "~> 1.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Max "~> 2.3")
+    
+    require "A" ResolverStrategy.Min "~> 2.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Min "~> 2.3")
+    require "A" ResolverStrategy.Min "~> 1.2" |> shouldBeGreaterThan (require "A" ResolverStrategy.Min "~> 2.3")
+
+[<Test>]
+let ``can order resolver strategy``() = 
+    require "A" ResolverStrategy.Min "~> 2.2" |> shouldBeSmallerThan (require "A" ResolverStrategy.Max "~> 2.3")
+    require "A" ResolverStrategy.Min "~> 2.2" |> shouldBeSmallerThan (require "A" ResolverStrategy.Max "~> 2.1")
+    require "A" ResolverStrategy.Min "~> 2.4" |> shouldBeSmallerThan (require "A" ResolverStrategy.Max "~> 1.3")
