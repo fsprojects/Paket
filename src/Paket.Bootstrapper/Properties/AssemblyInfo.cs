e1841c5485817007b8d5bb2fa99fb280dcd52d82
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-24 12:07:44 +0200

    Update all the things

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("0.9.7")]
-[assembly: AssemblyFileVersionAttribute("0.9.7")]
+[assembly: AssemblyVersionAttribute("0.10.0")]
+[assembly: AssemblyFileVersionAttribute("0.10.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.9.7";
+        internal const string Version = "0.10.0";
     }
 }
