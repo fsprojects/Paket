14ae52e2cb8360d3268ceb4bc04d83fb657c4513
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-31 14:58:34 +0100

    Bump version to 0.11.10

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("0.11.9")]
-[assembly: AssemblyFileVersionAttribute("0.11.9")]
+[assembly: AssemblyVersionAttribute("0.11.10")]
+[assembly: AssemblyFileVersionAttribute("0.11.10")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.11.9";
+        internal const string Version = "0.11.10";
     }
 }
