fa5e00247af9339e26b1bc061f4e63bc06170824
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-11-02 11:24:27 +0100

    Add Fuchu test - references #323

---
+++ tests/Paket.Tests/InstallModel/Xml/Fuchu.fs
@@ -0,0 +1,29 @@
+﻿module Paket.InstallModel.Xml.FuchuSpecs
+
+open Paket
+open NUnit.Framework
+open FsUnit
+open Paket.TestHelpers
+
+let expected = """
+<ItemGroup xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <Reference Include="Fuchu">
+    <HintPath>..\..\..\Fuchu\lib\Fuchu.dll</HintPath>
+    <Private>True</Private>
+    <Paket>True</Paket>
+  </Reference>
+</ItemGroup>"""
+
+[<Test>]
+let ``should generate Xml for Fuchu 0.4``() = 
+    let model =
+        InstallModel.CreateFromLibs("Fuchu", SemVer.Parse "0.4.0",        
+            [ @"..\Fuchu\lib\Fuchu.dll" 
+              @"..\Fuchu\lib\Fuchu.XML" 
+              @"..\Fuchu\lib\Fuchu.pdb" ],
+              Nuspec.All).FilterFallbacks()
+    
+    let chooseNode = ProjectFile.Load("./ProjectFile/TestData/Empty.fsprojtest").Value.GenerateXml(model)
+    chooseNode.OuterXml
+    |> normalizeXml
+    |> shouldEqual (normalizeXml expected)
