33a4886b2b571f8a5d56f20291e9cd96543b46a4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-11-13 16:46:09 +0100

    Bump version to 0.14.0-alpha001

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("0.13.1")]
-[assembly: AssemblyFileVersionAttribute("0.13.1")]
+[assembly: AssemblyVersionAttribute("0.14.0")]
+[assembly: AssemblyFileVersionAttribute("0.14.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.13.1";
+        internal const string Version = "0.14.0";
     }
 }
