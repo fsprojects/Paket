5127d4b874037a063d21f5423e47af34d6bff00e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-11-20 17:56:38 +0100

    Inlining more stuff

--- src/Paket.Core/Xml.fs
+++ src/Paket.Core/Xml.fs
@@ -3,37 +3,37 @@
 open System.Xml
 
 /// [omit]
-let addAttribute name value (node:XmlElement) =
+let inline addAttribute name value (node:XmlElement) =
     node.SetAttribute(name, value) |> ignore
     node
 
 /// [omit]
-let addChild child (node:XmlElement) =
+let inline addChild child (node:XmlElement) =
     node.AppendChild(child) |> ignore
     node
 
 /// [omit]
-let getAttribute name (node:XmlNode) =
+let inline getAttribute name (node:XmlNode) =
     node.Attributes 
     |> Seq.cast<XmlAttribute> 
     |> Seq.tryFind (fun a -> a.Name = name) 
     |> Option.map (fun a -> a.Value)
 
 /// [omit]
-let optGetAttribute name node = node |> Option.bind (getAttribute name)
+let inline optGetAttribute name node = node |> Option.bind (getAttribute name)
 
 /// [omit]
-let getNode name (node:XmlNode) =
+let inline getNode name (node:XmlNode) =
     let xpath = sprintf "*[local-name() = '%s']" name
     match node.SelectSingleNode(xpath) with
     | null -> None
     | n -> Some(n)
 
 /// [omit]
-let optGetNode name node = node |> Option.bind (getNode name)
+let inline optGetNode name node = node |> Option.bind (getNode name)
 
 /// [omit]
-let getNodes name (node:XmlNode) =
+let inline getNodes name (node:XmlNode) =
     let xpath = sprintf "*[local-name() = '%s']" name
     match node.SelectNodes(xpath) with
     | null -> []
@@ -43,7 +43,7 @@ let getNodes name (node:XmlNode) =
         |> Seq.toList
 
 /// [omit]
-let getDescendants name (node:XmlNode) = 
+let inline getDescendants name (node:XmlNode) = 
     let xpath = sprintf "//*[local-name() = '%s']" name
     match node.SelectNodes(xpath) with
     | null -> []
