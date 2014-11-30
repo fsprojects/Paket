c7721e72813389ed0ec761d144c53c0b31faf2d6
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-11-30 20:38:25 +0100

    Bump version to 0.17.3

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("0.17.1")]
-[assembly: AssemblyFileVersionAttribute("0.17.1")]
+[assembly: AssemblyVersionAttribute("0.17.3")]
+[assembly: AssemblyFileVersionAttribute("0.17.3")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.17.1";
+        internal const string Version = "0.17.3";
     }
 }
