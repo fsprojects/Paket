5a6870a4ac805a2027c0f7c3454528e9cf156809
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2014-12-01 18:00:25 +0100

    decode uri on createRelativePath

---
+++ tests/Paket.Tests/UtilsSpecs.fs
@@ -0,0 +1,9 @@
+﻿module Paket.UtilsSpecs
+
+open Paket
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``createRelativePath should handle spaces``() =
+    "C:/some file" |> createRelativePath "C:/a/b" |> shouldEqual "..\\some file"
