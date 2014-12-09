bb00796dafcc7f6a2a89d1db9331663cbb324119
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-12-09 11:49:12 +0100

    Use replacements to simplify help contents

--- src/Paket/HelpTexts.fs
+++ src/Paket/HelpTexts.fs
@@ -4,7 +4,14 @@ type CommandHelpTopic =
     { Title : string
       Text : string }
     member this.ToMarkDown() =
-        sprintf "# %s%s%s" this.Title System.Environment.NewLine this.Text
+        let text =
+            this.Text
+                .Replace("paket.dependencies file","[`paket.dependencies` file](dependencies-file.html)")
+                .Replace("paket.lock file","[`paket.lock` file](lock-file.html)")
+                .Replace("paket.references files","[`paket.references` files](references-files.html)")
+                .Replace("paket.references file","[`paket.references` file](references-files.html)")
+                
+        sprintf "# %s%s%s" this.Title System.Environment.NewLine text
 
 let commands =
     ["convert-from-nuget", 
@@ -13,22 +20,22 @@ let commands =
 
 If you are already using `NuGet.exe` for package restore then it should be easy to convert to Paket.
 
-1. Analyse your `packages.config` files and extract the referenced packages into a [`paket.dependencies` file](dependencies-file.html).
-2. Convert each `packages.config` file to [`paket.references`](references-files.html) syntax. This is very easy - you just have to remove all the XML and keep the package names.
-3. Run [paket install](paket-install.html) with the `--hard` flag. This will analyze the dependencies, generate a [`paket.lock` file](lock-file.html), remove all the old package references from your project files and replace them with equivalent `Reference`s in a syntax that can be managed automatically by Paket.
+1. Analyse your `packages.config` files and extract the referenced packages into a paket.dependencies file.
+2. Convert each `packages.config` file to a paket.references file. This is very easy - you just have to remove all the XML and keep the package names.
+3. Run [paket install](paket-install.html) with the `--hard` flag. This will analyze the dependencies, generate a paket.lock file, remove all the old package references from your project files and replace them with equivalent `Reference`s in a syntax that can be managed automatically by Paket.
 
 <div id="automatic"></div>
 ## Automated process
 
 Paket can assist you with the conversion. The `paket convert-from-nuget` command:
 
-1. Finds all `packages.config` files, generates a [`paket.dependencies` file](dependencies-file.html) in the solution root and replaces each `packages.config` with an equivalent [`paket.references` file](references-files.html). 
-2. If there is a solution-level `packages.config`, then it will be removed and its dependencies will be included into the [`paket.dependencies`](dependencies-file.html).
+1. Finds all `packages.config` files, generates a paket.dependencies file in the solution root and replaces each `packages.config` with an equivalent paket.references file. 
+2. If there is a solution-level `packages.config`, then it will be removed and its dependencies will be included into the paket.dependencies file.
 3. If you use NuGet Package Restore ([MSBuild-Integrated or Automatic Visual Studio Package Restore](http://docs.nuget.org/docs/workflows/migrating-to-automatic-package-restore)), then the [`paket init-auto-restore`](paket-init-auto-restore.html) command will be invoked.
 4. Next (unless `--no-install` is specified), the [paket install](paket-install.html) process with the `--hard` flag will be executed. This will:
 
   - analyze the dependencies.
-  - generate a [`paket.lock` file](lock-file.html).
+  - generate a paket.lock file.
   - remove all the old package references from your project files and install new references in Paket's syntax.
 
 5. If you specify `--force`, the conversion will attempt to infer additional dependencies from newly added / previously unprocessed `packages.config` files and 
@@ -43,7 +50,7 @@ Paket can assist you with the conversion. The `paket convert-from-nuget` command
 
 Options:
 
-  `--force`: Forces the conversion, even if a [`paket.dependencies` file](dependencies-file.html) or [`paket.references`](references-files.html) files are present.
+  `--force`: Forces the conversion, even if a paket.dependencies file or paket.references files are present.
 
   `--no-install`: Skips [`paket install --hard`](paket-install.html) process afterward generation of dependencies / references files.
 
@@ -56,14 +63,14 @@ Options:
 If you are using authorized NuGet feeds, convert-from-nuget command will automatically migrate the credentials for you.
 Following are valid modes for `--creds-migration` option:
 
-1. `encrypt` -  Encrypt your credentials and save in [Paket configuration file](paket-config-file.html).
-2. `plaintext` - Include your credentials in plaintext in [`paket.dependencies`](dependencies-file.html) file. See [example](nuget-dependencies.html#plaintext-credentials)
+1. `encrypt` -  Encrypt your credentials and save in [paket configuration file](paket-config-file.html).
+2. `plaintext` - Include your credentials in plaintext in paket.dependencies file. See [example](nuget-dependencies.html#plaintext-credentials)
 3. `selective` - Use this switch, if you're using more than one authorized NuGet feed, and want to apply different mode for each of them.
 
 ## Simplify direct dependencies
 
 After converting your solution from NuGet, you may end up with many indirect dependencies in your Paket files.
-Consider using [`paket simplify`](paket-simplify.html) to remove unnecessary indirect dependencies from your [`paket.dependencies`](dependencies-file.html) and [`paket.references`](references-files.html) files."""}
+Consider using [`paket simplify`](paket-simplify.html) to remove unnecessary indirect dependencies from your paket.dependencies file and paket.references files."""}
 
      "init-auto-restore",
         { Title = "paket init-auto-restore"
@@ -80,7 +87,7 @@ The command:
 
      "restore",
         { Title = "paket restore"
-          Text = """Ensures that all dependencies in your [`paket.dependencies` file](dependencies-file.html) are present in the `packages` directory .
+          Text = """Ensures that all dependencies in your paket.dependencies file are present in the `packages` directory .
 
     [lang=batchfile]
     $ paket restore [--force] [--references-files REFERENCESFILE1 REFERENCESFILE2 ...]
@@ -89,12 +96,12 @@ Options:
 
   `--force`: Forces the download of all packages.
 
-  `--references-files`: Allows to restore all packages from the given `paket.references` files. If no `paket.references` file is given then all packages will be restored."""}
+  `--references-files`: Allows to restore all packages from the given paket.references files. If no paket.references file is given then all packages will be restored."""}
 
      "simplify",
         { Title = "paket simplify"
-          Text = """Simplifies your [`paket.dependencies` file](dependencies-file.html) by removing indirect dependencies.
-Does also simplify [`paket.references` files](references-files.html), unless [strict](dependencies-file.html#Strict-references) mode is used.
+          Text = """Simplifies your paket.dependencies file by removing indirect dependencies.
+Does also simplify paket.references files, unless [strict](dependencies-file.html#Strict-references) mode is used.
 
     [lang=batchfile]
     $ paket simplify [-v] [--interactive]
@@ -116,32 +123,32 @@ When you install `Castle.Windsor` package in NuGet to a project, it will generat
       <package id="Castle.Windsor" version="3.3.0" targetFramework="net451" />
     </packages>
 
-After converting to Paket with [`paket convert-from-nuget command`](paket-convert-from-nuget.html), you should get a following [`paket.dependencies` file](dependencies-file.html):
+After converting to Paket with [`paket convert-from-nuget command`](paket-convert-from-nuget.html), you should get a following paket.dependencies file:
 
     source https://nuget.org/api/v2
     
     nuget Castle.Core 3.3.1
     nuget Castle.Windsor 3.3.0
 
-and the NuGet `packages.config` should be converted to following [`paket.references` file](references-files.html):
+and the NuGet `packages.config` should be converted to following paket.references file:
 
     Castle.Core
     Castle.Windsor
 
 As you have already probably guessed, the `Castle.Windsor` package happens to have a dependency on the `Castle.Core` package.
-Paket by default (without [strict](dependencies-file.html#Strict-references) mode) adds references to all required dependencies of a package that you define for a specific project in [`paket.references` file](references-files.html).
-In other words, you still get the same result if you remove `Castle.Core` from your [`paket.references` file](references-files.html).
+Paket by default (without [strict](dependencies-file.html#Strict-references) mode) adds references to all required dependencies of a package that you define for a specific project in paket.references file.
+In other words, you still get the same result if you remove `Castle.Core` from your paket.references file.
 And this is exactly what happens after executing `paket simplify` command:
 
     source https://nuget.org/api/v2
     
     nuget Castle.Windsor 3.3.0
 
-will be the content of your [`paket.dependencies` file](dependencies-file.html), and:
+will be the content of your paket.dependencies file, and:
 
     Castle.Windsor
 
-will be the content of your [`paket.references` file](references-files.html).
+will be the content of your paket.references file.
 
 Unless you are relying heavily on components from `Castle.Core`, you would not care about controlling the required version of `Castle.Core` package. Paket will do the job.
 
@@ -154,21 +161,21 @@ which will ask you to confirm before deleting a dependency from a file."""}
 
      "init",
         { Title = "paket init"
-          Text = """Creates empty dependencies file in working directory.
+          Text = """Creates empty paket.dependencies file in the working directory.
 
     [lang=batchfile]
     $ paket init"""}
 
      "add",
         { Title = "paket add"
-          Text = """Adds a new package to your [`paket.dependencies` file](dependencies-file.html).
+          Text = """Adds a new package to your paket.dependencies file.
 
     [lang=batchfile]
     $ paket add nuget PACKAGENAME [version VERSION] [--interactive] [--force] [--hard]
 
 Options:
 
-  `--interactive`: Asks the user for every project if he or she wants to add the package to the projects's [`paket.references` file](references-file.html).
+  `--interactive`: Asks the user for every project if he or she wants to add the package to the projects's paket.references file.
 
   `--force`: Forces the download and reinstallation of all packages.
 
@@ -178,7 +185,7 @@ Options:
 
 ## Sample
 
-Consider the following [`paket.dependencies` file](dependencies-file.html):
+Consider the following paket.dependencies file:
 
 	source https://nuget.org/api/v2
 
@@ -188,7 +195,7 @@ Now we run `paket add nuget xunit --interactive` install the package:
 
 ![alt text](img/interactive-add.png "Interactive paket add")
 
-This will add the package to the selected [`paket.references` files](references-file.html) and also to the [`paket.dependencies` file](dependencies-file.html):
+This will add the package to the selected paket.references files and also to the paket.dependencies file:
 
 	source https://nuget.org/api/v2
 
@@ -230,7 +237,7 @@ and paket gives the following output:
 
      "update",
         { Title = "paket update"
-          Text = """Recomputes the dependency resolution, updates the [`paket.lock` file](lock-file.html) and propagates any resulting package changes into all project files referencing updated packages.
+          Text = """Recomputes the dependency resolution, updates the paket.lock file and propagates any resulting package changes into all project files referencing updated packages.
 
     [lang=batchfile]
     $ paket update [--force] [--hard]	
@@ -265,18 +272,18 @@ Options:
 
   `--pre`: Includes prereleases.
 
-  `--ignore-constraints`: Ignores the version requirement as in the [`paket.dependencies`](dependencies-file.html).
+  `--ignore-constraints`: Ignores the version requirement as in the paket.dependencies file.
 
 ## Sample
 
-Consider the following [`paket.dependencies` file](dependencies-file.html):
+Consider the following paket.dependencies file:
 
     source https://nuget.org/api/v2
     
     nuget Castle.Core
     nuget Castle.Windsor
 
-and the following [`paket.lock` file](lock-file.html): 
+and the following paket.lock file: 
 
     NUGET
       remote: https://nuget.org/api/v2
@@ -291,14 +298,14 @@ Now we run `paket outdated`:
 
      "remove",
         { Title = "paket remove"
-          Text = """Removes a package from your [`paket.dependencies` file](dependencies-file.html) and all [`paket.references` files](references-file.html).
+          Text = """Removes a package from your paket.dependencies file and all paket.references files.
 
     [lang=batchfile]
     $ paket remove nuget PACKAGENAME [--interactive] [--force] [--hard]
 
 Options:
 
-  `--interactive`: Asks the user for every project if he or she wants to remove the package from the projects's [`paket.references` file](references-file.html). By default every installation of the package is removed.
+  `--interactive`: Asks the user for every project if he or she wants to remove the package from the projects's paket.references file. By default every installation of the package is removed.
 
   `--force`: Forces the download and reinstallation of all packages.
 
@@ -308,7 +315,7 @@ See also [paket add](paket-add.html)."""}
 
      "install",
         { Title = "paket install"
-          Text = """Ensures that all dependencies in your [`paket.dependencies` file](dependencies-file.html) are present in the `packages` directory and referenced correctly in all projects.
+          Text = """Ensures that all dependencies in your paket.dependencies file are present in the `packages` directory and referenced correctly in all projects.
 
     [lang=batchfile]
     $ paket install [--force] [--hard]
