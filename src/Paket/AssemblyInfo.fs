536c421c8ac7e495dcf44b8e682ceb1e33d0d953
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-12-20 14:36:08 +0100

    Bump version to 0.18.11

--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Paket")>]
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
-[<assembly: AssemblyVersionAttribute("0.18.10")>]
-[<assembly: AssemblyFileVersionAttribute("0.18.10")>]
+[<assembly: AssemblyVersionAttribute("0.18.11")>]
+[<assembly: AssemblyFileVersionAttribute("0.18.11")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.18.10"
+    let [<Literal>] Version = "0.18.11"
