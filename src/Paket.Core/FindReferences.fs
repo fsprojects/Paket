5eca726fd86a8c609b03e0f389315e3bb5b21e96
Max Malook <community@malook.de>
2015-01-07 23:59:32 +0100

    convert failures in FindReferences module to ROP messages

--- src/Paket.Core/FindReferences.fs
+++ src/Paket.Core/FindReferences.fs
@@ -6,28 +6,41 @@ open Logging
 open Paket.Domain
 open Paket.Rop
 
-let private findReferencesFor package (lockFile: LockFile) projects =
-    [ for project,referencesFile in projects do
-        let installedPackages =
-            referencesFile
-            |> lockFile.GetPackageHull
-            |> Seq.map NormalizedPackageName
-            |> Set.ofSeq
+let private findReferencesFor package (lockFile: LockFile) projects = rop {
+    let! referencedIn =
+        projects
+        |> Seq.map (fun (project, referencesFile) -> rop {
+            let! installedPackages =
+                referencesFile
+                |> lockFile.GetPackageHullSafe
 
-        if installedPackages.Contains(NormalizedPackageName package) then
-            yield project.FileName ]
+            let referenced =
+                installedPackages
+                |> Set.map NormalizedPackageName
+                |> Set.contains (NormalizedPackageName package)
+
+            return if referenced then Some project.FileName else None })
+        |> Rop.collect
+
+    return referencedIn |> List.choose id
+}
 
 let FindReferencesForPackage package environment = rop {
     let! lockFile = environment |> PaketEnv.ensureLockFileExists
 
-    return findReferencesFor package lockFile environment.Projects
+    return! findReferencesFor package lockFile environment.Projects
 }
 
 let ShowReferencesFor packages environment = rop {
     let! lockFile = environment |> PaketEnv.ensureLockFileExists
+    let! projectsPerPackage =
+        packages
+        |> Seq.map (fun package -> rop {
+            let! projects = findReferencesFor package lockFile environment.Projects
+            return package, projects })
+        |> Rop.collect
 
-    packages
-    |> Seq.map (fun package -> package, findReferencesFor package lockFile environment.Projects)
+    projectsPerPackage
     |> Seq.iter (fun (PackageName k, vs) ->
         tracefn "%s" k
         vs |> Seq.iter (tracefn "%s")
