60606c6eff69a93a8ea2d1d1e4ce40db7a528fb8
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-01-15 16:03:25 +0100

    Introduce Command DU

--- src/Paket/Program.fs
+++ src/Paket/Program.fs
@@ -79,6 +79,23 @@ with
             | FindRefs _ -> "finds all references to the given packages."
             | AddCredentials _ -> "add credentials to config file for the specified source."
 
+
+type Command =
+    | [<First>][<CustomCommandLine("add")>]                 Add
+    | [<First>][<CustomCommandLine("config")>]              Config
+    | [<First>][<CustomCommandLine("convert-from-nuget")>]  ConvertFromNuget
+    | [<First>][<CustomCommandLine("find-refs")>]           FindRefs 
+    | [<First>][<CustomCommandLine("init")>]                Init
+    | [<First>][<CustomCommandLine("init-auto-restore")>]   InitAutoRestore
+    | [<First>][<CustomCommandLine("install")>]             Install
+    | [<First>][<CustomCommandLine("outdated")>]            Outdated
+    | [<First>][<CustomCommandLine("remove")>]              Remove
+    | [<First>][<CustomCommandLine("restore")>]             Restore
+    | [<First>][<CustomCommandLine("simplify")>]            Simplify
+    | [<First>][<CustomCommandLine("update")>]              Update
+with 
+    interface IArgParserTemplate with
+        member __.Usage = ""
  
 type GlobalArgs =
     | [<AltCommandLine("-v")>] Verbose
@@ -87,7 +104,22 @@ with
     interface IArgParserTemplate with
         member __.Usage = ""
 
+let (|Command|_|) args = 
+    let results = 
+        UnionArgParser.Create<Command>()
+            .Parse(inputs = args,
+                   ignoreMissing = true, 
+                   ignoreUnrecognized = true, 
+                   raiseOnUsage = false)
+
+    match results.GetAllResults() with
+    | [ command ] -> Some (command, args.[1..])
+    | [] -> None
+    | _ -> failwith "expected only one command"
+
+
 let filterGlobalArgs args = 
+
     let globalResults = 
         UnionArgParser.Create<GlobalArgs>().Parse(ignoreMissing = true, ignoreUnrecognized = true, raiseOnUsage = false)
     let verbose = globalResults.Contains <@ GlobalArgs.Verbose @>
@@ -95,184 +127,184 @@ let filterGlobalArgs args =
 
     let rest = 
         match logFile with
-        | Some file -> args |> List.filter (fun a -> a <> "--log-file" && a <> file)
+        | Some file -> args |> Array.filter (fun a -> a <> "--log-file" && a <> file)
         | None -> args
 
     let rest = 
-        if verbose then rest |> List.filter (fun a -> a <> "-v" && a <> "--verbose")
+        if verbose then rest |> Array.filter (fun a -> a <> "-v" && a <> "--verbose")
         else rest
 
     verbose, logFile, rest
+
+    
+let commandArgs<'T when 'T :> IArgParserTemplate> args = 
+    UnionArgParser
+        .Create<'T>()
+        .Parse(inputs = args, raiseOnUsage = false, errorHandler = ProcessExiter())
+
 
 let showHelp (helpTopic:HelpTexts.CommandHelpTopic) = 
                         tracefn "%s" helpTopic.Title
                         tracefn "%s" helpTopic.Text
 
-let v, logFile, args = filterGlobalArgs (Environment.GetCommandLineArgs() |> Seq.skip 1 |> Seq.toList)
-
-let commandArgs<'T when 'T :> IArgParserTemplate> args = 
-    UnionArgParser
-        .Create<'T>()
-        .Parse(inputs = Array.ofList args, raiseOnUsage = false, errorHandler = ProcessExiter())
+let v, logFile, args = filterGlobalArgs (Environment.GetCommandLineArgs().[1..])
 
 Logging.verbose <- v
 Option.iter setLogFile logFile
 
 try
     match args with
-        | "add" :: args ->
-            let results = commandArgs<AddArgs> args
+    | Command(Add, args) ->
+        let results = commandArgs<AddArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["add"]
-            else
-                let packageName = results.GetResult <@ AddArgs.Nuget @>
-                let version = defaultArg (results.TryGetResult <@ AddArgs.Version @>) ""
-                let force = results.Contains <@ AddArgs.Force @>
-                let hard = results.Contains <@ AddArgs.Hard @>
-                let interactive = results.Contains <@ AddArgs.Interactive @>
-                let noInstall = results.Contains <@ AddArgs.No_Install @>
-                Dependencies.Locate().Add(packageName, version, force, hard, interactive, noInstall |> not)
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["add"]
+        else
+            let packageName = results.GetResult <@ AddArgs.Nuget @>
+            let version = defaultArg (results.TryGetResult <@ AddArgs.Version @>) ""
+            let force = results.Contains <@ AddArgs.Force @>
+            let hard = results.Contains <@ AddArgs.Hard @>
+            let interactive = results.Contains <@ AddArgs.Interactive @>
+            let noInstall = results.Contains <@ AddArgs.No_Install @>
+            Dependencies.Locate().Add(packageName, version, force, hard, interactive, noInstall |> not)
         
-        | "config" :: args ->
-            let results = commandArgs<ConfigArgs> args
+    | Command(Config, args) ->
+        let results = commandArgs<ConfigArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["config"]
-            else
-                let args = results.GetResults <@ ConfigArgs.AddCredentials @> 
-                let source = args.Item 0
-                let username = 
-                    if(args.Length > 1) then
-                        args.Item 1
-                    else
-                        ""
-                Paket.ConfigFile.askAndAddAuth(source)(username)
+        if results.IsUsageRequested then
+            trace <| results.Usage("paket config")
+        else
+            let args = results.GetResults <@ ConfigArgs.AddCredentials @> 
+            let source = args.Item 0
+            let username = 
+                if(args.Length > 1) then
+                    args.Item 1
+                else
+                    ""
+            Paket.ConfigFile.askAndAddAuth(source)(username)
 
-        | "convert-from-nuget" :: args ->
-            let results = commandArgs<ConvertFromNugetArgs> args
+    | Command(ConvertFromNuget, args) ->
+        let results = commandArgs<ConvertFromNugetArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["convert-from-nuget"]
-            else
-                let force = results.Contains <@ ConvertFromNugetArgs.Force @>
-                let noInstall = results.Contains <@ ConvertFromNugetArgs.No_Install @>
-                let noAutoRestore = results.Contains <@ ConvertFromNugetArgs.No_Install @>
-                let credsMigrationMode = results.TryGetResult <@ ConvertFromNugetArgs.Creds_Migration @>
-                Dependencies.ConvertFromNuget(force, noInstall |> not, noAutoRestore |> not, credsMigrationMode)
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["convert-from-nuget"]
+        else
+            let force = results.Contains <@ ConvertFromNugetArgs.Force @>
+            let noInstall = results.Contains <@ ConvertFromNugetArgs.No_Install @>
+            let noAutoRestore = results.Contains <@ ConvertFromNugetArgs.No_Install @>
+            let credsMigrationMode = results.TryGetResult <@ ConvertFromNugetArgs.Creds_Migration @>
+            Dependencies.ConvertFromNuget(force, noInstall |> not, noAutoRestore |> not, credsMigrationMode)
     
-        | "find-refs" :: args ->
-            let results = commandArgs<FindRefsArgs> args
+    | Command(FindRefs, args) ->
+        let results = commandArgs<FindRefsArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["find-refs"]
-            else
-                let packages = results.GetResults <@ FindRefsArgs.Packages @>
-                Dependencies.Locate().ShowReferencesFor(packages)
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["find-refs"]
+        else
+            let packages = results.GetResults <@ FindRefsArgs.Packages @>
+            Dependencies.Locate().ShowReferencesFor(packages)
         
-        | "init" :: args ->
-            let results = commandArgs<InitArgs> args
+    | Command(Init, args) ->
+        let results = commandArgs<InitArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["init"]
-            else
-                Dependencies.Init()
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["init"]
+        else
+            Dependencies.Init()
 
-        | "init-auto-restore" :: args ->
-            let results = commandArgs<InitAutoRestoreArgs> args
+    | Command(InitAutoRestore, args) ->
+        let results = commandArgs<InitAutoRestoreArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["init-auto-restore"]
-            else
-                Dependencies.Locate().InitAutoRestore()
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["init-auto-restore"]
+        else
+            Dependencies.Locate().InitAutoRestore()
 
-        | "install" :: args ->
-            let results = commandArgs<InstallArgs> args
+    | Command(Install, args) ->
+        let results = commandArgs<InstallArgs> args
             
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["install"]
-            else
-                let force = results.Contains <@ InstallArgs.Force @>
-                let hard = results.Contains <@ InstallArgs.Hard @>
-                let withBindingRedirects = results.Contains <@ InstallArgs.Redirects @>
-                Dependencies.Locate().Install(force,hard,withBindingRedirects)
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["install"]
+        else
+            let force = results.Contains <@ InstallArgs.Force @>
+            let hard = results.Contains <@ InstallArgs.Hard @>
+            let withBindingRedirects = results.Contains <@ InstallArgs.Redirects @>
+            Dependencies.Locate().Install(force,hard,withBindingRedirects)
 
-        | "outdated" :: args ->
-            let results = commandArgs<OutdatedArgs> args
+    | Command(Outdated, args) ->
+        let results = commandArgs<OutdatedArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["outdated"]
-            else
-                let strict = results.Contains <@ OutdatedArgs.Ignore_Constraints @> |> not
-                let includePrereleases = results.Contains <@ OutdatedArgs.Include_Prereleases @>
-                Dependencies.Locate().ShowOutdated(strict,includePrereleases)
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["outdated"]
+        else
+            let strict = results.Contains <@ OutdatedArgs.Ignore_Constraints @> |> not
+            let includePrereleases = results.Contains <@ OutdatedArgs.Include_Prereleases @>
+            Dependencies.Locate().ShowOutdated(strict,includePrereleases)
 
-        | "remove" :: args ->
-            let results = commandArgs<RemoveArgs> args
+    | Command(Remove, args) ->
+        let results = commandArgs<RemoveArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["remove"]
-            else 
-                let packageName = results.GetResult <@ RemoveArgs.Nuget @>
-                let force = results.Contains <@ RemoveArgs.Force @>
-                let hard = results.Contains <@ RemoveArgs.Hard @>
-                let interactive = results.Contains <@ RemoveArgs.Interactive @>
-                let noInstall = results.Contains <@ RemoveArgs.No_Install @>
-                Dependencies.Locate().Remove(packageName, force, hard, interactive, noInstall |> not)
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["remove"]
+        else 
+            let packageName = results.GetResult <@ RemoveArgs.Nuget @>
+            let force = results.Contains <@ RemoveArgs.Force @>
+            let hard = results.Contains <@ RemoveArgs.Hard @>
+            let interactive = results.Contains <@ RemoveArgs.Interactive @>
+            let noInstall = results.Contains <@ RemoveArgs.No_Install @>
+            Dependencies.Locate().Remove(packageName, force, hard, interactive, noInstall |> not)
 
-        | "restore" :: args ->
-            let results = commandArgs<RestoreArgs> args
+    | Command(Restore, args) ->
+        let results = commandArgs<RestoreArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["restore"]
-            else 
-                let force = results.Contains <@ RestoreArgs.Force @>
-                let files = results.GetResults <@ RestoreArgs.References_Files @> 
-                Dependencies.Locate().Restore(force,files)
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["restore"]
+        else 
+            let force = results.Contains <@ RestoreArgs.Force @>
+            let files = results.GetResults <@ RestoreArgs.References_Files @> 
+            Dependencies.Locate().Restore(force,files)
 
-        | "simplify" :: args ->
-            let results = commandArgs<SimplifyArgs> args
+    | Command(Simplify, args) ->
+        let results = commandArgs<SimplifyArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["simplify"]
-            else 
-                let interactive = results.Contains <@ SimplifyArgs.Interactive @>
-                Dependencies.Simplify(interactive)
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["simplify"]
+        else 
+            let interactive = results.Contains <@ SimplifyArgs.Interactive @>
+            Dependencies.Simplify(interactive)
 
-        | "update" :: args ->
-            let results = commandArgs<UpdateArgs> args
+    | Command(Update, args) ->
+        let results = commandArgs<UpdateArgs> args
 
-            if results.IsUsageRequested then
-                showHelp HelpTexts.commands.["update"]
-            else 
-                let hard = results.Contains <@ UpdateArgs.Hard @>
-                let force = results.Contains <@ UpdateArgs.Force @>
-                match results.TryGetResult <@ UpdateArgs.Nuget @> with
-                | Some packageName -> 
-                    let version = results.TryGetResult <@ UpdateArgs.Version @>
-                    Dependencies.Locate().UpdatePackage(packageName, version, force, hard)
-                | _ -> 
-                    let withBindingRedirects = results.Contains <@ UpdateArgs.Redirects @>
-                    Dependencies.Locate().Update(force,hard,withBindingRedirects)
+        if results.IsUsageRequested then
+            showHelp HelpTexts.commands.["update"]
+        else 
+            let hard = results.Contains <@ UpdateArgs.Hard @>
+            let force = results.Contains <@ UpdateArgs.Force @>
+            match results.TryGetResult <@ UpdateArgs.Nuget @> with
+            | Some packageName -> 
+                let version = results.TryGetResult <@ UpdateArgs.Version @>
+                Dependencies.Locate().UpdatePackage(packageName, version, force, hard)
+            | _ -> 
+                let withBindingRedirects = results.Contains <@ UpdateArgs.Redirects @>
+                Dependencies.Locate().Update(force,hard,withBindingRedirects)
 
-        | _ ->
+    | _ ->
+        let allCommands = 
+            Microsoft.FSharp.Reflection.FSharpType.GetUnionCases(typeof<Command>)
+            |> Array.map (fun command -> 
+                   let attr = 
+                       command.GetCustomAttributes(typeof<CustomCommandLineAttribute>)
+                       |> Seq.cast<CustomCommandLineAttribute>
+                       |> Seq.head
+                   attr.Name)
+            |> String.concat Environment.NewLine
 
-            let toCommandLine (commandArgsType : string) =
-                commandArgsType.Replace("Args", "")
-                |> Seq.mapi (fun i c -> if i > 0 && Char.IsUpper c then [|'-';c|] else [|c|])
-                |> Array.concat
-                |> (fun array -> String(array).ToLowerInvariant())
-                
-            let allCommands =
-                typeof<AddArgs>.DeclaringType.GetNestedTypes() 
-                |> Array.map (fun argsType -> toCommandLine argsType.Name)
-                |> String.concat Environment.NewLine
-
-            tracefn "available commands: %s%s%s%s" 
-                Environment.NewLine
-                Environment.NewLine
-                allCommands
-                Environment.NewLine
+        tracefn "available commands: %s%s%s%s" 
+            Environment.NewLine
+            Environment.NewLine
+            allCommands
+            Environment.NewLine
 
     let elapsedTime = Utils.TimeSpanToReadableString stopWatch.Elapsed
     tracefn "%s - ready." elapsedTime
