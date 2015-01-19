e5d53397eb9b143624e75b101b29324046e383ed
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-01-19 10:04:53 +0100

    Use ILRepack 1.25 from nuget.org

--- build.fsx
+++ build.fsx
@@ -142,7 +142,7 @@ Target "MergePaketTool" (fun _ ->
 
     let result =
         ExecProcess (fun info ->
-            info.FileName <- currentDirectory @@ "tools" @@ "ILRepack" @@ "ILRepack.exe"
+            info.FileName <- currentDirectory @@ "packages" @@ "ILRepack" @@ "tools" @@ "ILRepack.exe"
             info.Arguments <- sprintf "/internalize /verbose /lib:%s /ver:%s /out:%s %s" buildDir release.AssemblyVersion (buildMergedDir @@ "paket.exe") toPack
             ) (TimeSpan.FromMinutes 5.)
 
