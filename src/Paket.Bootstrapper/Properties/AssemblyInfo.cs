cb2968f5a93cc00c05130e1b779e3c08264985e4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-01-19 16:06:03 +0100

    Bump version to 0.24.0

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("0.23.1")]
-[assembly: AssemblyFileVersionAttribute("0.23.1")]
+[assembly: AssemblyVersionAttribute("0.24.0")]
+[assembly: AssemblyFileVersionAttribute("0.24.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.23.1";
+        internal const string Version = "0.24.0";
     }
 }
--- src/Paket.Core/AssemblyInfo.fs
+++ src/Paket.Core/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Paket.Core")>]
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
-[<assembly: AssemblyVersionAttribute("0.23.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.23.1")>]
+[<assembly: AssemblyVersionAttribute("0.24.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.24.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.23.1"
+    let [<Literal>] Version = "0.24.0"
