8f02f50a77c27e2d0f94532b75363c250316d6b4
Andrew Jones <ascjones@gmail.com>
2015-02-17 17:24:44 +0000

    blacklisted Microsoft.Bcl.Build.targets

---
+++ src/Paket.Core/Blacklist.fs
@@ -0,0 +1,15 @@
+﻿namespace Paket
+
+type Blacklist = 
+    { TargetsFiles : string list
+      LibraryFiles : string list }
+
+    // default set of blacklisted references we don't want to install
+    static member TheBlacklist = 
+        { TargetsFiles = 
+            [
+                "Microsoft.Bcl.Build.targets" // would install a targets file causing the build to fail in VS if no packages.config files
+            ] 
+          LibraryFiles = [] }
+
+
