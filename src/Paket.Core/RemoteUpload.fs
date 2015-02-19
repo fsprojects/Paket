1b37443320902dbb0abc915e9f21a72c5f626d9c
mavnn <michael@mavnn.co.uk>
2015-02-19 11:04:29 +0000

    Add Push method

---
+++ src/Paket.Core/RemoteUpload.fs
@@ -0,0 +1,18 @@
+﻿module Paket.RemoteUpload
+
+open System
+open System.IO
+open Paket
+open Paket.Logging
+
+let Push url apiKey package =
+    verbosefn "Pushing package %s to %s" package url
+    let client = Utils.createWebClient(url, None)
+    client.Headers.Add("X-NuGet-ApiKey", apiKey)
+    client.UploadFile(Uri(url + "/api/v2/package"), "PUT", package)
+    |> ignore
+    verbosefn "Pushing %s complete." package
+
+let PushAll root url apiKey =
+    Directory.GetFiles(root, "*.nupkg", SearchOption.AllDirectories)
+    |> Seq.iter (fun p -> Push url apiKey <| Path.Combine(root, p))
