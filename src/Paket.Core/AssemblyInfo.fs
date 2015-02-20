b0a3747304acdd14b863c2c79dd003f271e5a359
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-02-20 16:23:32 +0100

    Bump version to 0.30.0-alpha015

--- src/Paket.Core/AssemblyInfo.fs
+++ src/Paket.Core/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
 [<assembly: AssemblyVersionAttribute("0.30.0")>]
 [<assembly: AssemblyFileVersionAttribute("0.30.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.30.0-alpha014")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.30.0-alpha015")>]
 do ()
 
 module internal AssemblyVersionInformation =
