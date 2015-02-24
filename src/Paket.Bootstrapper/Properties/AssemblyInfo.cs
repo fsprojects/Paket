ebae7dd1136cca3dd66e6a086d1b3c42023d0d63
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-02-24 11:53:56 +0100

    Bump version to 0.30.6

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("0.30.5")]
-[assembly: AssemblyFileVersionAttribute("0.30.5")]
-[assembly: AssemblyInformationalVersionAttribute("0.30.5")]
+[assembly: AssemblyVersionAttribute("0.30.6")]
+[assembly: AssemblyFileVersionAttribute("0.30.6")]
+[assembly: AssemblyInformationalVersionAttribute("0.30.6")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.30.5";
+        internal const string Version = "0.30.6";
     }
 }
