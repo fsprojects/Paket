e154bdef59c21afe8e8dc8d4d7e0f20ee2021e44
Vidar L. Sømme <vidar@vetserve.no>
2015-02-24 20:48:57 +0100

    Uploading packages as multiform content type

--- src/Paket.Core/RemoteUpload.fs
+++ src/Paket.Core/RemoteUpload.fs
@@ -1,22 +1,43 @@
 ﻿module Paket.RemoteUpload
 
 open System
+open System.Globalization
 open System.IO
+open System.Net
+open System.Text
 open Paket
 open Paket.Logging
 
+type System.Net.WebClient with
+        member x.UploadFileAsMultipart (url:Uri) filename =
+            let fileTemplate = "--{0}\r\nContent-Disposition: form-data; name=\"{1}\"; filename=\"{2}\"\r\nContent-Type: {3}\r\n\r\n"
+            let boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x", CultureInfo.InvariantCulture)
+            let fileInfo = (new FileInfo(Path.GetFullPath(filename)))
+            let fileHeaderBytes = String.Format(CultureInfo.InvariantCulture, fileTemplate, boundary, "package", "package", "application/octet-stream")
+                                  |> Encoding.UTF8.GetBytes
+            let newlineBytes = Environment.NewLine |> Encoding.UTF8.GetBytes
+            let trailerbytes = String.Format(CultureInfo.InvariantCulture, "--{0}--", boundary) |> Encoding.UTF8.GetBytes
+            x.Headers.Add(HttpRequestHeader.ContentType, "multipart/form-data; boundary=" + boundary);
+            use stream = x.OpenWrite(url, "PUT")
+            stream.Write(fileHeaderBytes,0,fileHeaderBytes.Length)
+            use fileStream = File.OpenRead fileInfo.FullName
+            fileStream.CopyTo(stream, (4*1024))
+            stream.Write(newlineBytes, 0, newlineBytes.Length)
+            stream.Write(trailerbytes, 0, trailerbytes.Length)
+            ()
+            
 let Push maxTrials url apiKey packageFileName =
     let rec push trial =
         tracefn "Pushing package %s to %s - trial %d" packageFileName url trial
         try
             let client = Utils.createWebClient(url, None)
             client.Headers.Add("X-NuGet-ApiKey", apiKey)
-            client.UploadFile(Uri(url + "/api/v2/package"), "PUT", packageFileName)
+            client.UploadFileAsMultipart (new Uri(url)) packageFileName
             |> ignore
             tracefn "Pushing %s complete." packageFileName
         with
         | exn when trial < maxTrials ->             
             traceWarnfn "Could not push %s: %s" packageFileName exn.Message            
             push (trial + 1)
-        
+
     push 1
