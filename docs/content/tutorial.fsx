2540a1274ffff82fb405f212e5de36f0b2b19bd5
Steffen Forkmann <sforkmann@gmail.com>
2015-02-27 12:13:45 +0100

    Update tutorial.fsx

--- docs/content/tutorial.fsx
+++ docs/content/tutorial.fsx
@@ -10,8 +10,8 @@ Introducing your project
 Say more
 
 *)
-#r "FSharp.ProjectTemplate.dll"
-open FSharp.ProjectTemplate
+#r "FSharp.ProjectScaffold.dll"
+open FSharp.ProjectScaffold
 
 Library.hello 0
 (**
