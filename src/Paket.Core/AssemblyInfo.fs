a22ce1fa17b41728ff23847962206ddf339ef5a7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-03-09 19:36:22 +0100

    Bump version to 0.32.0

--- src/Paket.Core/AssemblyInfo.fs
+++ src/Paket.Core/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
-[<assembly: AssemblyVersionAttribute("0.31.13")>]
-[<assembly: AssemblyFileVersionAttribute("0.31.13")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.31.13")>]
+[<assembly: AssemblyVersionAttribute("0.32.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.32.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.32.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.31.13"
+    let [<Literal>] Version = "0.32.0"
