0baab5b029542bbe8b4beb7fbaaff0ef9b70e002
Christian Fiebrig <fiebrig@freenet.de>
2015-04-13 22:52:33 +0200

    fix some unit tests to be applicable on unixoid systems

--- tests/Paket.Tests/UtilsSpecs.fs
+++ tests/Paket.Tests/UtilsSpecs.fs
@@ -27,7 +27,9 @@ let ``relative local path is returned as is``() =
 let ``absolute path with drive letter``() =
     "c:\\Store" 
     |> normalizeLocalPath
-    |> shouldEqual (AbsolutePath "c:\\Store")
+    |> match System.Environment.OSVersion.Platform with
+        | System.PlatformID.Win32NT -> shouldEqual (AbsolutePath "c:\\Store")
+        | _ -> shouldEqual (RelativePath "c:\\Store")
     
 [<Test>]
 let ``relative path with drive letter``() =
@@ -36,7 +38,28 @@ let ``relative path with drive letter``() =
     |> shouldEqual (RelativePath "..\\Store")
     
 [<Test>]
+let ``relative path with local identifier``() =
+    ".\\Store" 
+    |> normalizeLocalPath
+    |> shouldEqual (RelativePath ".\\Store")
+
+[<Test>]
 let ``SMB path is returned as absolute path``() =
     "\\\\server\\Store" 
     |> normalizeLocalPath
-    |> shouldEqual (AbsolutePath "\\\\server\\Store")
+    |> match System.Environment.OSVersion.Platform with
+        | System.PlatformID.Win32NT | System.PlatformID.Win32S -> shouldEqual (AbsolutePath "\\\\server\\Store")
+        | _ -> shouldEqual (RelativePath "\\\\server\\Store")
+    
+[<Test>]
+let ``absolute path on unixoid systems``() =
+    "/server/Store" 
+    |> normalizeLocalPath
+    |> shouldEqual (AbsolutePath "/server/Store")
+    
+[<Test>]
+let ``relative path with local identifier on unxoid systems``() =
+    "./Store" 
+    |> normalizeLocalPath
+    |> shouldEqual (RelativePath "./Store")
+    
