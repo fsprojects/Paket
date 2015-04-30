57ea25dcca2b209259d8b8139d92fbaacf28b5d8
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-04-30 22:33:33 +0200

    install fscheck to test project

--- tests/Paket.Tests/DependenciesFile/PropertyTests.fs
+++ tests/Paket.Tests/DependenciesFile/PropertyTests.fs
@@ -1,2 +1,14 @@
 ﻿module Paket.DependenciesFile.PropertyTests
 
+open System
+open FsCheck
+open NUnit.Framework
+open Paket
+
+let ``round trip`` (lines : string[]) =
+    let df = DependenciesFile(DependenciesFileParser.parseDependenciesFile "dummy" lines)
+    df.ToString() = String.concat Environment.NewLine lines 
+
+[<Test>]
+let ``round trip test``() =
+    Check.QuickThrowOnFailure(``round trip``)
