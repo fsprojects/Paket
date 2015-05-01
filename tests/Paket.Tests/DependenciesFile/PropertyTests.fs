5c52142885ad8bd429055798d96c3ec856feaacb
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-01 10:10:37 +0200

    vanilla ice test

--- tests/Paket.Tests/DependenciesFile/PropertyTests.fs
+++ tests/Paket.Tests/DependenciesFile/PropertyTests.fs
@@ -2,13 +2,16 @@
 
 open System
 open FsCheck
-open NUnit.Framework
+open FsCheck.NUnit
 open Paket
 
+type DFFileGenerator =
+    static member StringArray() = 
+        {new Arbitrary<string[]>() with
+            override x.Generator = Gen.elements [[|"nuget FsCheck"|]]
+            override x.Shrinker t = Seq.empty }
+
+[<Property(Arbitrary = [|typeof<DFFileGenerator>|])>]
 let ``round trip`` (lines : string[]) =
     let df = DependenciesFile(DependenciesFileParser.parseDependenciesFile "dummy" lines)
     df.ToString() = String.concat Environment.NewLine lines 
-
-[<Test>]
-let ``round trip test``() =
-    Check.QuickThrowOnFailure(``round trip``)
