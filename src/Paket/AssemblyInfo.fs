058ac97d8695b1ea209a299a8dcc2d81ead4612d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-05-06 10:54:03 +0200

    Bump version to 1.3.22

--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
-[<assembly: AssemblyVersionAttribute("1.3.21")>]
-[<assembly: AssemblyFileVersionAttribute("1.3.21")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.3.21")>]
+[<assembly: AssemblyVersionAttribute("1.3.22")>]
+[<assembly: AssemblyFileVersionAttribute("1.3.22")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.3.22")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.3.21"
+    let [<Literal>] Version = "1.3.22"
