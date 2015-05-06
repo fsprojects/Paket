709920a3c6b3022ddb0edb790b08a2ed3aecb291
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-05-06 18:18:16 +0200

    Bump version to 1.3.28

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("1.3.27")]
-[assembly: AssemblyFileVersionAttribute("1.3.27")]
-[assembly: AssemblyInformationalVersionAttribute("1.3.27")]
+[assembly: AssemblyVersionAttribute("1.3.28")]
+[assembly: AssemblyFileVersionAttribute("1.3.28")]
+[assembly: AssemblyInformationalVersionAttribute("1.3.28")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.3.27";
+        internal const string Version = "1.3.28";
     }
 }
