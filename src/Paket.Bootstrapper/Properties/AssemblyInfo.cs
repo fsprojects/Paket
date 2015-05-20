df641ce9140c0a9e91be1686a8ff91df4528f228
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-05-20 08:04:25 +0200

    Bump version to 1.4.16

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("1.4.15")]
-[assembly: AssemblyFileVersionAttribute("1.4.15")]
-[assembly: AssemblyInformationalVersionAttribute("1.4.15")]
+[assembly: AssemblyVersionAttribute("1.4.16")]
+[assembly: AssemblyFileVersionAttribute("1.4.16")]
+[assembly: AssemblyInformationalVersionAttribute("1.4.16")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.4.15";
+        internal const string Version = "1.4.16";
     }
 }
