f4958e9d8b5a9a503583173b81c07e54d47454b2
cr7pt0gr4ph7 <cr7pt0gr4ph7@gmail.com>
2015-05-27 21:38:30 +0200

    Other formatting fixes for UpdateProcess.

--- src/Paket.Core/UpdateProcess.fs
+++ src/Paket.Core/UpdateProcess.fs
@@ -9,20 +9,20 @@ open System.Collections.Generic
 open Chessie.ErrorHandling
 open Paket.Logging
 
-let addPackagesFromReferenceFiles projects (dependenciesFile:DependenciesFile) =
+let addPackagesFromReferenceFiles projects (dependenciesFile : DependenciesFile) =
     let lockFileName = DependenciesFile.FindLockfile dependenciesFile.FileName
     let oldLockFile =
-        if lockFileName.Exists then 
+        if lockFileName.Exists then
             LockFile.LoadFrom(lockFileName.FullName)
         else
             LockFile.Create(lockFileName.FullName, dependenciesFile.Options, Resolution.Ok(Map.empty), [])
-            
+
     let allExistingPackages =
         oldLockFile.ResolvedPackages
         |> Seq.map (fun d -> NormalizedPackageName d.Value.Name)
         |> Set.ofSeq
 
-    let allReferencedPackages = 
+    let allReferencedPackages =
         projects
         |> Seq.collect (fun (_,referencesFile) -> referencesFile.NugetPackages)
 
@@ -33,24 +33,24 @@ let addPackagesFromReferenceFiles projects (dependenciesFile:DependenciesFile) =
             |> allExistingPackages.Contains
             |> not)
 
-    if Seq.isEmpty diff then 
+    if Seq.isEmpty diff then
         dependenciesFile
     else
         let newDependenciesFile =
             diff
-            |> Seq.fold (fun (dependenciesFile:DependenciesFile) dep -> 
-                if dependenciesFile.HasPackage dep.Name then 
+            |> Seq.fold (fun (dependenciesFile:DependenciesFile) dep ->
+                if dependenciesFile.HasPackage dep.Name then
                     dependenciesFile
                 else
                     dependenciesFile.AddAdditionalPackage(dep.Name,"",dep.Settings)) dependenciesFile
         newDependenciesFile.Save()
         newDependenciesFile
 
-let SelectiveUpdate(dependenciesFile:DependenciesFile, exclude, force) =
+let SelectiveUpdate(dependenciesFile : DependenciesFile, exclude, force) =
     let lockFileName = DependenciesFile.FindLockfile dependenciesFile.FileName
 
     let resolution =
-        if not lockFileName.Exists then 
+        if not lockFileName.Exists then
             dependenciesFile.Resolve(force)
         else
             let oldLockFile = LockFile.LoadFrom(lockFileName.FullName)
