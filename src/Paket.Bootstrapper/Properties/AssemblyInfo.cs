cbb2c6d3f01367442268712709f6b7b3200cffd5
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-06-09 15:07:22 +0200

    Bump version to 1.11.9

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("1.11.8")]
-[assembly: AssemblyFileVersionAttribute("1.11.8")]
-[assembly: AssemblyInformationalVersionAttribute("1.11.8")]
+[assembly: AssemblyVersionAttribute("1.11.9")]
+[assembly: AssemblyFileVersionAttribute("1.11.9")]
+[assembly: AssemblyInformationalVersionAttribute("1.11.9")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.11.8";
+        internal const string Version = "1.11.9";
     }
 }
