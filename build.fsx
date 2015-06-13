3cfb74d3505d7bd04cb710b54d0eb63996b1cfff
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-06-13 09:49:56 +0200

    Revert "Upgrade ILRepack"
    This reverts commit e594ea52cbc3d52ecc4fa6a41155093265d5a9a5.

--- build.fsx
+++ build.fsx
@@ -174,7 +174,7 @@ Target "MergePaketTool" (fun _ ->
     let result =
         ExecProcess (fun info ->
             info.FileName <- currentDirectory @@ "packages" @@ "ILRepack" @@ "tools" @@ "ILRepack.exe"
-            info.Arguments <- sprintf "/verbose /lib:%s /out:%s %s" buildDir (buildMergedDir @@ "paket.exe") toPack
+            info.Arguments <- sprintf "/verbose /lib:%s /ver:%s /out:%s %s" buildDir release.AssemblyVersion (buildMergedDir @@ "paket.exe") toPack
             ) (TimeSpan.FromMinutes 5.)
 
     if result <> 0 then failwithf "Error during ILRepack execution."
