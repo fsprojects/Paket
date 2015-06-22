23d2755cbb751a1119048ce1b144f2e1cc765c45
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-06-22 08:48:38 +0200

    Find-package-versions did not work - fixes #886

--- src/Paket.Core/NuGetV3.fs
+++ src/Paket.Core/NuGetV3.fs
@@ -65,12 +65,15 @@ let FindVersionsForPackage(auth, nugetURL, package, maxResults) =
         | Some url ->
             let! response = safeGetFromUrl(auth,sprintf "%s?id=%s&take=%d" url package 100000) // Nuget is showing old versions first
             match response with
-            | Some text -> 
-                let r = extractVersions text
-                if r.Length > maxResults then
-                    return r |> Seq.take maxResults |> Seq.toArray
-                else
-                    return r
+            | Some text ->
+                let versions =
+                    let extracted = extractVersions text
+                    if extracted.Length > maxResults then
+                        extracted |> Seq.take maxResults |> Seq.toArray
+                    else
+                        extracted
+
+                return SemVer.SortVersions versions
             | None -> return [||]
         | None -> return [||]
     }
