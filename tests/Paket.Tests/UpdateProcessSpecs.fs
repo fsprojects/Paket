560406417c31f34b15d3892b1bb9ef973f7872a2
Marcio Rinaldi <me@marciorinaldi.com>
2015-08-05 11:14:13 -0300

    Makes sure transitive dependencies of another package are constrained, and are only updated to the version within the allowed range

--- tests/Paket.Tests/UpdateProcessSpecs.fs
+++ tests/Paket.Tests/UpdateProcessSpecs.fs
@@ -583,3 +583,67 @@ let ``SelectiveUpdate updates package that conflicts with a transitive dependenc
     |> Seq.sortBy fst
     |> shouldEqual expected
     
+
+let graph3 = 
+    graph2 @
+    [ "Ninject.Extensions.Interception", "2.2.1.2", [ "Ninject", VersionRequirement(VersionRange.Between("2.2.0.0","2.3.0.0"),PreReleaseStatus.No) ]
+      "Ninject.Extensions.Interception", "2.2.1.3", [ "Ninject", VersionRequirement(VersionRange.Between("2.2.0.0","2.3.0.0"),PreReleaseStatus.No) ]
+      "Ninject.Extensions.Interception", "3.2.0", [ "Ninject", VersionRequirement(VersionRange.Between("3.2.0.0","3.3.0.0"),PreReleaseStatus.No) ] ]
+      
+let lockFileData3 = """NUGET
+  remote: http://nuget.org/api/v2
+  specs:
+    log4f (0.4.0)
+      log4net (>= 1.2.10 < 2.0.0)
+    log4net (1.0.4)
+    Ninject (2.2.1.4)
+    Ninject.Extensions.Logging (2.2.0.4)
+      Ninject (>= 2.2.0.0 < 2.3.0.0)
+    Ninject.Extensions.Logging.Log4net (2.2.0.4)
+      Ninject.Extensions.Logging (>= 2.2.0.0 < 2.3.0.0)
+      log4net (>= 1.0.4)
+    Ninject.Extensions.Interception (2.2.1.2)
+      Ninject (>= 2.2.0.0 < 2.3.0.0)
+"""
+let lockFile3 = lockFileData3 |> getLockFile
+
+[<Test>]
+let ``SelectiveUpdate updates package that conflicts with a transitive dependency of another package with correct version``() = 
+
+    let dependenciesFile = DependenciesFile.FromCode("""source http://nuget.org/api/v2
+
+    nuget log4f
+    nuget Ninject.Extensions.Logging.Log4net
+    nuget Ninject.Extensions.Interception""")
+    
+    let updateAll = false
+    let packageName = NormalizedPackageName(PackageName "Ninject.Extensions.Logging.Log4net")
+    let requirements =
+        lockFile3.ResolvedPackages
+        |> createPackageRequirements [packageName]
+        |> List.ofSeq
+    let resolve = resolve' graph3 requirements
+
+    let lockFile = 
+        Some(packageName)
+        |> selectiveUpdate resolve lockFile3 dependenciesFile updateAll
+    
+    let result = 
+        lockFile.ResolvedPackages
+        |> Map.toSeq
+        |> Seq.map snd
+        |> Seq.map (fun r -> (string r.Name, string r.Version))
+
+    let expected = 
+        [("Ninject.Extensions.Logging.Log4net","2.2.0.5");
+        ("Ninject.Extensions.Logging","2.2.0.5");
+        ("Ninject.Extensions.Interception","2.2.1.2");
+        ("Ninject", "2.2.1.5");
+        ("log4f", "0.4.0");
+        ("log4net", "1.2.11")]
+        |> Seq.sortBy fst
+
+    result
+    |> Seq.sortBy fst
+    |> shouldEqual expected
+    
