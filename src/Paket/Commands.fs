d02c7c4eef0fba6a256c4d713376f070b278972d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-08-24 11:48:41 +0200

    small clarification in docs

--- src/Paket/Commands.fs
+++ src/Paket/Commands.fs
@@ -258,7 +258,7 @@ with
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | Output(_) -> "Output directory to put nupkgs."
+            | Output(_) -> "Output directory to put .nupkg files."
             | BuildConfig(_) -> "Optionally specify build configuration that should be packaged (defaults to Release)."
             | Version(_) -> "Specify version of the package."
             | TemplateFile(_) -> "Allows to specify a single template file."
