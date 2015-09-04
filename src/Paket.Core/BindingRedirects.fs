4a71a35a7a9bf94ce2f1392c629e7259caca6cbc
Isaac Abraham <isaac.abraham@gmail.com>
2015-09-04 18:32:21 +0200

    Fixes #808.

--- src/Paket.Core/BindingRedirects.fs
+++ src/Paket.Core/BindingRedirects.fs
@@ -82,10 +82,12 @@ let private applyBindingRedirects bindingRedirects (configFilePath:string) =
     config.Save configFilePath
 
 let private configFiles = [ "app"; "web" ] |> Set.ofList
+let private projectFiles = [ ".csproj"; ".vbproj"; ".fsproj" ] |> Set.ofList
 let private toLower (s:string) = s.ToLower()
 let private isAppOrWebConfig = configFiles.Contains << (Path.GetFileNameWithoutExtension >> toLower)
+let private isDotNetProject = projectFiles.Contains << (Path.GetExtension >> toLower)
 let internal getFoldersWithPaketReferencesAndNoConfig getFiles rootPath  =
-    getFiles(rootPath, "paket.references", SearchOption.AllDirectories)
+    getFiles(rootPath, Constants.ReferencesFile, SearchOption.AllDirectories)
     |> Seq.map Path.GetDirectoryName
     |> Seq.filter(fun directory -> getFiles(directory, "*.config", SearchOption.TopDirectoryOnly) |> Seq.forall (not << isAppOrWebConfig))
     |> Seq.toList
@@ -96,14 +98,33 @@ let private baseConfig = """<?xml version="1.0" encoding="utf-8"?>
 <configuration>
 </configuration>
 """
-let private createAppConfigFile folder = File.WriteAllText(Path.Combine(folder, "app.config"), baseConfig)
+let private createAppConfigInDirectory folder =
+    File.WriteAllText(Path.Combine(folder, "app.config"), baseConfig)
+    folder
+let private getProjectFilesInDirectory folder =
+    Directory.GetFiles(folder, "*proj")
+    |> Seq.filter (Path.GetExtension >> isDotNetProject)
+let private addConfigFileToProject projectFile =
+    ProjectFile.Load projectFile
+    |> Option.bind(fun project ->
+        project.ProjectNode
+        |> Xml.getNodes "ItemGroup"
+        |> List.tryHead
+        |> Option.map(fun g -> project, g))
+    |> Option.iter(fun (project, itemGroup) ->
+        project.CreateNode "Content"
+        |> Xml.addAttribute "Include" "app.config"
+        |> itemGroup.AppendChild
+        |> ignore
+        project.Save())
 
 /// Applies a set of binding redirects to all .config files in a specific folder.
 let applyBindingRedirectsToFolder rootPath bindingRedirects =
     // First create missing configuration files.
     rootPath
     |> getFoldersWithPaketReferencesAndNoConfig Directory.GetFiles
-    |> Seq.iter createAppConfigFile
+    |> Seq.collect (createAppConfigInDirectory >> getProjectFilesInDirectory)
+    |> Seq.iter addConfigFileToProject
 
     // Now ensure all configuration files have binding redirects.
     rootPath
