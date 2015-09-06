196423e570ff93a30a24179c2237d0b96ccf3edc
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-09-06 15:07:10 +0200

    Bump version to 2.0.0-beta005

--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
 [<assembly: AssemblyVersionAttribute("2.0.0")>]
 [<assembly: AssemblyFileVersionAttribute("2.0.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.0.0-beta004")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.0.0-beta005")>]
 do ()
 
 module internal AssemblyVersionInformation =
