4763098a0a6b33b0ceb3efbf9717a36323b06a99
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-09-13 10:39:20 +0200

    Bump version to 1.39.9

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("1.39.8")]
-[assembly: AssemblyFileVersionAttribute("1.39.8")]
-[assembly: AssemblyInformationalVersionAttribute("1.39.8")]
+[assembly: AssemblyVersionAttribute("1.39.9")]
+[assembly: AssemblyFileVersionAttribute("1.39.9")]
+[assembly: AssemblyInformationalVersionAttribute("1.39.9")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.39.8";
+        internal const string Version = "1.39.9";
     }
 }
--- src/Paket.PowerShell/AssemblyInfo.fs
+++ src/Paket.PowerShell/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
-[<assembly: AssemblyVersionAttribute("1.39.8")>]
-[<assembly: AssemblyFileVersionAttribute("1.39.8")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.39.8")>]
+[<assembly: AssemblyVersionAttribute("1.39.9")>]
+[<assembly: AssemblyFileVersionAttribute("1.39.9")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.39.9")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.39.8"
+    let [<Literal>] Version = "1.39.9"
