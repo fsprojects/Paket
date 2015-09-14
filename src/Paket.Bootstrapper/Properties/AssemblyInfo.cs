a68d7e07331725e346d67fa123012da20612e10c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-09-14 16:39:59 +0200

    Bump version to 2.0.0-rc007

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
 [assembly: AssemblyVersionAttribute("2.0.0")]
 [assembly: AssemblyFileVersionAttribute("2.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.0.0-rc006")]
+[assembly: AssemblyInformationalVersionAttribute("2.0.0-rc007")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "2.0.0";
