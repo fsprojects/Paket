37b629c000c2126e66224b48755c4581afe67453
Mauricio Scheffer <mauricioscheffer@gmail.com>
2015-09-21 12:41:29 +0100

    Fix description for update group

--- src/Paket/Commands.fs
+++ src/Paket/Commands.fs
@@ -213,7 +213,7 @@ with
         member this.Usage =
             match this with
             | Nuget(_) -> "NuGet package id."
-            | Group(_) -> "Allows to specify the group the group."
+            | Group(_) -> "Allows to specify the dependency group."
             | Version(_) -> "Allows to specify version of the package."
             | Force -> "Forces the download and reinstallation of all packages."
             | Hard -> "Replaces package references within project files even if they are not yet adhering to the Paket's conventions (and hence considered manually managed)."
