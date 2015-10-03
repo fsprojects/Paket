3fd088ff971883f8ecf597b9f7b9cb6aa2f780d2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-10-03 19:45:33 +0200

    Simplify outdated

--- src/Paket.Core/FindOutdated.fs
+++ src/Paket.Core/FindOutdated.fs
@@ -41,7 +41,7 @@ let FindOutdated strict includingPrereleases environment = trial {
 
     let groups = 
         dependenciesFile.Groups
-        |> Map.map (fun groupName group -> dependenciesFile.Groups.[groupName].Packages)
+        |> Map.map (fun groupName group -> [])
 
     let newResolution = dependenciesFile.Resolve(true,getSha1,(fun (x,y,_) -> NuGetV2.GetVersions root (x,y)),NuGetV2.GetPackageDetails root true,groups)
 
