e08a3a68ac0431882aee063cca66ff7c4f88b482
Julien Roncaglia <julien@roncaglia.fr>
2015-10-11 14:16:21 +0200

    Add a test proving issue with '>= net40' and silverlight
    Issue #1124

---
+++ tests/Paket.Tests/RestrictionApplicationSpecs.fs
@@ -0,0 +1,19 @@
+﻿module Packet.RestrictionApplicationSpecs
+
+open System.IO
+open Paket
+open Paket.Domain
+open Chessie.ErrorHandling
+open FsUnit
+open NUnit.Framework
+open TestHelpers
+open Paket.Requirements
+
+[<Test>]
+let ``>= net40 does not include silverlight (#1124)`` () =
+    /// https://github.com/fsprojects/Paket/issues/1124
+    let restrictions = [FrameworkRestriction.AtLeast(DotNetFramework(FrameworkVersion.V4))]
+    let targets = KnownTargetProfiles.DotNetFrameworkProfiles @ KnownTargetProfiles.SilverlightProfiles
+    let restricted = applyRestrictionsToTargets restrictions targets
+    
+    restricted |> shouldEqual KnownTargetProfiles.DotNetFrameworkProfiles
