4313cda7f26cbe9fea8d3dda747730a1e217dba2
Marcio Rinaldi <me@marciorinaldi.com>
2015-10-14 13:24:40 -0300

    Combining Between with AtLeast on lower limit

--- tests/Paket.Tests/RestrictionFilterSpecs.fs
+++ tests/Paket.Tests/RestrictionFilterSpecs.fs
@@ -89,3 +89,19 @@ let ``should filter >= net40 < net45 and net45``() =
     let l2 = [FrameworkRestriction.Exactly(DotNetFramework(FrameworkVersion.V4_5))]
     filterRestrictions l1 l2
     |> shouldEqual []
+
+[<Test>]
+let ``should filter >= net20 < net46 and >= net40``() = 
+    let l1 = [FrameworkRestriction.Between(DotNetFramework(FrameworkVersion.V2),DotNetFramework(FrameworkVersion.V4_6))]
+    let l2 = [FrameworkRestriction.AtLeast(DotNetFramework(FrameworkVersion.V4))]
+
+    filterRestrictions l1 l2
+    |> shouldEqual [FrameworkRestriction.Between(DotNetFramework(FrameworkVersion.V4),DotNetFramework(FrameworkVersion.V4_6))]
+
+[<Test>]
+let ``should filter >= net45 < net46 and >= net40``() =
+    let l1 = [FrameworkRestriction.Between(DotNetFramework(FrameworkVersion.V4_5),DotNetFramework(FrameworkVersion.V4_6))]
+    let l2 = [FrameworkRestriction.AtLeast(DotNetFramework(FrameworkVersion.V4))]
+
+    filterRestrictions l1 l2
+    |> shouldEqual l1
