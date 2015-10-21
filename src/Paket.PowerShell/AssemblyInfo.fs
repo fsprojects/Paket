7c6748d51fec1ef8f1d1764489e138b6f1bb4d99
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-10-21 12:15:44 +0200

    Bump version to 2.16.0-alpha004

--- src/Paket.PowerShell/AssemblyInfo.fs
+++ src/Paket.PowerShell/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
 [<assembly: AssemblyVersionAttribute("2.16.0")>]
 [<assembly: AssemblyFileVersionAttribute("2.16.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.16.0-alpha003")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.16.0-alpha004")>]
 do ()
 
 module internal AssemblyVersionInformation =
