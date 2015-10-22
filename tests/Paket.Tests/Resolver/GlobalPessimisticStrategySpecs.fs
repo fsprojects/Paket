5b504be39bb238fe09c7ef8e5f83f2550a919f8c
Marcio Rinaldi <me@marciorinaldi.com>
2015-10-22 10:37:09 -0200

    Cleanup

--- tests/Paket.Tests/Resolver/GlobalPessimisticStrategySpecs.fs
+++ tests/Paket.Tests/Resolver/GlobalPessimisticStrategySpecs.fs
@@ -7,6 +7,10 @@ open TestHelpers
 open Paket.Domain
 open Paket.PackageResolver
 
+let resolve graph updateMode (cfg : DependenciesFile) =
+    let groups = [Constants.MainDependencyGroup, None ] |> Map.ofSeq
+    cfg.Resolve(true,noSha1,VersionsFromGraphAsSeq graph,PackageDetailsFromGraph graph,groups,updateMode).[Constants.MainDependencyGroup].ResolvedPackages.GetModelOrFail()
+
 let graph = [
     "Nancy.Bootstrappers.Windsor","0.23",["Castle.Windsor",VersionRequirement(VersionRange.AtLeast "3.2.1",PreReleaseStatus.No)]
     "Castle.Windsor","3.2.1",[]
@@ -22,8 +26,9 @@ nuget Nancy.Bootstrappers.Windsor ~> 0.23
 
 [<Test>]
 let ``should resolve simple config1``() = 
-    let cfg = DependenciesFile.FromCode(config1)
-    let resolved = ResolveWithGraph(cfg,noSha1,VersionsFromGraphAsSeq graph, PackageDetailsFromGraph graph).[Constants.MainDependencyGroup].ResolvedPackages.GetModelOrFail()
+    let resolved =
+        DependenciesFile.FromCode(config1)
+        |> resolve graph UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Castle.Windsor"] |> shouldEqual "3.2.1"
     getVersion resolved.[PackageName "Nancy.Bootstrappers.Windsor"] |> shouldEqual "0.23"
 
@@ -37,8 +42,9 @@ nuget Nancy.Bootstrappers.Windsor ~> 0.23
 
 [<Test>]
 let ``should resolve simple config2``() = 
-    let cfg = DependenciesFile.FromCode(config2)
-    let resolved = ResolveWithGraph(cfg,noSha1,VersionsFromGraphAsSeq graph, PackageDetailsFromGraph graph).[Constants.MainDependencyGroup].ResolvedPackages.GetModelOrFail()
+    let resolved =
+        DependenciesFile.FromCode(config2)
+        |> resolve graph UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Castle.Windsor"] |> shouldEqual "3.3.0"
     getVersion resolved.[PackageName "Nancy.Bootstrappers.Windsor"] |> shouldEqual "0.23"
 
@@ -53,8 +59,9 @@ nuget Castle.Windsor
 
 [<Test>]
 let ``should resolve simple config3``() = 
-    let cfg = DependenciesFile.FromCode(config3)
-    let resolved = ResolveWithGraph(cfg,noSha1,VersionsFromGraphAsSeq graph, PackageDetailsFromGraph graph).[Constants.MainDependencyGroup].ResolvedPackages.GetModelOrFail()
+    let resolved =
+        DependenciesFile.FromCode(config3)
+        |> resolve graph UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Castle.Windsor"] |> shouldEqual "3.3.0"
     getVersion resolved.[PackageName "Nancy.Bootstrappers.Windsor"] |> shouldEqual "0.23"
 
@@ -86,8 +93,9 @@ nuget Nancy.Bootstrappers.Windsor ~> 0.23
 
 [<Test>]
 let ``should resolve simple config4``() = 
-    let cfg = DependenciesFile.FromCode(config4)
-    let resolved = ResolveWithGraph(cfg,noSha1,VersionsFromGraphAsSeq graph2, PackageDetailsFromGraph graph2).[Constants.MainDependencyGroup].ResolvedPackages.GetModelOrFail()
+    let resolved =
+        DependenciesFile.FromCode(config4)
+        |> resolve graph2 UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Castle.Windsor"] |> shouldEqual "3.2.1"
     getVersion resolved.[PackageName "Nancy.Bootstrappers.Windsor"] |> shouldEqual "0.23"
 
@@ -100,8 +108,9 @@ nuget Nancy.Bootstrappers.Windsor @~> 0.23
 
 [<Test>]
 let ``should override global strategy``() = 
-    let cfg = DependenciesFile.FromCode(config5)
-    let resolved = ResolveWithGraph(cfg,noSha1,VersionsFromGraphAsSeq graph, PackageDetailsFromGraph graph).[Constants.MainDependencyGroup].ResolvedPackages.GetModelOrFail()
+    let resolved =
+        DependenciesFile.FromCode(config5)
+        |> resolve graph2 UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Castle.Windsor"] |> shouldEqual "3.3.0"
     getVersion resolved.[PackageName "Nancy.Bootstrappers.Windsor"] |> shouldEqual "0.23"
 
@@ -113,15 +122,11 @@ nuget Castle.Core-NLog @~> 3.2
 nuget Castle.Windsor-NLog !~> 3.2
 """
 
-let resolveWithUpdateMode graph updateMode (cfg : DependenciesFile) =
-    let groups = [Constants.MainDependencyGroup, None ] |> Map.ofSeq
-    cfg.Resolve(true,noSha1,VersionsFromGraphAsSeq graph,PackageDetailsFromGraph graph,groups,updateMode).[Constants.MainDependencyGroup].ResolvedPackages.GetModelOrFail()
-
 [<Test>]
 let ``should update to lastest when updating all``() = 
     let resolved =
         DependenciesFile.FromCode(config6)
-        |> resolveWithUpdateMode graph2 UpdateMode.UpdateAll
+        |> resolve graph2 UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Castle.Windsor-NLog"] |> shouldEqual "3.3.0"
     getVersion resolved.[PackageName "Castle.Core-NLog"] |> shouldEqual "3.3.1"
     getVersion resolved.[PackageName "Castle.Core"] |> shouldEqual "3.3.1"
@@ -130,7 +135,7 @@ let ``should update to lastest when updating all``() =
 let ``should respect overrides when updating single package``() = 
     let resolved =
         DependenciesFile.FromCode(config6)
-        |> resolveWithUpdateMode graph2 (UpdateMode.UpdatePackage(Constants.MainDependencyGroup, PackageName "Castle.Windsor-NLog"))
+        |> resolve graph2 (UpdateMode.UpdatePackage(Constants.MainDependencyGroup, PackageName "Castle.Windsor-NLog"))
     getVersion resolved.[PackageName "Castle.Windsor-NLog"] |> shouldEqual "3.3.0"
     getVersion resolved.[PackageName "Castle.Core-NLog"] |> shouldEqual "3.3.0"
     getVersion resolved.[PackageName "Castle.Core"] |> shouldEqual "3.3.1"
