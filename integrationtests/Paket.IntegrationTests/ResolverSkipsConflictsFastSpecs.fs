e858f92b50501a30bdb6342899387aaf4272a8f8
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-10-29 16:41:08 +0100

    Create integration test for sample with multiple feeds - references #1157

--- integrationtests/Paket.IntegrationTests/ResolverSkipsConflictsFastSpecs.fs
+++ integrationtests/Paket.IntegrationTests/ResolverSkipsConflictsFastSpecs.fs
@@ -12,5 +12,10 @@ let ``#1166 Should resolve Nancy without timeout``() =
     update "i001166-resolve-nancy-fast"
 
 [<Test>]
+let ``#1157 should resolve from multiple feeds``() = 
+    update "i001157-resolve-multiple-feeds"
+
+
+[<Test>]
 let ``#1174 Should find Ninject error``() = 
     updateShouldFindPackageConflict "Ninject" "i001174-resolve-fast-conflict"
