7d0a41d6c13ff13596d5a0b9b37e34d423df2397
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-10-29 19:15:33 +0100

    should ignore trailing zero during resolve - references #71

---
+++ integrationtests/Paket.IntegrationTests/BasicResolverSpecs.fs
@@ -0,0 +1,34 @@
+﻿module Paket.IntegrationTests.BasicResolverSpecs
+
+open Fake
+open Paket
+open System
+open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open Paket.Domain
+
+[<Test>]
+let ``#49 windsor should resolve correctly``() =
+    let lockFile = update "i000049-resolve-windsor-correctly"
+    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Castle.Windsor"].Version
+    |> shouldEqual (SemVer.Parse "3.2.1")
+
+[<Test>]
+let ``#51 should resolve with pessimistic strategy correctly``() =
+    let lockFile = update "i000051-resolve-with-pessimistic-strategy"
+    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Castle.Windsor-log4net"].Version
+    |> shouldEqual (SemVer.Parse "3.2.0.1")
+
+[<Test>]
+let ``#55 should resolve with pessimistic strategy correctly``() =
+    let lockFile = update "i000055-resolve-with-pessimistic-strategy"
+    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Castle.Windsor"].Version
+    |> shouldEqual (SemVer.Parse "3.2.1")
+
+[<Test>]
+let ``#71 should ignore trailing zero during resolve``() =
+    let lockFile = update "i000071-ignore-trailing-zero-during-resolve"
+    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Newtonsoft.Json"].Version
+    |> shouldEqual (SemVer.Parse "6.0.5.0")
--- integrationtests/Paket.IntegrationTests/ResolverSkipsConflictsFastSpecs.fs
+++ integrationtests/Paket.IntegrationTests/ResolverSkipsConflictsFastSpecs.fs
@@ -10,24 +10,6 @@ open System.IO
 open Paket.Domain
 
 [<Test>]
-let ``#49 windsor should resolve correctly``() =
-    let lockFile = update "i000049-resolve-windsor-correctly"
-    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Castle.Windsor"].Version
-    |> shouldEqual (SemVer.Parse "3.2.1")
-
-[<Test>]
-let ``#51 should resolve with pessimistic strategy correctly``() =
-    let lockFile = update "i000051-resolve-with-pessimistic-strategy"
-    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Castle.Windsor-log4net"].Version
-    |> shouldEqual (SemVer.Parse "3.2.0.1")
-
-[<Test>]
-let ``#55 should resolve with pessimistic strategy correctly``() =
-    let lockFile = update "i000055-resolve-with-pessimistic-strategy"
-    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Castle.Windsor"].Version
-    |> shouldEqual (SemVer.Parse "3.2.1")
-
-[<Test>]
 let ``#1166 Should resolve Nancy without timeout``() =
     let lockFile = update "i001166-resolve-nancy-fast"
     lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Nancy"].Version
