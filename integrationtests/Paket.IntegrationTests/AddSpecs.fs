73a1a26e25395fe688a306c886fce220486e0551
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-10-30 14:03:30 +0100

    Create regression tests for #310

---
+++ integrationtests/Paket.IntegrationTests/AddSpecs.fs
@@ -0,0 +1,19 @@
+﻿module Paket.IntegrationTests.AddSpecs
+
+open Fake
+open System
+open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open System.Diagnostics
+open Paket
+open Paket.Domain
+
+[<Test>]
+let ``#310 paket add nuget should not resolve inconsistent dependency graph``() = 
+    try
+        paket "add nuget Castle.Windsor version 3.3.0" "i000310-add-should-not-create-invalid-resolution" |> ignore
+        failwith "resolver error expected"
+    with
+    | exn when exn.Message.Contains("There was a version conflict during package resolution") -> ()
