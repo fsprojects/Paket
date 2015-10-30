2390168e7ceba38416fb453f5a8a342f9ba99a90
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-10-30 10:34:32 +0100

    Create regression test for #140

---
+++ integrationtests/Paket.IntegrationTests/FrameworkRestrictionsSpecs.fs
@@ -0,0 +1,17 @@
+﻿module Paket.IntegrationTests.FrameworkRestrictionsSpecs
+
+open Fake
+open Paket
+open System
+open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open Paket.Domain
+open Paket.Requirements
+
+[<Test>]
+let ``#140 windsor should resolve framework dependent dependencies``() =
+    let lockFile = update "i000140-resolve-framework-restrictions"
+    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "TaskParallelLibrary"].Settings.FrameworkRestrictions
+    |> shouldEqual [FrameworkRestriction.Exactly(DotNetFramework(FrameworkVersion.V3_5))]
