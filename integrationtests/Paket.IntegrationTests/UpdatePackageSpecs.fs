43d7061a6a67b0b0a82904201bbc87e4835b6a4b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-10-30 20:39:59 +0100

    Create a regression test for group specific package update - references #1018

--- integrationtests/Paket.IntegrationTests/UpdatePackageSpecs.fs
+++ integrationtests/Paket.IntegrationTests/UpdatePackageSpecs.fs
@@ -10,6 +10,25 @@ open System.Diagnostics
 open Paket
 open Paket.Domain
 
+
+[<Test>]
+let ``#1018 update package in main group``() =
+    paket "update nuget Newtonsoft.json" "i001018-legacy-groups-update" |> ignore
+    let lockFile = LockFile.LoadFrom(Path.Combine(scenarioTempPath "i001018-legacy-groups-update","paket.lock"))
+    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Newtonsoft.Json"].Version
+    |> shouldBeGreaterThan (SemVer.Parse "6.0.3")
+    lockFile.Groups.[GroupName "Legacy"].Resolution.[PackageName "Newtonsoft.Json"].Version
+    |> shouldEqual (SemVer.Parse "5.0.2")
+
+[<Test>]
+let ``#1018 update package in group``() =
+    paket "update nuget Newtonsoft.json group leGacy" "i001018-legacy-groups-update" |> ignore
+    let lockFile = LockFile.LoadFrom(Path.Combine(scenarioTempPath "i001018-legacy-groups-update","paket.lock"))
+    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "Newtonsoft.Json"].Version
+    |> shouldEqual (SemVer.Parse "6.0.3")
+    lockFile.Groups.[GroupName "Legacy"].Resolution.[PackageName "Newtonsoft.Json"].Version
+    |> shouldBeGreaterThan (SemVer.Parse "5.0.2")
+
 [<Test>]
 let ``#1178 update specific package``() =
     paket "update nuget NUnit" "i001178-update-with-regex" |> ignore
