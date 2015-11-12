7799379187ccc50398e2b4d5c7ed444dd1c3ed78
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-11-12 13:37:50 +0100

    Create integration test for #1211

---
+++ integrationtests/Paket.IntegrationTests/NuGetV3Specs.fs
@@ -0,0 +1,18 @@
+﻿module Paket.IntegrationTests.NuGetV3Specs
+
+open Fake
+open System
+open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open System.Diagnostics
+open Paket
+open Paket.Domain
+
+[<Test>]
+let ``#1211 update with V3 API``() =
+    let lockFile = update "i001211-top-level"
+        
+    lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "FsLexYacc.Runtime"].Version
+    |> shouldBeGreaterThan (SemVer.Parse "6")
