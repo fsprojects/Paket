06471573f0457a0d78f4bc312789537988c9daed
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-11-24 16:11:16 +0100

    Bump version to 2.27.14

--- src/Paket.PowerShell/AssemblyInfo.fs
+++ src/Paket.PowerShell/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
-[<assembly: AssemblyVersionAttribute("2.27.13")>]
-[<assembly: AssemblyFileVersionAttribute("2.27.13")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.27.13")>]
+[<assembly: AssemblyVersionAttribute("2.27.14")>]
+[<assembly: AssemblyFileVersionAttribute("2.27.14")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.27.14")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.27.13"
+    let [<Literal>] Version = "2.27.14"
