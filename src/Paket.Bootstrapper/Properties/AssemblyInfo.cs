0cbee3d236dccf9660d94bd4a7255ccf0c80235f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-12-20 13:25:55 +0100

    Bump version to 2.37.0-alpha003

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
 [assembly: AssemblyVersionAttribute("2.37.0")]
 [assembly: AssemblyFileVersionAttribute("2.37.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.37.0-alpha002")]
+[assembly: AssemblyInformationalVersionAttribute("2.37.0-alpha003")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "2.37.0";
--- src/Paket.Core/AssemblyInfo.fs
+++ src/Paket.Core/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
 [<assembly: AssemblyVersionAttribute("2.37.0")>]
 [<assembly: AssemblyFileVersionAttribute("2.37.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.37.0-alpha002")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.37.0-alpha003")>]
 do ()
 
 module internal AssemblyVersionInformation =
