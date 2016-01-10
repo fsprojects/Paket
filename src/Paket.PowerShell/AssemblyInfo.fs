537505073fd4f3cb834d3bc3c86fd0c35975ce4c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-01-10 14:11:59 +0100

    Bump version to 3.0.0-alpha018

--- src/Paket.PowerShell/AssemblyInfo.fs
+++ src/Paket.PowerShell/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
 [<assembly: AssemblyVersionAttribute("3.0.0")>]
 [<assembly: AssemblyFileVersionAttribute("3.0.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.0.0-alpha017")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.0.0-alpha018")>]
 do ()
 
 module internal AssemblyVersionInformation =
