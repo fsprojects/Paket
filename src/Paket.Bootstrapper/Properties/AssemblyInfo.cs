166622c5d4427d09372135d189a4f27f6874ee01
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-01-19 09:46:01 +0100

    Bump version to 2.46.0

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("2.45.0")]
-[assembly: AssemblyFileVersionAttribute("2.45.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.45.0")]
+[assembly: AssemblyVersionAttribute("2.46.0")]
+[assembly: AssemblyFileVersionAttribute("2.46.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.46.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.45.0";
+        internal const string Version = "2.46.0";
     }
 }
