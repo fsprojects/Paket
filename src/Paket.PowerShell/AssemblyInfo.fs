dc33a8d9428e23d2eb9b2fa7c93a90d672c73600
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-01-27 10:31:41 +0100

    Bump version to 2.47.3-alpha002

--- src/Paket.PowerShell/AssemblyInfo.fs
+++ src/Paket.PowerShell/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
 [<assembly: AssemblyVersionAttribute("2.47.3")>]
 [<assembly: AssemblyFileVersionAttribute("2.47.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.47.3-alpha001")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.47.3-alpha002")>]
 do ()
 
 module internal AssemblyVersionInformation =
