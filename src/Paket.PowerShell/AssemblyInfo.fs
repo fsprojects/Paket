baaa62cf9138a3e203acf3ea96435f24a2a79abe
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-02-06 18:41:19 +0100

    Bump version to 2.49.6

--- src/Paket.PowerShell/AssemblyInfo.fs
+++ src/Paket.PowerShell/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
-[<assembly: AssemblyVersionAttribute("2.49.5")>]
-[<assembly: AssemblyFileVersionAttribute("2.49.5")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.49.5")>]
+[<assembly: AssemblyVersionAttribute("2.49.6")>]
+[<assembly: AssemblyFileVersionAttribute("2.49.6")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.49.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.49.5"
+    let [<Literal>] Version = "2.49.6"
