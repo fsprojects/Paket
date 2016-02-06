d85acae9f2dc9496f4ce2c937272472d4faafb23
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-02-06 16:48:11 +0100

    Very basic project.json dependency manipulation

---
+++ src/Paket.Core/ProjectJsonFile.fs
@@ -0,0 +1,74 @@
+﻿namespace Paket.ProjectJson
+
+open Newtonsoft.Json
+open System.Collections.Generic
+open Newtonsoft.Json.Linq
+open System.Text
+open System
+
+
+type ProjectJsonProperties = {
+      [<JsonProperty("dependencies")>]
+      Dependencies : Dictionary<string, string>
+
+      [<JsonExtensionData>]
+      mutable AdditionalData: IDictionary<string, JToken>
+    }
+
+type ProjectJsonProject(text:string) =
+    
+    let findPos (property:string) =
+        let needle = sprintf "\"%s\"" property
+        match text.IndexOf needle with
+        | -1 -> text.Length - 1,text.Length - 1
+        | start ->
+            let pos = ref (start + needle.Length)
+            while text.[!pos] <> '{' do
+                incr pos
+
+            let balance = ref 1
+            incr pos
+            while !balance > 0 do
+                match text.[!pos] with
+                | '{' -> incr balance
+                | '}' -> decr balance
+                |_ -> ()
+                incr pos
+
+
+            start,!pos
+
+    member this.WithDependencies dependencies =
+        let dependencies = Map.ofSeq dependencies
+        let start,endPos = findPos "dependencies"
+        let getIndent() =
+            let pos = ref start
+            let indent = ref 0
+            while !pos > 0 && text.[!pos] <> '\r' && text.[!pos] <> '\n' do
+                incr indent
+                decr pos
+            !indent
+
+        let sb = StringBuilder(text.Substring(0,start))
+        sb.Append("\"dependencies\": ") |> ignore
+
+        let deps =
+            if Map.isEmpty dependencies then
+                sb.Append "{ }"
+            else
+                sb.AppendLine "{" |> ignore
+                let indent = "".PadLeft (max 2 (getIndent() + 3))
+                let i = ref 1
+                let n = dependencies.Count
+                for d in dependencies do
+                    let line = sprintf "\"%s\": \"%O\"%s" d.Key d.Value (if !i < n then "," else "")
+
+                    sb.AppendLine(indent + line) |> ignore
+                    incr i
+                sb.Append(indent.Substring(4) +  "}")
+
+        sb.Append(text.Substring(endPos)) |> ignore
+
+        ProjectJsonProject(sb.ToString())
+
+    override __.ToString() = text
---
+++ tests/Paket.Tests/ProjectJson/PropertySpecs.fs
@@ -0,0 +1,94 @@
+﻿module Paket.ProjectJson.PropertySpecs
+
+open Paket.ProjectJson
+open NUnit.Framework
+open FsUnit
+open System.Collections.Generic
+
+let freshProject =
+        """{
+    "version": "1.0.0-*",
+    "compilationOptions": {
+        "emitEntryPoint": true
+    },
+
+    "dependencies": {
+        "NETStandard.Library": "1.0.0-rc2-23727"
+    },
+
+    "frameworks": {
+        "dnxcore50": { }
+    }
+}"""
+
+let empty = """{ }"""
+
+let removed =
+        """{
+    "version": "1.0.0-*",
+    "compilationOptions": {
+        "emitEntryPoint": true
+    },
+
+    "dependencies": { },
+
+    "frameworks": {
+        "dnxcore50": { }
+    }
+}"""
+
+
+[<Test>]
+let ``can remove dependencies from fresh project.json``() = 
+
+    let doc = ProjectJsonProject freshProject
+    let doc' = doc.WithDependencies []
+    doc'.ToString() |> shouldEqual removed
+    
+
+[<Test>]
+let ``can add simple dependency to fresh project.json``() = 
+
+
+
+    let doc = ProjectJsonProject removed
+    let doc' = doc.WithDependencies ["NETStandard.Library", "1.0.0-rc2-23727"]
+    doc'.ToString() |> shouldEqual freshProject
+    
+[<Test>]
+let ``can add simple dependencies to fresh project.json``() = 
+
+    let expected =
+        """{
+    "version": "1.0.0-*",
+    "compilationOptions": {
+        "emitEntryPoint": true
+    },
+
+    "dependencies": {
+        "NETStandard.Library": "1.0.0-rc2-23727",
+        "a": "1.0.0"
+    },
+
+    "frameworks": {
+        "dnxcore50": { }
+    }
+}"""
+
+    let doc = ProjectJsonProject removed
+    let doc' = doc.WithDependencies ["NETStandard.Library", "1.0.0-rc2-23727"; "a", "1.0.0"]
+    doc'.ToString() |> shouldEqual expected
+    
+[<Test>]
+let ``can add simple dependencies to empty project.json``() = 
+
+    let expected =
+        """{ "dependencies": {
+     "NETStandard.Library": "1.0.0-rc2-23727",
+     "a": "1.0.0"
+ }}"""
+
+    let doc = ProjectJsonProject empty
+    let doc' = doc.WithDependencies ["NETStandard.Library", "1.0.0-rc2-23727"; "a", "1.0.0"]
+    doc'.ToString() |> shouldEqual expected
+    
