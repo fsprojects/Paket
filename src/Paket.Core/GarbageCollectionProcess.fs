a3e22f94a9d5c516888d3b6cf78d57c0d28f49d4
Christoph Ruegg <git@cdrnet.ch>
2016-02-18 08:11:06 +0100

    Garbage Collection: remove packages no longer needed from the packages folder

---
+++ src/Paket.Core/GarbageCollectionProcess.fs
@@ -0,0 +1,51 @@
+﻿/// Contains methods for the garbage collection of no longer needed files.
+module Paket.GarbageCollectionProcess
+
+open Paket
+open Paket.Domain
+open Paket.Logging
+open System.IO
+
+type ExtractedPackage = {
+    GroupName: GroupName
+    PackageName: PackageName
+    Path: DirectoryInfo
+}
+
+/// Discover all packages currently available in the packages folder
+let discoverExtractedPackages root : ExtractedPackage list =
+    let packageInDir groupName (dir:DirectoryInfo) =
+        match dir.GetFiles("*.nuspec") with
+        | [| nuspec |] ->
+            Some {
+                GroupName = groupName
+                PackageName = PackageName (Path.GetFileNameWithoutExtension nuspec.Name)
+                Path = dir
+            }
+        | _ -> None
+
+    let findGroupPackages groupName (groupDir:DirectoryInfo) =
+        groupDir.GetDirectories()
+        |> Array.choose (packageInDir groupName)
+
+    let packagesFolder = DirectoryInfo(Path.Combine(root, Constants.PackagesFolderName))
+    [
+        findGroupPackages Constants.MainDependencyGroup packagesFolder
+        packagesFolder.GetDirectories() |> Array.collect (fun dir -> findGroupPackages (GroupName dir.Name) dir)
+    ] |> Array.concat |> List.ofArray
+
+/// Remove all packages from the packages folder which are not part pf the lock file.
+let DeleteUnusedPackages(root, lockFile:LockFile) =
+
+    let resolutionKey package = package.GroupName, package.PackageName
+    let delete package =
+        try
+            Utils.deleteDir package.Path
+        with
+        | exn -> traceWarnfn "Could not delete no longer needed directory '%s'. %s." package.Path.FullName exn.Message
+
+    let resolutions = lockFile.GetGroupedResolution()
+
+    discoverExtractedPackages root
+    |> List.filter (fun p -> resolutions |> Map.containsKey (resolutionKey p) |> not)
+    |> List.iter delete
