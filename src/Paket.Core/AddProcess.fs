e509baff0d1f04f603213c5d23bfc14e0f07e42f
Christoph Ruegg <git@cdrnet.ch>
2016-02-18 13:32:25 +0100

    Garbage Collection: automatically do gc on add, remove, and install, unless --no-install is enabled

--- src/Paket.Core/AddProcess.fs
+++ src/Paket.Core/AddProcess.fs
@@ -32,6 +32,7 @@ let private add installToProjects addToProjectsF dependenciesFileName groupName 
 
         if installAfter then
             InstallProcess.Install(options, hasChanged, dependenciesFile, lockFile, updatedGroups)
+            GarbageCollectionProcess.DeleteUnusedPackages(Path.GetDirectoryName(dependenciesFileName), lockFile)
 
 // Add a package with the option to add it to a specified project.
 let AddToProject(dependenciesFileName, groupName, package, version, options : InstallerOptions, projectName, installAfter) =
--- src/Paket.Core/RemoveProcess.fs
+++ src/Paket.Core/RemoveProcess.fs
@@ -46,6 +46,7 @@ let private remove removeFromProjects dependenciesFileName groupName (package: P
     if installAfter then
         let updatedGroups = Map.add groupName 0 Map.empty
         InstallProcess.Install(InstallerOptions.CreateLegacyOptions(force, hard, false, false, SemVerUpdateMode.NoRestriction), hasChanged, dependenciesFile, lockFile, updatedGroups)
+        GarbageCollectionProcess.DeleteUnusedPackages(root, lockFile)
 
 /// Removes a package with the option to remove it from a specified project.
 let RemoveFromProject(dependenciesFileName, groupName, packageName:PackageName, force, hard, projectName, installAfter) =
