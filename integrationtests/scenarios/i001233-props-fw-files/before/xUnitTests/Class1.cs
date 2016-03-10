28116e0802bedeba7e5251dbd6051909e812aaaf
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-03-10 09:39:58 +0100

    Try to reproduce #1233

---
+++ integrationtests/scenarios/i001233-props-fw-files/before/xUnitTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitTests
+{
+    public class When_testing
+    {
+        [Fact]
+        public void tests_work()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
