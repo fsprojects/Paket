eed4b5977ba860b3089b094358d2e4f110f543e7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-03-17 17:06:52 +0100

    Bump version to 2.52.14

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("2.52.13")]
-[assembly: AssemblyFileVersionAttribute("2.52.13")]
-[assembly: AssemblyInformationalVersionAttribute("2.52.13")]
+[assembly: AssemblyVersionAttribute("2.52.14")]
+[assembly: AssemblyFileVersionAttribute("2.52.14")]
+[assembly: AssemblyInformationalVersionAttribute("2.52.14")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.52.13";
+        internal const string Version = "2.52.14";
     }
 }
