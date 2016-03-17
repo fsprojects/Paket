2202b73b9294b2f5cca91365f65dbdc30c95218c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-03-17 20:09:10 +0100

    Try to use FsCheck

---
+++ tests/Paket.Tests/Resolver/PropertyTests.fs
@@ -0,0 +1,15 @@
+﻿module Resovler.PropertyTests
+
+open Paket
+open NUnit.Framework
+open FsUnit
+open TestHelpers
+open Paket.Domain
+open Paket.PackageResolver
+open FsCheck
+
+
+[<Test>]
+let ``sound check`` () =
+    let p xs = List.rev (List.rev xs) = xs
+    Check.QuickThrowOnFailure p
