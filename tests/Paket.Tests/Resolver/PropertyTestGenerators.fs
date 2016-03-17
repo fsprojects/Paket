0486854d5bbec7a8e023b5e9eb6818cc304ca8d4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-03-17 20:09:10 +0100

    Use a brute force resolver to verify Paket's resolver

---
+++ tests/Paket.Tests/Resolver/PropertyTestGenerators.fs
@@ -0,0 +1,136 @@
+﻿module Resovler.PropertyTestGenerators
+
+open Paket
+open NUnit.Framework
+open FsUnit
+open TestHelpers
+open Paket.Domain
+open Paket.PackageResolver
+open FsCheck
+open System.Diagnostics
+
+let chooseFromList xs = 
+  gen { 
+    let! i = Gen.choose (0, List.length xs-1) 
+    return (List.item i xs) }
+
+type PackageList = (PackageName*SemVerInfo) list
+type Dependencies = (PackageName * VersionRequirement) list
+type PackageGraph = (PackageName*SemVerInfo*Dependencies) list
+
+type ResolverPuzzle = PackageGraph * Dependencies
+
+type PackageTypes =
+    static member PackageName() = 
+        Arb.from<NonNegativeInt>
+        |> Arb.convert (fun x -> PackageName("P" + (int x).ToString())) (fun x -> x.ToString().Replace("P","") |> int |> NonNegativeInt)
+
+    static member GroupName() = 
+        Arb.from<NonNegativeInt>
+        |> Arb.convert (fun x -> PackageName("G" + (int x).ToString())) (fun x -> x.ToString().Replace("G","") |> int |> NonNegativeInt)
+
+    static member Versions() = 
+         Arb.generate<NonNegativeInt*NonNegativeInt*NonNegativeInt>
+         |> Gen.map (fun (major,minor,patch) -> SemVer.Parse(sprintf "%d.%d.%d" (int major) (int minor) (int patch)))
+         |> Arb.fromGen
+
+    static member DistinctPackages() =
+        Arb.generate<(PackageName * (SemVerInfo list)) list>
+        |> Gen.map (fun packages ->
+            packages
+            |> List.map (fun (p,vs) -> vs |> List.map (fun v -> p,v))
+            |> List.concat
+            |> List.distinct
+            |> List.sort
+            : PackageList)
+        |> Arb.fromGen
+
+    static member Dependencies packages =
+        if packages = [] then [] else
+        let r = System.Random()
+        Gen.listOf (chooseFromList packages)
+        |> Gen.map (fun dependencies ->
+            dependencies
+            |> List.map (fun (p,v) -> 
+                [ p,VersionRequirement(VersionRange.Specific(v),PreReleaseStatus.All)
+                  p,VersionRequirement(VersionRange.Minimum(v),PreReleaseStatus.All)
+                  p,VersionRequirement(VersionRange.Maximum(v),PreReleaseStatus.All)
+                  p,VersionRequirement(VersionRange.AtLeast("0"),PreReleaseStatus.All)])
+            |> List.concat
+            |> List.sortBy (fun _ -> r.Next()))
+        |> Gen.eval 100 (Random.newSeed())          // create deps
+        |> List.distinctBy fst
+        |> List.sort
+
+    static member DependenciesForPackage packages package =
+        PackageTypes.Dependencies packages
+        |> List.filter (fun (d,vr) -> d <> package)
+
+    static member ShrinkGraph (g:PackageGraph) : PackageGraph seq = 
+        seq {
+            // remove one dependency
+            for (p,v,deps) in g do
+                for d in deps do
+                    yield g |> List.map (fun  (p',v',deps') -> if p = p' && v = v' then p,v,deps |> List.filter ((<>) d) else p',v',deps')
+
+
+            // remove one package
+            for (p,v,deps) in g do
+                let g' =  g |> List.filter (fun (p',v',deps') -> p <> p' || v <> v')
+                let g'' =
+                    g'
+                    |> List.map (fun (p',v',deps') -> 
+                        let hasPackage p vr = g' |> List.exists (fun (p',_,_) -> p' = p)
+
+                        let newDeps =
+                            deps 
+                            |> List.filter (fun (n,vr) -> hasPackage n vr)
+                        p',v',newDeps)
+
+                yield g''
+        }
+
+    static member FullGraph() : Arbitrary<PackageGraph> =
+        let generator =
+            Arb.generate<PackageList>
+            |> Gen.map (fun packages ->
+                    packages 
+                    |> List.map (fun (p,vs) -> p,vs, PackageTypes.DependenciesForPackage packages p))
+                    
+        Arb.fromGenShrink (generator,PackageTypes.ShrinkGraph)
+
+    static member ShrinkPuzzle ((g,deps):ResolverPuzzle) : ResolverPuzzle seq = 
+        seq {
+            // remove one dependency
+            for d in deps do
+                yield g, deps |> List.filter ((<>) d)
+
+            // shrink graph
+            for g' in PackageTypes.ShrinkGraph g do
+                let hasPackage g p vr = g |> List.exists (fun (p',_,_) -> p' = p)
+
+                let newDeps =
+                    deps 
+                    |> List.filter (fun (n,vr) -> hasPackage g' n vr)
+                yield g',newDeps
+        }
+
+    static member Puzzle() : Arbitrary<ResolverPuzzle> =
+    
+        let generator =
+            Arb.generate<PackageGraph>
+            |> Gen.map (fun g -> g,g |> List.map (fun (p,v,_) -> p,v) |> PackageTypes.Dependencies)
+
+        Arb.fromGenShrink (generator,PackageTypes.ShrinkPuzzle)
+
+[<DebuggerStepThrough>]
+let check p =
+    Arb.register<PackageTypes>() |> ignore
+    Check.QuickThrowOnFailure p
+
+let resolve (g:PackageGraph) (deps:(PackageName*VersionRequirement) list) =
+    let deps = deps |> List.map (fun (p,vr) -> p.ToString(),vr.Range)
+    let g = 
+        g
+        |> List.map (fun (p,v,deps) -> p.ToString(),v.ToString(),deps |> List.map (fun (d,vr) -> d.ToString(),vr))
+    safeResolve g deps    
---
+++ tests/Paket.Tests/Resolver/PropertyTests.fs
@@ -0,0 +1,90 @@
+﻿module Resovler.PropertyTests
+
+open Paket
+open NUnit.Framework
+open FsUnit
+open TestHelpers
+open Paket.Domain
+open Paket.PackageResolver
+open FsCheck
+open PropertyTestGenerators
+open System.Collections.Generic
+open Paket.Requirements
+
+let hasDuplicates (resolution:PackageResolution) =
+    let hashSet = HashSet<_>()
+    resolution
+    |> Map.exists (fun p _ -> hashSet.Add p |> not)
+
+let hasError ((g,deps):ResolverPuzzle) (resolution:PackageResolution) =
+    deps
+    |> List.exists (fun (d,vr:VersionRequirement) -> 
+        match resolution |> Map.tryFind d with
+        | Some r -> vr.IsInRange(r.Version) |> not
+        | None -> false)
+
+let isValid ((g,deps):ResolverPuzzle) resolution =
+    deps
+    |> List.forall (fun (d,vr:VersionRequirement) -> 
+        match resolution |> Map.tryFind d with
+        | Some r -> vr.IsInRange(r.Version) 
+        | None -> false)
+
+let bruteForce ((g,deps):ResolverPuzzle) =
+    let rec check ((g,deps):ResolverPuzzle) resolution =
+        match g with
+        | (p,v,packageDeps) :: g' ->
+            let select() =
+                let resolved =
+                    { Name = p
+                      Version = v
+                      Dependencies = Set.empty
+                      Unlisted = false
+                      Settings = InstallSettings.Default
+                      Source = PackageSources.DefaultNuGetSource }
+                match Map.tryFind p resolution with
+                | Some(_) -> None
+                | _ ->
+                    let resolution' = Map.add p resolved resolution
+                    let deps' = packageDeps @ deps
+                    if hasError (g',deps') resolution' then None else
+                    check (g',deps') resolution'
+
+            match check (g',deps) resolution with
+            | None -> select()
+            | r -> r
+
+        | _ -> if isValid (g,deps) resolution then Some resolution else None
+
+    check (g,deps) Map.empty
+
+[<Test>]
+let ``can resolve empty requirements`` () =
+    check (fun (g:PackageGraph) ->
+        match resolve g [] with
+        | Resolution.Ok resolution -> resolution |> Map.isEmpty            
+        | _ -> false)
+
+[<Test>]
+[<Ignore>]
+let ``if it resolves then, it should satisfy all deps. if not we have a real conflict`` () =
+    check (fun ((g,deps):ResolverPuzzle) ->
+        match resolve g deps with
+        | Resolution.Ok resolution ->
+            if hasDuplicates resolution then
+                failwithf "duplicates found in resolution: %A" resolution
+
+            deps
+            |> List.forall (fun (d,vr) -> 
+                match resolution |> Map.tryFind d with
+                | Some r -> vr.IsInRange(r.Version) 
+                | None -> false)
+            
+        | conflict ->
+            match bruteForce (g,deps) with
+            | None -> ()
+            | Some resolution ->
+                failwithf "brute force found %A" resolution
+
+            let conflicts = conflict.GetConflicts()
+            conflicts |> List.isEmpty |> not)
