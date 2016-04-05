fc9ed028038dac6d5be14ec58d568e3dd4ab0d50
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-04-05 11:47:39 +0200

    Bump version to 2.58.0

--- docs/content/paket-push.md
+++ docs/content/paket-push.md
@@ -11,10 +11,11 @@ Pushes the given `.nupkg` file.
 
   `file <string>`: Path to the package.
 
-  `apikey <string>`: Optionally specify your API key on the command line. Otherwise uses the value of the `nugetkey` environment variable, or the api key stored in the [`paket.config` file](paket-config.html) for this url.
+  `apikey <string>`: Optionally specify your API key on the command line. Otherwise uses the value of the `nugetkey` environment variable.
 
   `endpoint <string>`: Optionally specify a custom api endpoint to push to. Defaults to `/api/v2/package`.
 
 If you add the `-v` flag, then Paket will run in verbose mode and show detailed information.
 
 With `--log-file [FileName]` you can trace the logged information into a file.
+
--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("2.57.1")]
-[assembly: AssemblyFileVersionAttribute("2.57.1")]
-[assembly: AssemblyInformationalVersionAttribute("2.57.1")]
+[assembly: AssemblyVersionAttribute("2.58.0")]
+[assembly: AssemblyFileVersionAttribute("2.58.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.58.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.57.1";
+        internal const string Version = "2.58.0";
     }
 }
