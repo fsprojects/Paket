0a917114f993b3e3b9917a208c768273b42ee87f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-04-11 18:28:01 +0200

    Bump version to 2.58.14

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("2.58.13")]
-[assembly: AssemblyFileVersionAttribute("2.58.13")]
-[assembly: AssemblyInformationalVersionAttribute("2.58.13")]
+[assembly: AssemblyVersionAttribute("2.58.14")]
+[assembly: AssemblyFileVersionAttribute("2.58.14")]
+[assembly: AssemblyInformationalVersionAttribute("2.58.14")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.58.13";
-        internal const string InformationalVersion = "2.58.13";
+        internal const string Version = "2.58.14";
+        internal const string InformationalVersion = "2.58.14";
     }
 }
--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")>]
-[<assembly: AssemblyVersionAttribute("2.58.13")>]
-[<assembly: AssemblyFileVersionAttribute("2.58.13")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.58.13")>]
+[<assembly: AssemblyVersionAttribute("2.58.14")>]
+[<assembly: AssemblyFileVersionAttribute("2.58.14")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.58.14")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.58.13"
-    let [<Literal>] InformationalVersion = "2.58.13"
+    let [<Literal>] Version = "2.58.14"
+    let [<Literal>] InformationalVersion = "2.58.14"
