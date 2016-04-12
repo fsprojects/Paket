00ee31570a6c50d3ad016d3e6ab6c31f6aed21a1
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-04-12 10:38:03 +0200

    Use correct NUnit3 runner

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A package dependency manager for .NET with support for NuGet packages and GitHub repositories.")]
-[assembly: AssemblyVersionAttribute("2.58.16")]
-[assembly: AssemblyFileVersionAttribute("2.58.16")]
-[assembly: AssemblyInformationalVersionAttribute("2.58.16")]
+[assembly: AssemblyVersionAttribute("2.59.0")]
+[assembly: AssemblyFileVersionAttribute("2.59.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.59.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.58.16";
-        internal const string InformationalVersion = "2.58.16";
+        internal const string Version = "2.59.0";
+        internal const string InformationalVersion = "2.59.0";
     }
 }
