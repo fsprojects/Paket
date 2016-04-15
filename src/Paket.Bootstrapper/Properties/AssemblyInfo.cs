3ce9988a6525e32e1080ea01a97b1a122cde0f39
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-04-15 16:16:22 +0200

    Bump version to 3.0.0-beta001

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Reflection;
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
 [assembly: AssemblyVersionAttribute("3.0.0")]
 [assembly: AssemblyFileVersionAttribute("3.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("3.0.0-alpha115")]
+[assembly: AssemblyInformationalVersionAttribute("3.0.0-beta001")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "3.0.0";
-        internal const string InformationalVersion = "3.0.0-alpha115";
+        internal const string InformationalVersion = "3.0.0-beta001";
     }
 }
