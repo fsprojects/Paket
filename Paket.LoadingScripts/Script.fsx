778edead99b6e253b80d0bb13d78bc27da9afa6f
Gauthier Segay <gauthier.segay@gmail.com>
2016-04-17 23:04:43 +0200

    * load dependency and lock files in generateFSharpScriptsForRootFolder, pass those down to lower level functions * migrate and integrate weightTargetProfiles, makeTargetPredicate and getDllFilesWithinPackage in generateFSharpScript

--- Paket.LoadingScripts/Script.fsx
+++ Paket.LoadingScripts/Script.fsx
@@ -7,8 +7,8 @@ open Mono.Cecil
 open QuickGraph
 
 let dependenciesFile, lockFile =
-  //let rootFolder = (@"C:\dev\src\g\fiddle3d") |> DirectoryInfo
-  let rootFolder = (__SOURCE_DIRECTORY__) |> DirectoryInfo
+  let rootFolder = (@"C:\dev\src\g\fiddle3d") |> DirectoryInfo
+  //let rootFolder = (__SOURCE_DIRECTORY__) |> DirectoryInfo
   let deps = Paket.Dependencies.Locate(rootFolder.FullName)
   let lock =
     deps.DependenciesFile
@@ -17,62 +17,13 @@ let dependenciesFile, lockFile =
     |> Paket.LockFile.LoadFrom
   deps, lock
 
-let tryFind key (dict: IDictionary<_,_>) =
-  match dict.TryGetValue(key) with
-  | true, v -> Some v
-  | _       -> None
+Paket.LoadingScripts.ScriptGeneratingModule.generateFSharpScriptsForRootFolder ((@"C:\dev\src\g\fiddle3d") |> DirectoryInfo)
 
-let getDllFilesWithinPackage (paketDependencies: Paket.Dependencies) (targetPredicate: _ -> Paket.LibFolder option) packageName =
-  
-  let installModel =
-    let groupName = None
-    paketDependencies.GetInstalledPackageModel(groupName, packageName)
-  let libFolder = 
-    targetPredicate installModel.ReferenceFileFolders
-    (**installModel.ReferenceFileFolders
-    |> Seq.map (fun f -> f, targetPredicate f)
-    |> Seq.choose (snd >> id)
-    |> Seq.tryHead*)
-  
-  let references = 
-    match libFolder with
-    | None        -> Set.empty
-    | Some folder -> folder.Files.References
-
-  let referenceByAssembly =
-    references
-    |> Seq.filter (fun f -> f.LibName.IsSome)
-    |> Seq.map (fun r -> (r.Path |> AssemblyDefinition.ReadAssembly), r)
-    |> dict
-
-  let assemblyByName =
-    referenceByAssembly.Keys
-    |> Seq.map (fun a -> a.Name.ToString(), a)
-    |> dict
-
-  let graph = AdjacencyGraph<_,_>()
-  
-  for r in referenceByAssembly do
-    let assembly = r.Key
-    let paketRef = referenceByAssembly.[assembly]
-    graph.AddVertex(paketRef) |> ignore
-    let references = assembly.MainModule.AssemblyReferences
-    printfn "%A" paketRef
-    references
-    |> Seq.map (fun a -> tryFind a.FullName assemblyByName)
-    |> Seq.choose id
-    |> Seq.map (fun a -> paketRef, referenceByAssembly.[a])
-    |> Seq.iter (fun (fromRef, toRef) ->
-      graph.AddVertex(toRef) |> ignore
-      graph.AddEdge(Edge(fromRef, toRef)) |> ignore
-      )
-
-  let result =
-    let topologicalSort = Algorithms.TopologicalSort.TopologicalSortAlgorithm(graph)
-    topologicalSort.Compute()
-    topologicalSort.SortedVertices |> Seq.rev |> Seq.toArray
-
-  result
+(*
+let tryFind = Paket.LoadingScripts.ScriptGeneratingModule.tryFind
+let weightTargetProfiles = Paket.LoadingScripts.ScriptGeneratingModule.weightTargetProfiles
+let makeTargetPredicate = Paket.LoadingScripts.ScriptGeneratingModule.makeTargetPredicate
+let getDllFilesWithinPackage = Paket.LoadingScripts.ScriptGeneratingModule.getDllFilesWithinPackage
 
 let computePackageTopologyGraph (lockFile: Paket.LockFile) =
   let lookup = Dictionary<_,_>()
@@ -107,57 +58,6 @@ let computePackageTopologyGraph (lockFile: Paket.LockFile) =
   ] 
   |> dict
 
-let weightTargetProfiles possibleFrameworksInOrderOfPreference (profiles: Paket.TargetProfile list) =
-  
-  let relevantFrameworksWithPreferenceIndex =
-    possibleFrameworksInOrderOfPreference
-    |> Seq.mapi (fun i f -> f, i)
-    |> dict
-  
-  let profileWithAllFrameworks =
-    profiles
-    |> Seq.map (fun targetProfile ->
-      targetProfile, 
-      match targetProfile with
-      | Paket.SinglePlatform platform -> [platform]
-      | Paket.PortableProfile (_, platforms) -> platforms
-    )
-    |> Seq.map (fun (targetProfile, profiles) ->
-      targetProfile, 
-      profiles
-      |> Seq.map (fun p -> p, tryFind p relevantFrameworksWithPreferenceIndex)
-    )
-  
-  // we pick the first one for which the sum of prefered indexes is the lowest
-  let choices = 
-    profileWithAllFrameworks
-    |> Seq.map (fun (targetProfile, selection) -> 
-      let sum =
-        let selectionWithMatches =
-          selection
-          |> Seq.filter (snd >> Option.isSome)
-        if selectionWithMatches |> Seq.isEmpty then
-          Int32.MaxValue
-        else
-          selectionWithMatches
-          |> Seq.map (snd)
-          |> Seq.choose id
-          |> Seq.sum
-      targetProfile,sum
-    )
-    |> Seq.filter (snd >> ((<>) Int32.MaxValue))
-  choices
-
-let makeTargetPredicate frameworks =
-  fun (folders: Paket.LibFolder list) ->
-    folders
-    |> Seq.map (fun folder-> folder, weightTargetProfiles frameworks folder.Targets)
-    |> Seq.map (fun (folder, weightedTargetProfiles) -> 
-      folder, weightedTargetProfiles |> Seq.sumBy snd
-    )
-    |> Seq.sortBy snd
-    |> Seq.map fst
-    |> Seq.tryHead
 
 let frameworkIdentifiersInPreferenceOrder =
   [
@@ -173,7 +73,7 @@ for refFolder in profiles.ReferenceFileFolders do
 
 let targetPredicate = makeTargetPredicate frameworkIdentifiersInPreferenceOrder
 
-getDllFilesWithinPackage dependenciesFile targetPredicate ("fspickler")
+getDllFilesWithinPackage dependenciesFile targetPredicate None ("fspickler")
 
 let packagesGraph = computePackageTopologyGraph lockFile
 
@@ -181,7 +81,7 @@ for (group, package) in packagesGraph.Keys do
   
   printfn "= %s %s =" (group.ToString()) (package.GetCompareString())
   try
-    let dlls = getDllFilesWithinPackage dependenciesFile targetPredicate (package.ToString())
+    let dlls = getDllFilesWithinPackage dependenciesFile targetPredicate (None) (package.ToString())
     for d in dlls do
       printfn "\t-> %s" d.Path
   with
@@ -194,3 +94,4 @@ for p in installModel.ReferenceFileFolders do
   printfn "%s" folderName
   for t in p.Targets do
     printfn "\t-> %A" t
+*)
