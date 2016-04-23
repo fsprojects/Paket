ebc8c8e19053ed23cefb91a43c4daf251f159d35
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-04-23 09:19:48 +0200

    add an integration test

---
+++ integrationtests/Paket.IntegrationTests/DevSourceOverrideSpecs.fs
@@ -0,0 +1,24 @@
+﻿module Paket.IntegrationTests.DevSourceOverrideSpecs
+
+open System.IO
+open System.Xml.Linq
+open System.Xml.XPath
+
+open FsUnit
+
+open NUnit.Framework
+
+[<Test>]
+let ``#1633 should favor overriden source from paket.local``() = 
+    paket "restore" "i001633-dev-source-override" |> ignore
+    let nunitVersion =
+        Path.Combine(
+            scenarioTempPath "i001633-dev-source-override",
+            "packages",
+            "NUnit",
+            "NUnit.nuspec")
+        |> XDocument.Load
+        |> fun doc -> doc.XPathSelectElement("/package/metadata/version")
+        |> fun elem -> elem.Value
+    
+    nunitVersion |> shouldEqual "2.6.4"
