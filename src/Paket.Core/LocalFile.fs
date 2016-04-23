e1cebedb872fc5976335011cbf37b8ec9fa34a51
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-04-23 18:08:11 +0200

    add source .fs files for LocalFile

---
+++ src/Paket.Core/LocalFile.fs
@@ -0,0 +1,16 @@
+﻿namespace Paket
+
+open Chessie.ErrorHandling
+
+open Paket.Domain
+open Paket.PackageSources
+
+type DevSourceOverride =
+    | DevNugetSourceOverride of packageName: PackageName * devSource: PackageSource
+
+type LocalFile = DevSourceOverride list
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module LocalFile =
+    let parse lines =
+        Trial.pass ()
---
+++ tests/Paket.Tests/LocalFile/LocalFileSpecs.fs
@@ -0,0 +1,2 @@
+module Paket.LocalFileSpecs
+
