014d1ebdaa372b4bf50e1bf2fdb8290bbd1b337c
Matthias Dittrich <matthi.d@gmail.com>
2016-04-23 15:38:30 +0200

    move functionality to paket.core
    (minor) remove changes unrelated to this PR

--- src/Paket.LoadingScripts/Scripts/load-project-debug.fsx
+++
@@ -1,5 +0,0 @@
-// Warning: generated file; your changes could be lost when a new file is generated.
-#I __SOURCE_DIRECTORY__
-#load "load-references-debug.fsx"
-#load "../AssemblyInfo.fs"
-      "../LoadingScriptsGenerator.fs"
