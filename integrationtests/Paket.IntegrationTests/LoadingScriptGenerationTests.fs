7a00e61eb949a0ef49b946944fb475c063ccab51
Gauthier Segay <gauthier.segay@gmail.com>
2016-04-23 13:05:22 +0200

    simple skeleton for integration tests of loading script generation

---
+++ integrationtests/Paket.IntegrationTests/LoadingScriptGenerationTests.fs
@@ -0,0 +1,14 @@
+﻿module Paket.IntegrationTests.LoadingScriptGenerationTests
+open NUnit.Framework
+open Paket.IntegrationTests.TestHelpers
+open System.IO
+
+let paket command scenario =
+  paket command (Path.Combine("loading-scripts-scenarios", scenario))
+
+[<Test>]
+let ``simple dependencies generates expected scripts``() = 
+  paket "install" "simple-dependencies" |> ignore
+  // todo
+  ()
+  
