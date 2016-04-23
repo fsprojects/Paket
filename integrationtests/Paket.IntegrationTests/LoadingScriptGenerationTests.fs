fed823d446689b449cdc827d721c9fe85897c51e
Gauthier Segay <gauthier.segay@gmail.com>
2016-04-23 20:32:08 +0200

    integration test when framework is specified

--- integrationtests/Paket.IntegrationTests/LoadingScriptGenerationTests.fs
+++ integrationtests/Paket.IntegrationTests/LoadingScriptGenerationTests.fs
@@ -1,7 +1,8 @@
 ﻿module Paket.IntegrationTests.LoadingScriptGenerationTests
+open System
+open System.IO
 open NUnit.Framework
 open Paket.IntegrationTests.TestHelpers
-open System.IO
 open Paket
 
 let makeScenarioPath scenario    = Path.Combine("loading-scripts-scenarios", scenario)
@@ -22,7 +23,6 @@ let ``simple dependencies generates expected scripts``() =
   let framework = "net4"
   paket "install" scenario |> ignore
 
-
   directPaket (sprintf "generate-include-scripts framework %s" framework) scenario |> ignore
   
   let files = getGeneratedScriptFiles framework scenario
@@ -36,4 +36,38 @@ let ``simple dependencies generates expected scripts``() =
       "include.nunit.fsx"
   |]
   Assert.AreEqual(expectedFiles, actualFiles)
+  
+
+[<Test>]
+let ``framework specified``() = 
+  let scenario = "framework-specified"
+  paket "install" scenario |> ignore
+
+  directPaket "generate-include-scripts" scenario |> ignore
+  
+  let files =
+    getGeneratedScriptFiles "net35" scenario
+    |> Seq.map (fun f -> f.Name, f)
+    |> dict
+
+  let expectations = [
+    "include.iesi.collections.csx", ["Net35/Iesi.Collections.dll"]
+    "include.iesi.collections.fsx", ["Net35/Iesi.Collections.dll"]
+    "include.nhibernate.csx", ["Net35/NHibernate.dll";"#load \"include.iesi.collections.csx\""]
+    "include.nhibernate.fsx", ["Net35/NHibernate.dll";"#load @\"include.iesi.collections.fsx\""]
+  ]
+
+  let failures = seq {
+    for (file, contains) in expectations do
+      match files.TryGetValue file with
+      | false, _ -> yield sprintf "file %s was not found" file
+      | true, file -> 
+        let text = file.FullName |> File.ReadAllText
+        for expectedText in contains do
+          if not (text.Contains expectedText) then
+            yield sprintf "file %s didn't contain %s" file.FullName expectedText
+  }
+
+  if not (Seq.isEmpty failures) then
+    Assert.Fail (failures |> String.concat Environment.NewLine)
   
