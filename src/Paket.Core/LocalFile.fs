2c24b7f00b50f35e4957f746fa1a75e1609d54eb
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-04-26 07:21:25 +0200

    add unit test for parsing paket.local file

--- src/Paket.Core/LocalFile.fs
+++ src/Paket.Core/LocalFile.fs
@@ -8,9 +8,10 @@ open Paket.PackageSources
 type DevSourceOverride =
     | DevNugetSourceOverride of packageName: PackageName * devSource: PackageSource
 
-type LocalFile = DevSourceOverride list
+type LocalFile = LocalFile of devSourceOverrides: DevSourceOverride list
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module LocalFile =
     let parse lines =
-        Trial.pass ()
+        LocalFile []
+        |> Trial.pass
--- tests/Paket.Tests/LocalFile/LocalFileSpecs.fs
+++ tests/Paket.Tests/LocalFile/LocalFileSpecs.fs
@@ -1,2 +1,26 @@
 module Paket.LocalFileSpecs
 
+open Chessie.ErrorHandling
+
+open NUnit.Framework
+
+open FsUnit
+
+open Paket.Domain
+open Paket.PackageSources
+open Paket.TestHelpers
+
+[<Test>]
+let ``should parse single dev source override``() = 
+    let contents = """
+        nuget NUnit -> source ./local_source
+        """
+    let expected = 
+        LocalFile [
+            DevNugetSourceOverride (PackageName "NUnit", LocalNuGet ("./local_source", None)) 
+        ]
+        |> Trial.ok
+
+    let actual = LocalFile.parse (toLines contents)
+
+    actual |> shouldEqual expected
