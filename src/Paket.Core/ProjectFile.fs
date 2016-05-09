ee2aaa63ff2b19591429779cfd726f2524f30492
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-05-09 13:15:15 +0200

    fix tests

--- src/Paket.Core/ProjectFile.fs
+++ src/Paket.Core/ProjectFile.fs
@@ -527,7 +527,7 @@ module ProjectFile =
 
             if !isPaketNode = paketOnes then yield node]
 
-    let getCustomReferenceAndFrameworkNodes project = findNodes false "Reference" project
+    let getCustomReferenceAndFrameworkNodes project = (findNodes false "Reference" project) @ (findNodes false "NativeReference" project)
 
     let findPaketNodes name (project:ProjectFile) = findNodes true name  project
 
@@ -866,6 +866,7 @@ module ProjectFile =
     let removePaketNodes (project:ProjectFile) = 
         deletePaketNodes "Analyzer" project
         deletePaketNodes "Reference" project
+        deletePaketNodes "NativeReference" project
 
         let rec getPaketNodes (node:XmlNode) =
             [for node in node.ChildNodes do
