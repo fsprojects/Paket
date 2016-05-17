73ae1a7b4b9c2aa18b54595842b12143e946c891
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-05-17 15:49:11 +0200

    enable all integration tests

--- build.fsx
+++ build.fsx
@@ -186,8 +186,7 @@ Target "RunIntegrationTests" (fun _ ->
         { p with
             ShadowCopy = false
             WorkingDir = "tests/Paket.Tests"
-            TimeOut = TimeSpan.FromMinutes 40.
-            Where = "test =~ /Paket.IntegrationTests.LocalOverrideSpecs/" })
+            TimeOut = TimeSpan.FromMinutes 40. })
 )
 
 
