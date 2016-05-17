57f24f32c416847e7aaf1c4ee06ae63309fd96d9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-05-17 12:08:49 +0200

    Adding AddRange for Maps

--- src/Paket.Core/AssemblyInfo.fs
+++ src/Paket.Core/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("2.64.6")>]
-[<assembly: AssemblyFileVersionAttribute("2.64.6")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.64.6")>]
+[<assembly: AssemblyVersionAttribute("2.64.7")>]
+[<assembly: AssemblyFileVersionAttribute("2.64.7")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.64.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.64.6"
-    let [<Literal>] InformationalVersion = "2.64.6"
+    let [<Literal>] Version = "2.64.7"
+    let [<Literal>] InformationalVersion = "2.64.7"
