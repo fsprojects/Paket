1b16686dd6f992fe163f976fedc3c6d40b91da7b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-05-17 15:32:46 +0200

    Bump version to 3.0.0-beta041

--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -7,9 +7,9 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
 [<assembly: AssemblyVersionAttribute("3.0.0")>]
 [<assembly: AssemblyFileVersionAttribute("3.0.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.0.0-beta040")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.0.0-beta041")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] Version = "3.0.0"
-    let [<Literal>] InformationalVersion = "3.0.0-beta040"
+    let [<Literal>] InformationalVersion = "3.0.0-beta041"
