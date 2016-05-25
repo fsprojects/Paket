167d5af08ad4a94ee0063fbfb6f4ffba5313662d
shortsn <shortsn@users.noreply.github.com>
2016-05-25 21:16:24 +0200

    #1600 prepare test

---
+++ integrationtests/Paket.IntegrationTests/RemoveSpecs.fs
@@ -0,0 +1,15 @@
+﻿module Paket.IntegrationTests.RemoveSpecs
+
+open Fake
+open System
+open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open System.Diagnostics
+open Paket
+open Paket.Domain
+
+[<Test>]
+let ``#1600 paket remove nuget should remove empty groups``() = 
+    paket "remove add Castle.Core testgroup" "i001600-remove-empty-group" |> ignore
