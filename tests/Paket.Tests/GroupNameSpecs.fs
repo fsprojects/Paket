aa150e5d1f6fa88c906387d27f0915522e29fc22
shortsn <shortsn@users.noreply.github.com>
2016-06-01 21:22:41 +0200

    #1627 throw on invalid group names

---
+++ tests/Paket.Tests/GroupNameSpecs.fs
@@ -0,0 +1,11 @@
+﻿module Paket.GroupNameSpecs
+
+open Paket
+open Paket.Domain
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``should throw on prohibited group names``() = 
+    shouldFail<System.ArgumentException>(fun () -> GroupName("lib") |> ignore)
+    shouldFail<System.ArgumentException>(fun () -> GroupName("runtimes") |> ignore)
