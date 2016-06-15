eac1c2965505a31c7744b4bb3c86726941d1af74
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-06-15 15:43:09 +0200

    remove flaky tests

--- integrationtests/Paket.IntegrationTests/AutocompleteSpecs.fs
+++ integrationtests/Paket.IntegrationTests/AutocompleteSpecs.fs
@@ -12,31 +12,6 @@ open Paket
 open Paket.PackageSources
 
 [<Test>]
-let ``#1298 should autocomplete for FAKE on NuGet 2``() = 
-    let result = Dependencies.FindPackagesByName([PackageSources.DefaultNuGetSource],"fake")
-    result |> shouldContain "FAKE"
-    result |> shouldContain "FAKE.IIS"
-
-[<Test>]
-let ``#1298 should autocomplete for FAKE on NuGet3``() = 
-    let result = Dependencies.FindPackagesByName([PackageSource.NuGetV3Source Constants.DefaultNuGetV3Stream],"fake")
-    result |> shouldContain "FAKE"
-    result |> shouldContain "FAKE.IIS"
-
-[<Test>]
-let ``#1298 should autocomplete for nunit on NuGet3``() = 
-    let result = Dependencies.FindPackagesByName([PackageSource.NuGetV3Source Constants.DefaultNuGetV3Stream],"nunit")
-    result |> shouldContain "NUnit.Runners"
-    result |> shouldContain "Cloak.NUnit"
-    result |> shouldContain "NUnit"
-
-[<Test>]
-[<Ignore("it's only working on forki's machine")>]
-let ``#1298 should autocomplete for msu on local teamcity``() = 
-    let result = Dependencies.FindPackagesByName([PackageSource.NuGetV2Source "http://teamcity/guestAuth/app/nuget/v1/FeedService.svc/"],"msu")
-    result |> shouldContain "msu.Addins"
-
-[<Test>]
 let ``#1298 should autocomplete for dapper on local feed``() = 
     let result = Dependencies.FindPackagesByName([PackageSource.LocalNuGet(Path.Combine(originalScenarioPath "i001219-props-files", "nuget_repo"),None)],"dapp")
     result |> shouldContain "Dapper"
