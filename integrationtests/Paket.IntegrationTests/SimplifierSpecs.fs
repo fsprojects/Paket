41af538e91316eddd037900c2422eff333895554
Chet Husk <chusk3@gmail.com>
2016-06-18 19:05:14 -0500

    update simplify to property generate the list of targetframework restrictions when the framework is being auto-detected

---
+++ integrationtests/Paket.IntegrationTests/SimplifierSpecs.fs
@@ -0,0 +1,18 @@
+﻿module Paket.IntegrationTests.SimplifierSpecs
+
+open NUnit.Framework
+open Fake
+open FsUnit
+
+[<Test>]
+let ``#1737 simplify should handle auto-detected framework``() =
+    let scenario = "i001737-simplify-with-auto-framework"
+    prepare scenario
+    paket "install" scenario |> ignore
+
+    let deps = Paket.Dependencies(scenarioTempPath scenario </> "paket.dependencies")
+    deps.Simplify(false)
+    
+    deps.GetDependenciesFile().Groups.[Paket.Constants.MainDependencyGroup].Packages |> shouldHaveLength 1
+
+
