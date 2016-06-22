a52b0e8c176ade88decc21089f351c40993644fe
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-06-22 13:09:31 +0200

    Bump version to 3.1.9

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
-[assembly: AssemblyVersionAttribute("3.1.8")]
-[assembly: AssemblyFileVersionAttribute("3.1.8")]
-[assembly: AssemblyInformationalVersionAttribute("3.1.8")]
+[assembly: AssemblyVersionAttribute("3.1.9")]
+[assembly: AssemblyFileVersionAttribute("3.1.9")]
+[assembly: AssemblyInformationalVersionAttribute("3.1.9")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "3.1.8";
-        internal const string InformationalVersion = "3.1.8";
+        internal const string Version = "3.1.9";
+        internal const string InformationalVersion = "3.1.9";
     }
 }
