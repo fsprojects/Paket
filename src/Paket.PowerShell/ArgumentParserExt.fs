dc0dd293baf41122b75bc056a65a8a95111f58cc
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2016-06-27 20:21:52 +0300

    update to argu 3

--- src/Paket.PowerShell/ArgumentParserExt.fs
+++ src/Paket.PowerShell/ArgumentParserExt.fs
@@ -6,7 +6,6 @@ open Argu
 // https://github.com/nessos/UnionArgParser/issues/35
 
 type ArgumentParser<'Args when 'Args :> IArgParserTemplate> with
-    member parser.CreateParseResultsOfList(inputs : 'Args list) : ParseResults<'Args> =
+    member parser.CreateParseResultsOfList(inputs : 'Args list) : ParseResult<'Args> =
         let cliParams = parser.PrintCommandLine inputs // unparse inputs to cli form
         parser.ParseCommandLine cliParams
-
