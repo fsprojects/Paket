4e5c336ae8f0e1cf4e7c1bead6cbf8be7b43c082
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-28 12:54:39 +0200

    Repro: failing tests for second run of auto-restore on

--- integrationtests/Paket.IntegrationTests/AutoRestoreSpec.fs
+++ integrationtests/Paket.IntegrationTests/AutoRestoreSpec.fs
@@ -15,5 +15,16 @@ let ``#1835 auto-restore on downloads targets and updates proj file``() =
     let projectFile = File.ReadAllText(Path.Combine(scenarioPath, "AutoRestoreTwice" ,"AutoRestoreTwice.csproj"))
     StringAssert.Contains("<Import Project=\"..\.paket\paket.targets\" />", projectFile)
 
+[<Test>]
+let ``#1835 auto-restore on twice same result as first run``() = 
+    let scenario = "i001835-auto-restore-on-twice"
+    paket "auto-restore on" scenario |> ignore
+    // now running auto-restore a second time on the same project.
+    directPaket "auto-restore on" scenario |> ignore
+    let scenarioPath = scenarioTempPath scenario
+    Path.Combine(scenarioPath , ".paket","paket.targets") |> checkFileExists
+    let projectFile = File.ReadAllText(Path.Combine(scenarioPath, "AutoRestoreTwice" ,"AutoRestoreTwice.csproj"))
+    StringAssert.Contains("<Import Project=\"..\.paket\paket.targets\" />", projectFile)
+
 
 
