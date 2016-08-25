2d4b05ec270f28d81f68365af2e7c843d9a62042
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2016-08-25 03:12:07 +0100

    minor code cleanup

--- src/Paket.Core/Utils.fs
+++ src/Paket.Core/Utils.fs
@@ -30,12 +30,10 @@ let internal memoize (f: 'a -> 'b) : 'a -> 'b =
 
 let internal memoizeAsync f =
     let cache = System.Collections.Concurrent.ConcurrentDictionary<'a, System.Threading.Tasks.Task<'b>>()
-    fun (x: 'a) ->
-        async {
-            return! cache.GetOrAdd(x, fun x ->
-                let threadfix = lazy( f(x) |> Async.StartAsTask )
-                threadfix.Force()) |> Async.AwaitTask
-        }
+    fun (x: 'a) -> // task.Result serialization to sync after done.
+        cache.GetOrAdd(x, fun x -> // Lazy is needed for "if managed to start multiple before added"
+            let threadfix = lazy( f(x) |> Async.StartAsTask )
+            threadfix.Force()) |> Async.AwaitTask
 
 type Auth = 
     | Credentials of Username : string * Password : string
