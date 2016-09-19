f4f63bd3f83498ef5c916f458ddb05e2311c2327
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-09-19 13:07:08 +0200

    Bump version to 3.19.6

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
-[assembly: AssemblyVersionAttribute("3.19.5")]
-[assembly: AssemblyFileVersionAttribute("3.19.5")]
-[assembly: AssemblyInformationalVersionAttribute("3.19.5")]
+[assembly: AssemblyVersionAttribute("3.19.6")]
+[assembly: AssemblyFileVersionAttribute("3.19.6")]
+[assembly: AssemblyInformationalVersionAttribute("3.19.6")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "3.19.5";
-        internal const string InformationalVersion = "3.19.5";
+        internal const string Version = "3.19.6";
+        internal const string InformationalVersion = "3.19.6";
     }
 }
--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("3.19.5")>]
-[<assembly: AssemblyFileVersionAttribute("3.19.5")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.19.5")>]
+[<assembly: AssemblyVersionAttribute("3.19.6")>]
+[<assembly: AssemblyFileVersionAttribute("3.19.6")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.19.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.19.5"
-    let [<Literal>] InformationalVersion = "3.19.5"
+    let [<Literal>] Version = "3.19.6"
+    let [<Literal>] InformationalVersion = "3.19.6"
