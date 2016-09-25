6d82cb5802665b797b0f4a23ac303ea4135fea79
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-09-25 21:37:15 +0200

    Bump version to 3.19.7

--- src/Paket.PowerShell/AssemblyInfo.fs
+++ src/Paket.PowerShell/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("3.19.6")>]
-[<assembly: AssemblyFileVersionAttribute("3.19.6")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.19.6")>]
+[<assembly: AssemblyVersionAttribute("3.19.7")>]
+[<assembly: AssemblyFileVersionAttribute("3.19.7")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.19.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.19.6"
-    let [<Literal>] InformationalVersion = "3.19.6"
+    let [<Literal>] Version = "3.19.7"
+    let [<Literal>] InformationalVersion = "3.19.7"
