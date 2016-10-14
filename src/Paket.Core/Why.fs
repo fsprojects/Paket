af8f319c9d9fa08141ad67e37a7a9116faf65f19
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-10-14 08:28:32 +0200

    draw dependency graph paths

--- src/Paket.Core/Why.fs
+++ src/Paket.Core/Why.fs
@@ -1,8 +1,41 @@
 module Paket.Why
 
+open Paket.Domain
 open Paket.Logging
 
-let ohWhy (packageName, lockFile: LockFile, groupName, usage) =
+type AdjGraph<'a> = list<'a * list<'a>>
+
+let adj n (g: AdjGraph<_>) =
+    g
+    |> List.find (fst >> (=) n)
+    |> snd
+
+let rec paths start stop (g : AdjGraph<'a>) =
+    if start = stop then [[start]]
+    else
+        [ for n in adj start g do
+            for path in paths n stop g do
+                yield start :: path ]
+
+let depGraph (res : PackageResolver.PackageResolution) : AdjGraph<Domain.PackageName> =
+    res
+    |> Seq.toList
+    |> List.map (fun pair -> pair.Key, (pair.Value.Dependencies 
+                                       |> Set.map (fun (p,_,_) -> p) 
+                                       |> Set.toList))
+
+let prettyPrintPath path = 
+    path 
+    |> List.mapi 
+        (fun i v -> 
+            sprintf "%s%s%s%s"
+                    (String.replicate i "  ")
+                    (if i > 0 then "-> " else "")
+                    v
+                    (if i = 0 then " (top-level dependency)" else ""))
+    |> List.iter tracen
+
+let ohWhy (packageName, lockFile : LockFile, groupName, usage) =
     let group = lockFile.GetGroup(groupName)
     if not <| group.Resolution.ContainsKey packageName then
         match lockFile.Groups |> Seq.filter (fun g -> g.Value.Resolution.ContainsKey packageName) |> Seq.toList with
@@ -17,20 +50,34 @@ let ohWhy (packageName, lockFile: LockFile, groupName, usage) =
         | [] ->
             traceErrorfn "NuGet %O was not found in %s" packageName Constants.LockFileName
     else
-        let isTopLevel =
-            lockFile.GetTopLevelDependencies groupName
-            |> Map.exists (fun key _ -> key = packageName)
-        if isTopLevel then
-            tracefn "NuGet %O is in %s group because it's defined as a top-level dependency" packageName (groupName.ToString()) 
-        else
-            let xs =
-                group.Resolution
-                |> Seq.filter (fun pair -> pair.Value.Dependencies
-                                        |> Seq.exists (fun (name,_,_) -> name = packageName))
-                |> Seq.map (fun pair -> pair.Key.ToString())
-                |> Seq.toList
-            
-            tracefn "NuGet %O is in %s group because it's a dependency of those packages: %A"
-                    packageName
-                    (groupName.ToString())
-                    xs
+        let g = depGraph group.Resolution
+        let topLevel = lockFile.GetTopLevelDependencies groupName
+        let topLevelPaths = 
+            topLevel
+            |> Seq.map (fun pair -> pair.Key)
+            |> Seq.toList
+            |> List.collect (fun p -> paths p packageName g)
+        tracefn "Dependency graphs for %O" packageName
+        tracefn ""
+        for path in topLevelPaths do
+            path 
+            |> List.map (sprintf "%O")
+            |> prettyPrintPath
+            tracefn ""
+        //let isTopLevel =
+        //    lockFile.GetTopLevelDependencies groupName
+        //    |> Map.exists (fun key _ -> key = packageName)
+        //if isTopLevel then
+        //    tracefn "NuGet %O is in %s group because it's defined as a top-level dependency" packageName (groupName.ToString()) 
+        //else
+        //    let xs =
+        //        group.Resolution
+        //        |> Seq.filter (fun pair -> pair.Value.Dependencies
+        //                                |> Seq.exists (fun (name,_,_) -> name = packageName))
+        //        |> Seq.map (fun pair -> pair.Key.ToString())
+        //        |> Seq.toList
+        //    
+        //    tracefn "NuGet %O is in %s group because it's a dependency of those packages: %A"
+        //            packageName
+        //            (groupName.ToString())
+        //            xs
