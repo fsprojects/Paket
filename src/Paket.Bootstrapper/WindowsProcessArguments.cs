c2e92ca790d65ceaf1ea5bb6c0f8170a6dd21644
Julien Roncaglia <julien@roncaglia.fr>
2016-10-19 22:23:18 +0200

    Better windows argument generation with tests

--- src/Paket.Bootstrapper/WindowsProcessArguments.cs
+++ src/Paket.Bootstrapper/WindowsProcessArguments.cs
@@ -5,52 +5,78 @@ namespace Paket.Bootstrapper
 {
     static class WindowsProcessArguments
     {
+        static void AddBackslashes(StringBuilder builder, int backslashes, bool beforeQuote)
+        {
+            if (backslashes == 0)
+            {
+                return;
+            }
+
+            // Always using 'backslashes * 2' would work it would just produce needless '\'
+            var count = beforeQuote || backslashes % 2 == 0
+                ? backslashes * 2
+                : (backslashes-1) * 2 + 1 ;
+
+            for(int i = 0; i < count; i++)
+            {
+                builder.Append('\\');
+            }
+        }
+
+        /* A good summary of the code used to process arguments in most windows programs can be found at :
+         * http://www.daviddeley.com/autohotkey/parameters/parameters.htm#WINARGV
+         */
         static void AppendEscaped(StringBuilder builder, string arg)
         {
-            var needQuote = arg.Contains(" ") || arg.Contains("\t");
+            builder.EnsureCapacity(builder.Length + arg.Length);
+
+            var needQuote = false;
+            var containsQuoteOrBackslash = false;
+            foreach(var c in arg)
+            {
+                needQuote |= (c == ' ');
+                needQuote |= (c == '\t');
+                containsQuoteOrBackslash |= (c == '"');
+                containsQuoteOrBackslash |= (c == '\\');
+            }
+
             if (needQuote)
             {
                 builder.Append('"');
             }
+            else if (!containsQuoteOrBackslash)
+            {
+                // No special characters are present, early exit
+                builder.Append(arg);
+                return;
+            }
 
             var index = 0;
+            var backslashes = 0;
             while (index < arg.Length)
             {
                 var c = arg[index];
-                if (c == '"')
+                if (c == '\\')
                 {
-                    builder.Append(@"\""");
-                    
-                } else if (c == '\\')
+                    backslashes++;
+                }
+                else if (c == '"')
                 {
-                    var isLast = index == arg.Length-1;
-                    if (isLast && needQuote)
-                    {
-                        builder.Append(@"\\");
-                    } else if (needQuote)
-                    {
-                        var next = arg[index+1];
-                        if (next == '"')
-                        {
-                            builder.Append(@"\\\""");
-                            index += 1;
-                        }
-                        else
-                        {
-                            builder.Append(c);
-                        }
-                        
-                    } else
-                    {
-                        builder.Append(c);
-                    }
+                    AddBackslashes(builder, backslashes, true);
+                    backslashes = 0;
+                    builder.Append('\\');
+                    builder.Append(c);
                 }
                 else
                 {
+                    AddBackslashes(builder, backslashes, false);
+                    backslashes = 0;
                     builder.Append(c);
                 }
                 index += 1;
             }
+
+            AddBackslashes(builder, backslashes, needQuote);
 
             if (needQuote)
             {
@@ -60,7 +86,7 @@ namespace Paket.Bootstrapper
 
         public static string ToString(IEnumerable<string> args)
         {
-            var builder = new StringBuilder();
+            var builder = new StringBuilder(255);
             foreach (var arg in args)
             {
                 if (builder.Length != 0)
