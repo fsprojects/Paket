8000258f30f2afc4c49f522470d2fbed4bf7802e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-10-25 16:29:22 +0200

    fix test

--- integrationtests/Paket.IntegrationTests/InstallSpecs.fs
+++ integrationtests/Paket.IntegrationTests/InstallSpecs.fs
@@ -408,12 +408,6 @@ let ``#1333 should install framework refs only once``() =
     let s2 = File.ReadAllText newFile |> normalizeLineEndings
     s2 |> shouldEqual s1
 
-    let newFile = Path.Combine(scenarioTempPath "i001854-submodules","Submodule","SubLevel","Project1.fsproj")
-    let oldFile = Path.Combine(originalScenarioPath "i001854-submodules","Submodule","SubLevel","Project1.fsprojtemplate")
-    let s1 = File.ReadAllText oldFile |> normalizeLineEndings
-    let s2 = File.ReadAllText newFile |> normalizeLineEndings
-    s2 |> shouldEqual s1
-
 [<Test>]
 let ``#1779 net20 only in net461``() =
     install "i001779-net20-only-in-net461" |> ignore
