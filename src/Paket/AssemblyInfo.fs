afc154ae132a77c2fb4f8899385aed2b04153dad
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-11-14 13:37:17 +0100

    Bump version to 3.27.02

--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("3.27.01")>]
-[<assembly: AssemblyFileVersionAttribute("3.27.01")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.27.01")>]
+[<assembly: AssemblyVersionAttribute("3.27.02")>]
+[<assembly: AssemblyFileVersionAttribute("3.27.02")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.27.02")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -16,6 +16,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Paket"
     let [<Literal>] AssemblyCompany = "Paket team"
     let [<Literal>] AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories."
-    let [<Literal>] AssemblyVersion = "3.27.01"
-    let [<Literal>] AssemblyFileVersion = "3.27.01"
-    let [<Literal>] AssemblyInformationalVersion = "3.27.01"
+    let [<Literal>] AssemblyVersion = "3.27.02"
+    let [<Literal>] AssemblyFileVersion = "3.27.02"
+    let [<Literal>] AssemblyInformationalVersion = "3.27.02"
