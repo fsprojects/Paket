c59a1945168b6f77b330f3dc9e0e12442ce406be
Johannes Vorster <johannes.vorster@outlook.com>
2017-01-15 21:28:50 +0200

    Add function to remove double quotes from the local paths

--- src/Paket.Core/PackageSources.fs
+++ src/Paket.Core/PackageSources.fs
@@ -53,6 +53,10 @@ let tryParseWindowsStyleNetworkPath (path : string) =
         trimmed.Replace('\\', '/') |> sprintf "smb:%s" |> Some
     else None
 
+let RemoveStringQuotes(path : string) =
+    let trimChars = [|'\"'|]
+    path.Trim(trimChars)
+
 type NugetSource = 
     { Url : string
       Authentication : NugetSourceAuthentication option }
@@ -232,7 +236,9 @@ type PackageSource =
         | NuGetV2 x -> n x.Url x.Authentication
         | NuGetV3 x -> n x.Url x.Authentication
         | LocalNuGet(path,_) -> 
-            if not <| Directory.Exists (path.Replace("\"", "")) then 
+            if not <| Directory.Exists (RemoveStringQuotes path) then 
                 traceWarnfn "Local NuGet feed doesn't exist: %s." path
 
 let DefaultNuGetSource = PackageSource.NuGetV2Source Constants.DefaultNuGetStream
+
+
