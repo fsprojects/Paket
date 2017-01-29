4081e77bbaa6807e45b02b6fe155cca102dba5a3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-01-29 10:47:06 +0100

    Bump version to 3.34.0

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
-[assembly: AssemblyVersionAttribute("3.33.5")]
-[assembly: AssemblyFileVersionAttribute("3.33.5")]
-[assembly: AssemblyInformationalVersionAttribute("3.33.5")]
+[assembly: AssemblyVersionAttribute("3.34.0")]
+[assembly: AssemblyFileVersionAttribute("3.34.0")]
+[assembly: AssemblyInformationalVersionAttribute("3.34.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Paket.Bootstrapper";
         internal const System.String AssemblyProduct = "Paket";
         internal const System.String AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories.";
-        internal const System.String AssemblyVersion = "3.33.5";
-        internal const System.String AssemblyFileVersion = "3.33.5";
-        internal const System.String AssemblyInformationalVersion = "3.33.5";
+        internal const System.String AssemblyVersion = "3.34.0";
+        internal const System.String AssemblyFileVersion = "3.34.0";
+        internal const System.String AssemblyInformationalVersion = "3.34.0";
     }
 }
