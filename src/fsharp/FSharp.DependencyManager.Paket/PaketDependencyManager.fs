fd11802768628a19893b86c111c29fa2b4335678
Gauthier Segay <gauthier.segay@gmail.com>
2017-03-03 01:51:33 +0100

    * extract all paket hanlder specifc code to separate assembly (FSharp.DependencyManager.Paket) * define marker FSharpCompilerExtensibility attribute (checked only by name) * define ReflectionHelper * define implementation of IDependencyManagerProvider (kept as internal contract to help documentation) using reflection * change registeredDependencyManagers to load the paket assembly (we will need to change this to some mechanism) and inspect for types satisfying IDependencyManagerProvider "constraints"

---
+++ src/fsharp/FSharp.DependencyManager.Paket/PaketDependencyManager.fs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.FSharp.DependencyManager.Paket
+
+// used as a marker that compiler scans for, although there is no hard dependency, filtered by name
+type FSharpCompilerExtensibilityAttribute() =
+  inherit System.Attribute()
+
+type [<FSharpCompilerExtensibility>] PaketDependencyManager() =
+    member __.Name = "Paket"
+    member __.ToolName = "paket.exe"
+    member __.Key = "paket"
+    member __.ResolveDependencies(targetFramework:string, scriptDir: string, scriptName: string, packageManagerTextLines: string seq) = 
+        ReferenceLoading.PaketHandler.ResolveDependencies(
+            targetFramework,
+            scriptDir,
+            scriptName,
+            packageManagerTextLines)
+
+    interface System.IDisposable with
+        member __.Dispose() = ()
---
+++ src/fsharp/FSharp.DependencyManager.Paket/PaketDependencyManager.fsi
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.FSharp.DependencyManager.Paket
+
+type [<Class>] PaketDependencyManager =
+    interface System.IDisposable
+    new : unit -> PaketDependencyManager
+    member Name : string
+    member ToolName: string
+    member Key: string
+    member ResolveDependencies : targetFramework: string * scriptDir: string * scriptName: string * dependencyManagerTextLines: string seq -> string * string list
