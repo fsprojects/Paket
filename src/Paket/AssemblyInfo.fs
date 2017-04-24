00f42d376ef2cda153de3576341b3bab651268af
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-04-24 15:15:42 +0200

    Bump version to 4.6.2

--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("4.6.1")>]
-[<assembly: AssemblyFileVersionAttribute("4.6.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.6.1")>]
+[<assembly: AssemblyVersionAttribute("4.6.2")>]
+[<assembly: AssemblyFileVersionAttribute("4.6.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.6.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -16,6 +16,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Paket"
     let [<Literal>] AssemblyCompany = "Paket team"
     let [<Literal>] AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories."
-    let [<Literal>] AssemblyVersion = "4.6.1"
-    let [<Literal>] AssemblyFileVersion = "4.6.1"
-    let [<Literal>] AssemblyInformationalVersion = "4.6.1"
+    let [<Literal>] AssemblyVersion = "4.6.2"
+    let [<Literal>] AssemblyFileVersion = "4.6.2"
+    let [<Literal>] AssemblyInformationalVersion = "4.6.2"
