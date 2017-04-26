1c9cef2d44dd40ceed77e3adc1edf14123b2d1c6
Jared Hester <jared.hester@gmail.com>
2017-04-26 18:21:34 -0400

    test category constants

---
+++ tests/Paket.Tests/Categories.fs
@@ -0,0 +1,16 @@
+﻿[<AutoOpen>]
+module Paket.Constants
+
+[<RequireQualifiedAccess>]
+module Category =
+
+    let [<Literal>] Versioning = "Versioning"
+    let [<Literal>] Nuget = "Nuget"
+    let [<Literal>] DependenciesFile = "DependenciesFile"
+    let [<Literal>] Resolver = "Resolver"
+    let [<Literal>] LockFile = "LockFile"
+    let [<Literal>] ProjectFile = "ProjectFile"
+    let [<Literal>] InstallModel = "InstallModel"
+    let [<Literal>] Xml = "Xml"
+    let [<Literal>] Packaging = "Packaging"
+    let [<Literal>] ScriptGeneration = "Script Generation"
