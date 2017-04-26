57966c685e0daa967948d9d8cd1adf08a5a260a4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-04-26 10:28:26 +0200

    Bump version to 4.8.3

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
-[assembly: AssemblyVersionAttribute("4.8.2")]
-[assembly: AssemblyFileVersionAttribute("4.8.2")]
-[assembly: AssemblyInformationalVersionAttribute("4.8.2")]
+[assembly: AssemblyVersionAttribute("4.8.3")]
+[assembly: AssemblyFileVersionAttribute("4.8.3")]
+[assembly: AssemblyInformationalVersionAttribute("4.8.3")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Paket.Bootstrapper";
         internal const System.String AssemblyProduct = "Paket";
         internal const System.String AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories.";
-        internal const System.String AssemblyVersion = "4.8.2";
-        internal const System.String AssemblyFileVersion = "4.8.2";
-        internal const System.String AssemblyInformationalVersion = "4.8.2";
+        internal const System.String AssemblyVersion = "4.8.3";
+        internal const System.String AssemblyFileVersion = "4.8.3";
+        internal const System.String AssemblyInformationalVersion = "4.8.3";
     }
 }
