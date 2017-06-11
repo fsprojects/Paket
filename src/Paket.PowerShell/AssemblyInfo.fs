bd7e24b5bedf09537284905321ce3a43fd014f11
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-06-11 11:36:20 +0200

    Bump version to 4.8.8

--- src/Paket.PowerShell/AssemblyInfo.fs
+++ src/Paket.PowerShell/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("4.8.7")>]
-[<assembly: AssemblyFileVersionAttribute("4.8.7")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.8.7")>]
+[<assembly: AssemblyVersionAttribute("4.8.8")>]
+[<assembly: AssemblyFileVersionAttribute("4.8.8")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.8.8")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -16,6 +16,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Paket"
     let [<Literal>] AssemblyCompany = "Paket team"
     let [<Literal>] AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories."
-    let [<Literal>] AssemblyVersion = "4.8.7"
-    let [<Literal>] AssemblyFileVersion = "4.8.7"
-    let [<Literal>] AssemblyInformationalVersion = "4.8.7"
+    let [<Literal>] AssemblyVersion = "4.8.8"
+    let [<Literal>] AssemblyFileVersion = "4.8.8"
+    let [<Literal>] AssemblyInformationalVersion = "4.8.8"
