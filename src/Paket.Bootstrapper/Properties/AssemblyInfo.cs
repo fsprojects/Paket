b86034b80069dec5d5380721e3b42ec705418da5
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-07-19 10:00:45 +0200

    Downgrade FSharp.Core

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
-[assembly: AssemblyVersionAttribute("5.79.2")]
-[assembly: AssemblyFileVersionAttribute("5.79.2")]
-[assembly: AssemblyInformationalVersionAttribute("5.79.2")]
+[assembly: AssemblyVersionAttribute("5.79.3")]
+[assembly: AssemblyFileVersionAttribute("5.79.3")]
+[assembly: AssemblyInformationalVersionAttribute("5.79.3")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Paket.Bootstrapper";
         internal const System.String AssemblyProduct = "Paket";
         internal const System.String AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories.";
-        internal const System.String AssemblyVersion = "5.79.2";
-        internal const System.String AssemblyFileVersion = "5.79.2";
-        internal const System.String AssemblyInformationalVersion = "5.79.2";
+        internal const System.String AssemblyVersion = "5.79.3";
+        internal const System.String AssemblyFileVersion = "5.79.3";
+        internal const System.String AssemblyInformationalVersion = "5.79.3";
     }
 }
