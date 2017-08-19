f5ef73eed93ddf95bea247347650b5e81e6ca922
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-08-19 11:40:22 +0200

    Bump version to 5.86.0-alpha005

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
 [assembly: AssemblyVersionAttribute("5.86.0")]
 [assembly: AssemblyFileVersionAttribute("5.86.0")]
-[assembly: AssemblyInformationalVersionAttribute("5.86.0-alpha004")]
+[assembly: AssemblyInformationalVersionAttribute("5.86.0-alpha005")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Paket.Bootstrapper";
@@ -14,6 +14,6 @@ namespace System {
         internal const System.String AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories.";
         internal const System.String AssemblyVersion = "5.86.0";
         internal const System.String AssemblyFileVersion = "5.86.0";
-        internal const System.String AssemblyInformationalVersion = "5.86.0-alpha004";
+        internal const System.String AssemblyInformationalVersion = "5.86.0-alpha005";
     }
 }
