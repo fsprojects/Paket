548214bafb1f30166e8d5166cdae4f764c2ab12c
Lukas Rieger <0x53A@users.noreply.github.com>
2017-09-11 10:18:38 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ then
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
   fi
-  # export MSBuild=packages/build/RoslynTools.MSBuild/tools/msbuild/MSBuild.exe
-  packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+  MSBuild=packages/build/RoslynTools.MSBuild/tools/msbuild/MSBuild.exe packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
 else
   mono .paket/paket.exe restore
   exit_code=$?
