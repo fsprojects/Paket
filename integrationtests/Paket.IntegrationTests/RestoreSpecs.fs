f6f9dd9714c729001a2482dbf69c378f345da36b
jkwiecinski <julian.kwiecinski@castingnetworks.com>
2017-10-02 18:28:12 +0200

    Better integration test

--- integrationtests/Paket.IntegrationTests/RestoreSpecs.fs
+++ integrationtests/Paket.IntegrationTests/RestoreSpecs.fs
@@ -6,6 +6,7 @@ open Fake
 open NUnit.Framework
 open FsUnit
 open Paket
+open Paket.Utils
 
 [<Test>]
 let ``#2496 Paket fails on projects that target multiple frameworks``() = 
@@ -24,8 +25,11 @@ let ``#2812 Lowercase package names in package cache``() =
 
     let shouldContainExactlyOneCasingVariant (str: string) (sequence: string seq) = 
         let matchingStrings = getCasingVariantsOf str sequence |> List.ofSeq
-        if not (matchingStrings |> Seq.tryExactlyOne |> Option.exists ((=) str))
-        then failwithf "Expected \"%s\" but got %A" str matchingStrings
+        shouldEqual [ str ] matchingStrings
+
+    let fileNamesInDir (dir: string) = dir |> directoryInfo |> filesInDir |> Seq.map (fun x -> x.Name)
+    let subDirectoryNames (dir: string) = dir |> directoryInfo |> subDirectories |> Seq.map (fun x -> x.Name)    
+    let matchingFileNamesInDir (filter: string -> bool) (dir: string) = fileNamesInDir dir |> Seq.filter filter
 
     let scenario = "i002812"
     let packageName = "AutoMapper"
@@ -36,15 +40,23 @@ let ``#2812 Lowercase package names in package cache``() =
     [ packageName; packageNameLowercase ] |> Seq.iter clearPackage
     directPaket "restore" scenario |> ignore
     
-    let packageCacheDirectory = Constants.UserNuGetPackagesFolder |> directoryInfo
-    let packageFolderNames = packageCacheDirectory |> subDirectories |> Array.map (fun x -> x.Name)
-    packageFolderNames |> shouldContainExactlyOneCasingVariant packageNameLowercase
+    // check if packages stored in cache are properly lowercased
+    Constants.UserNuGetPackagesFolder
+        |> subDirectoryNames
+        |> shouldContainExactlyOneCasingVariant packageNameLowercase
+    Constants.UserNuGetPackagesFolder @@ packageNameLowercase @@ packageVersion
+        |> matchingFileNamesInDir (String.startsWithIgnoreCase (sprintf "%s." packageName))
+        |> Seq.map (fun x -> x.Substring(0, packageName.Length))
+        |> Seq.distinct
+        |> shouldContainExactlyOneCasingVariant packageNameLowercase
 
-    let packageContentFolder = packageCacheDirectory.FullName @@ packageNameLowercase @@ packageVersion
-    let packageFiles = packageContentFolder |> directoryInfo |> filesInDir |> Array.map (fun x -> x.Name)
-
-    let filesToFind = [ sprintf ".%s.nupkg" packageVersion
-                        sprintf ".%s.nupkg.sha512" packageVersion
-                        ".nuspec" ]
-                      |> List.map (sprintf "%s%s" packageNameLowercase)
-    filesToFind |> Seq.iter (fun x -> packageFiles |> shouldContainExactlyOneCasingVariant x)
+    // check if packages stored in solution package folder are left unchanged
+    let packagesDirectory = (scenarioTempPath scenario) @@ "packages"
+    packagesDirectory
+        |> subDirectoryNames
+        |> shouldContainExactlyOneCasingVariant packageName
+    packagesDirectory @@ packageName
+        |> matchingFileNamesInDir (String.startsWithIgnoreCase (sprintf "%s." packageName))
+        |> Seq.map (fun x -> x.Substring(0, packageName.Length))
+        |> Seq.distinct
+        |> shouldContainExactlyOneCasingVariant packageName
