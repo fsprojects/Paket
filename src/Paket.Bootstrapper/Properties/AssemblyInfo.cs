4cf21d3a0e57e1c426700bfd367785767aa456e9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-10-20 10:40:06 +0200

    Update Paket.Restore.targets

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
-[assembly: AssemblyVersionAttribute("5.119.4")]
-[assembly: AssemblyFileVersionAttribute("5.119.4")]
-[assembly: AssemblyInformationalVersionAttribute("5.119.4")]
+[assembly: AssemblyVersionAttribute("5.119.5")]
+[assembly: AssemblyFileVersionAttribute("5.119.5")]
+[assembly: AssemblyInformationalVersionAttribute("5.119.5")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Paket.Bootstrapper";
         internal const System.String AssemblyProduct = "Paket";
         internal const System.String AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories.";
-        internal const System.String AssemblyVersion = "5.119.4";
-        internal const System.String AssemblyFileVersion = "5.119.4";
-        internal const System.String AssemblyInformationalVersion = "5.119.4";
+        internal const System.String AssemblyVersion = "5.119.5";
+        internal const System.String AssemblyFileVersion = "5.119.5";
+        internal const System.String AssemblyInformationalVersion = "5.119.5";
     }
 }
