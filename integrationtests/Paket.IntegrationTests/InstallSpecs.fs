533aacb2f5a64ecebb074319f4fb38931738e903
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-10-27 09:04:26 +0200

    Fix test

--- integrationtests/Paket.IntegrationTests/InstallSpecs.fs
+++ integrationtests/Paket.IntegrationTests/InstallSpecs.fs
@@ -507,7 +507,7 @@ let ``#1860 faulty condition was generated`` () =
 [<Test>]
 let ``#2777 should not conflict with locked packages``() =
     let newLockFile = install "i002777"
-    newLockFile.Groups.[GroupName "main"].Resolution.ContainsKey (PackageName "FsPickler")
+    newLockFile.Groups.[GroupName "main"].Resolution.ContainsKey (PackageName "FsPickler") |> shouldEqual true
 
 
 #if INTERACTIVE
