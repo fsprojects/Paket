49e3eb6b21ed699a85818383537146b1faa5ee6c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-12-05 12:06:58 +0100

    enable the riddle - fixes #2631

--- integrationtests/Paket.IntegrationTests/ResolverSkipsConflictsFastSpecs.fs
+++ integrationtests/Paket.IntegrationTests/ResolverSkipsConflictsFastSpecs.fs
@@ -36,7 +36,7 @@ let ``#2289 Paket 4.x install command takes hours to complete``() =
     nunitVersion
     |> shouldBeSmallerThan (SemVer.Parse "3.0")
 
-[<Test; Ignore "This is a really good test, but the riddle got a lot harder with netstandard20 release - ignoring until we have a good solution #2631">]
+[<Test>]
 let ``#2294 Cannot pin NETStandard.Library = 1.6.0``() =
     let lockFile = update "i002294-pin-netstandard-1-6"
     lockFile.Groups.[Constants.MainDependencyGroup].Resolution.[PackageName "NETStandard.Library"].Version
