3cf15c214c30698653dfdcd536a79fdf1d9443b5
Enrico Sada <enrico@sada.io>
2017-12-28 19:25:33 +0100

    add test to check paket install -> execute tool

--- integrationtests/Paket.IntegrationTests/RepoToolSpecs.fs
+++ integrationtests/Paket.IntegrationTests/RepoToolSpecs.fs
@@ -22,3 +22,21 @@ let ``#3000 repo tool should work after restore``() =
     let resultCmd = directToolEx false helloCmdPath "" (scenarioTempPath scenario) 
     CollectionAssert.AreEqual( [| "Hello World from F#!" |], (resultCmd |> Seq.map PaketMsg.getMessage |> Array.ofSeq) )
 
+
+[<Test>]
+let ``#3001 repo tool should work after install``() =
+    let scenario = "i003001-repo-tool-in-dep"
+    prepare scenario
+
+    paket "install" scenario |> ignore
+
+    let wrappersPath = Path.Combine(scenarioTempPath scenario, "paket-files", "bin")
+
+    let helloCmdPath = Path.Combine(wrappersPath, "hello.cmd")
+    Assert.IsTrue(File.Exists(helloCmdPath), (sprintf "file '%s' not found" helloCmdPath))
+    
+    let helloBashPath = Path.Combine(wrappersPath, "hello")
+    Assert.IsTrue(File.Exists(helloBashPath), (sprintf "file '%s' not found" helloBashPath))
+
+    let resultCmd = directToolEx false helloCmdPath "" (scenarioTempPath scenario) 
+    CollectionAssert.AreEqual( [| "Hello World from F#!" |], (resultCmd |> Seq.map PaketMsg.getMessage |> Array.ofSeq) )
