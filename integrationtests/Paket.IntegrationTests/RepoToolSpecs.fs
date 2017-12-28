284397c61b72e0ec208e1d7eee65265565f60af8
Enrico Sada <enrico@sada.io>
2017-12-28 19:25:33 +0100

    add integration test

---
+++ integrationtests/Paket.IntegrationTests/RepoToolSpecs.fs
@@ -0,0 +1,16 @@
+﻿module Paket.IntegrationTests.RepoToolSpecs
+
+open NUnit.Framework
+open Fake
+open FsUnit
+
+[<Test>]
+let ``#3000 repo tool should work after restore``() =
+    let scenario = "i003000-repo-tool"
+    prepare scenario
+    paket "restore" scenario |> ignore
+
+    ()
+
+
+
