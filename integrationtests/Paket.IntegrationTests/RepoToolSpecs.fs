f2bfd64828155c61055c6170ef17dd0eddadb489
Enrico Sada <enrico@sada.io>
2017-12-28 19:25:33 +0100

    f add integration test

--- integrationtests/Paket.IntegrationTests/RepoToolSpecs.fs
+++ integrationtests/Paket.IntegrationTests/RepoToolSpecs.fs
@@ -3,6 +3,7 @@
 open NUnit.Framework
 open Fake
 open FsUnit
+open System.IO
 
 [<Test>]
 let ``#3000 repo tool should work after restore``() =
@@ -10,7 +11,13 @@ let ``#3000 repo tool should work after restore``() =
     prepare scenario
     paket "restore" scenario |> ignore
 
-    ()
+    let wrappersPath = Path.Combine(scenarioTempPath scenario, "paket-files", "bin")
+
+    let helloCmdPath = Path.Combine(wrappersPath, "hello.cmd")
+    Assert.IsTrue(File.Exists(helloCmdPath), (sprintf "file '%s' not found" helloCmdPath))
+    
+    let helloBashPath = Path.Combine(wrappersPath, "hello")
+    Assert.IsTrue(File.Exists(helloBashPath), (sprintf "file '%s' not found" helloBashPath))
 
 
 
