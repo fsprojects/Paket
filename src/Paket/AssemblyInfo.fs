4c78603ae441e6000789fdaecaa5e3407bef884c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-12-29 11:06:58 +0100

    release notes

--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("5.127.11")>]
-[<assembly: AssemblyFileVersionAttribute("5.127.11")>]
-[<assembly: AssemblyInformationalVersionAttribute("5.127.11")>]
+[<assembly: AssemblyVersionAttribute("5.127.12")>]
+[<assembly: AssemblyFileVersionAttribute("5.127.12")>]
+[<assembly: AssemblyInformationalVersionAttribute("5.127.12")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -16,6 +16,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Paket"
     let [<Literal>] AssemblyCompany = "Paket team"
     let [<Literal>] AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories."
-    let [<Literal>] AssemblyVersion = "5.127.11"
-    let [<Literal>] AssemblyFileVersion = "5.127.11"
-    let [<Literal>] AssemblyInformationalVersion = "5.127.11"
+    let [<Literal>] AssemblyVersion = "5.127.12"
+    let [<Literal>] AssemblyFileVersion = "5.127.12"
+    let [<Literal>] AssemblyInformationalVersion = "5.127.12"
