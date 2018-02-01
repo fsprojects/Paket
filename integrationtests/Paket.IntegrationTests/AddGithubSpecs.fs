65777988fbaf11289c5e203282761c276bbd9030
Fredrik Lönnegren <fredrik.lonnegren@gmail.com>
2018-02-01 15:28:26 +0100

    Github spec

---
+++ integrationtests/Paket.IntegrationTests/AddGithubSpecs.fs
@@ -0,0 +1,23 @@
+﻿module Paket.IntegrationTests.AddGithubSpecs
+
+open Fake
+open System
+open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open System.Diagnostics
+open Paket
+open Paket.Domain
+
+[<Test>]
+let ``#2923 paket add-github clitool``() =
+    let scenario = "i002923-add-github"
+    prepare scenario
+    paket "add-github forki/FsUnit" scenario |> ignore
+
+    let depsFile = DependenciesFile.ReadFromFile(Path.Combine(scenarioTempPath scenario,"paket.dependencies"))
+    let requirement = depsFile.GetGroup(Constants.MainDependencyGroup).Packages |> List.exactlyOne
+    requirement.Name |> shouldEqual (PackageName "dotnet-fable")
+    requirement.VersionRequirement.ToString() |> shouldEqual "1.3.7"
+    requirement.Kind |> shouldEqual Paket.Requirements.PackageRequirementKind.DotnetCliTool
