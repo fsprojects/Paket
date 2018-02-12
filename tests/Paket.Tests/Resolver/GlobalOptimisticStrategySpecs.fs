e787f3383afba6a82fcc928010344e70b8d84053
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2018-02-12 07:31:49 +0100

    fix tests

--- tests/Paket.Tests/Resolver/GlobalOptimisticStrategySpecs.fs
+++ tests/Paket.Tests/Resolver/GlobalOptimisticStrategySpecs.fs
@@ -199,4 +199,4 @@ let ``should favor global strategy to resolve strategy override conflicts``() =
         |> resolve graph2 UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Castle.Windsor"] |> shouldEqual "3.2.0"
     getVersion resolved.[PackageName "Castle.Core-NLog"] |> shouldEqual "3.2.0"
-    getVersion resolved.[PackageName "Castle.Core"] |> shouldEqual "3.3.1"
+    getVersion resolved.[PackageName "Castle.Core"] |> shouldEqual "3.2.0"
