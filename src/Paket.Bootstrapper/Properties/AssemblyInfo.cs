51ecc7923763a1f019a78d04ffada6fedced26c8
Matthias Dittrich <matthi.d@gmail.com>
2018-02-25 21:16:11 +0100

    update

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
-[assembly: AssemblyVersionAttribute("5.138.5")]
-[assembly: AssemblyFileVersionAttribute("5.138.5")]
-[assembly: AssemblyInformationalVersionAttribute("5.138.5")]
+[assembly: AssemblyVersionAttribute("5.138.6")]
+[assembly: AssemblyFileVersionAttribute("5.138.6")]
+[assembly: AssemblyInformationalVersionAttribute("5.138.6-alpha001")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Paket.Bootstrapper";
         internal const System.String AssemblyProduct = "Paket";
         internal const System.String AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories.";
-        internal const System.String AssemblyVersion = "5.138.5";
-        internal const System.String AssemblyFileVersion = "5.138.5";
-        internal const System.String AssemblyInformationalVersion = "5.138.5";
+        internal const System.String AssemblyVersion = "5.138.6";
+        internal const System.String AssemblyFileVersion = "5.138.6";
+        internal const System.String AssemblyInformationalVersion = "5.138.6-alpha001";
     }
 }
