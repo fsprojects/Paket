67b30a261bc36470f46dcd27b875f685f5bf15ac
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2018-03-13 07:40:14 +0100

    Bump version to 5.150.0

--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("5.149.0")>]
-[<assembly: AssemblyFileVersionAttribute("5.149.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("5.149.0")>]
+[<assembly: AssemblyVersionAttribute("5.150.0")>]
+[<assembly: AssemblyFileVersionAttribute("5.150.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("5.150.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -16,6 +16,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Paket"
     let [<Literal>] AssemblyCompany = "Paket team"
     let [<Literal>] AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories."
-    let [<Literal>] AssemblyVersion = "5.149.0"
-    let [<Literal>] AssemblyFileVersion = "5.149.0"
-    let [<Literal>] AssemblyInformationalVersion = "5.149.0"
+    let [<Literal>] AssemblyVersion = "5.150.0"
+    let [<Literal>] AssemblyFileVersion = "5.150.0"
+    let [<Literal>] AssemblyInformationalVersion = "5.150.0"
