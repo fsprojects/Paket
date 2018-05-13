ee00949ee37770f6e917ca4b27e7f325b43156b3
Matthias Dittrich <matthi.d@gmail.com>
2018-05-13 14:17:17 +0200

    Bump version to 5.162.0-alpha001

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
-[assembly: AssemblyVersionAttribute("5.161.3")]
-[assembly: AssemblyFileVersionAttribute("5.161.3")]
-[assembly: AssemblyInformationalVersionAttribute("5.161.3")]
+[assembly: AssemblyVersionAttribute("5.162.0")]
+[assembly: AssemblyFileVersionAttribute("5.162.0")]
+[assembly: AssemblyInformationalVersionAttribute("5.162.0-alpha001")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Paket.Bootstrapper";
         internal const System.String AssemblyProduct = "Paket";
         internal const System.String AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories.";
-        internal const System.String AssemblyVersion = "5.161.3";
-        internal const System.String AssemblyFileVersion = "5.161.3";
-        internal const System.String AssemblyInformationalVersion = "5.161.3";
+        internal const System.String AssemblyVersion = "5.162.0";
+        internal const System.String AssemblyFileVersion = "5.162.0";
+        internal const System.String AssemblyInformationalVersion = "5.162.0-alpha001";
     }
 }
--- src/Paket/AssemblyInfo.fs
+++ src/Paket/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("5.161.3")>]
-[<assembly: AssemblyFileVersionAttribute("5.161.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("5.161.3")>]
+[<assembly: AssemblyVersionAttribute("5.162.0")>]
+[<assembly: AssemblyFileVersionAttribute("5.162.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("5.162.0-alpha001")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -16,6 +16,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Paket"
     let [<Literal>] AssemblyCompany = "Paket team"
     let [<Literal>] AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories."
-    let [<Literal>] AssemblyVersion = "5.161.3"
-    let [<Literal>] AssemblyFileVersion = "5.161.3"
-    let [<Literal>] AssemblyInformationalVersion = "5.161.3"
+    let [<Literal>] AssemblyVersion = "5.162.0"
+    let [<Literal>] AssemblyFileVersion = "5.162.0"
+    let [<Literal>] AssemblyInformationalVersion = "5.162.0-alpha001"
