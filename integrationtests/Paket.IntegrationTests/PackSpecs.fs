2379e1f289d910038f7ed1e478bf9218023ac3d5
Enrico Sada <enrico.sada@jet.com>
2018-09-12 18:25:54 +0200

    add failing test: doing `paket pack` if `--template` is specified, shouldn't consider others template files.
    in the regression test, the `paket pack` failed because tried to load `PaketBug2`template file, who
    isnt ready (a file required doesn't exists)

--- integrationtests/Paket.IntegrationTests/PackSpecs.fs
+++ integrationtests/Paket.IntegrationTests/PackSpecs.fs
@@ -692,3 +692,10 @@ let ``#4003 dotnet pack of a global tool with p2p``() =
     // Problably not as "packaged" console applications have this dependency by default, see https://www.nuget.org/packages/dotnet-mergenupkg
     nuspec.Dependencies.Value.Length
     |> shouldEqual 0
+
+[<Test>]
+let ``#4010-pack-template-only``() =
+    let scenario = "i004010-pack-template-only"
+    let outPath = Path.Combine(scenarioTempPath scenario, "out")
+    let templatePath = Path.Combine(scenarioTempPath scenario, "PaketBug", "paket.template")
+    paket (sprintf """pack --template "%s" "%s" """ templatePath outPath) scenario |> ignore
