8fc56c9c57a772fe493321db392bc6e7ab2c9a7c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2018-09-17 11:59:16 +0200

    Bump version to 5.180.0

--- src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket")]
 [assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")]
-[assembly: AssemblyVersionAttribute("5.179.1")]
-[assembly: AssemblyFileVersionAttribute("5.179.1")]
-[assembly: AssemblyInformationalVersionAttribute("5.179.1")]
+[assembly: AssemblyVersionAttribute("5.180.0")]
+[assembly: AssemblyFileVersionAttribute("5.180.0")]
+[assembly: AssemblyInformationalVersionAttribute("5.180.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Paket.Bootstrapper";
         internal const System.String AssemblyProduct = "Paket";
         internal const System.String AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories.";
-        internal const System.String AssemblyVersion = "5.179.1";
-        internal const System.String AssemblyFileVersion = "5.179.1";
-        internal const System.String AssemblyInformationalVersion = "5.179.1";
+        internal const System.String AssemblyVersion = "5.180.0";
+        internal const System.String AssemblyFileVersion = "5.180.0";
+        internal const System.String AssemblyInformationalVersion = "5.180.0";
     }
 }
