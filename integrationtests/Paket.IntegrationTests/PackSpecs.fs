cece905a9bd4256d76b72d69234271dd0397b4a8
Enrico Sada <enrico.sada@jet.com>
2019-05-24 18:20:17 +0200

    cleanup

--- integrationtests/Paket.IntegrationTests/PackSpecs.fs
+++ integrationtests/Paket.IntegrationTests/PackSpecs.fs
@@ -739,7 +739,6 @@ let ``#4003 dotnet pack of a global tool with p2p``() =
     nuspec.Dependencies.Value.Length
     |> shouldEqual 0
 
-
 [<Test>]
 let ``#4004 dotnet pack using different versions``() = 
     let project = "lib1"
@@ -748,7 +747,7 @@ let ``#4004 dotnet pack using different versions``() =
 
     let rootPath = scenarioTempPath scenario
 
-    directPaket ("restore") scenario
+    directPaket "restore" scenario
     |> ignore
 
     let pack outDir additionalArgs expectedVersion =
@@ -758,16 +757,11 @@ let ``#4004 dotnet pack using different versions``() =
         |> ignore
 
         let nupkgPath = Path.Combine(outPath, sprintf "%s.%s.nupkg" project expectedVersion)
-        if File.Exists nupkgPath |> not then Assert.Fail(sprintf "Expected '%s' to exist" nupkgPath)
+        Assert.True(File.Exists nupkgPath, sprintf "Expected '%s' to exist" nupkgPath)
 
     pack "out1" "" "1.0.0"
     pack "out1" "/p:Version=1.2.3" "1.2.3"
     pack "out2" "/p:Version=2.3.4" "2.3.4"
-
-
-
-
-
 
 [<Test>]
 let ``#4010-pack-template-only``() =
