e1bc44e2720664c7b017d6c5ef5fb32b5fe4bb5a
Gauthier Segay <gauthier.segay@gmail.com>
2020-03-22 19:17:53 -0700

    return type is different between 5.0.100-preview.1.20155.7 and vs2019 16.6.0 preview 1

--- src/FSharp.DependencyManager.Paket/PaketDependencyManager.fs
+++ src/FSharp.DependencyManager.Paket/PaketDependencyManager.fs
@@ -8,11 +8,18 @@ module Attributes =
     [<assembly: DependencyManagerAttribute()>]
     do ()
 
+#if PREVIEW5
+// dotnet 5.0.100-preview.1.20155.7
+type ReturnType = bool * string list * string list * string list 
+#else
+// VS 16.6.0 Preview 1
+type ReturnType = bool * string seq * string seq * string seq
+#endif
 [<DependencyManager>]
 type PaketDependencyManagerProvider(outputDir: string option) =
   member x.Name = "paket"
   member x.Key = "paket"
-  member x.ResolveDependencies(scriptDir: string, mainScriptName: string, scriptName: string, packageManagerTextLines: string seq, targetFramework: string) : bool * string list * string list * string list =
+  member x.ResolveDependencies(scriptDir: string, mainScriptName: string, scriptName: string, packageManagerTextLines: string seq, targetFramework: string) : ReturnType =
     try
       let loadScript, additionalIncludeDirs = 
         ReferenceLoading.PaketHandler.ResolveDependencies(
@@ -20,8 +27,8 @@ type PaketDependencyManagerProvider(outputDir: string option) =
                  scriptDir,
                  scriptName,
                  packageManagerTextLines)
-      true, [], [loadScript], additionalIncludeDirs
+      true, [] :> _ , [loadScript] :> _ , additionalIncludeDirs:> _ 
     with
       e -> 
         printfn "exception while resolving dependencies: %s" (string e)
-        false, [], [], []
+        false, [] :> _ , [] :> _ , [] :> _ 
