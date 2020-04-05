7aff54c4fda7474f86f374933250549979a8838a
NicoVIII <nico@nicoviii.net>
2020-04-05 13:24:05 +0200

    Add support for "license-expression" in paket.template
    NuGet deprecated LicenseUrl a while ago but Paket does not support
    any of the alternatives (file and expression).

--- src/Paket.Core/Packaging/NupkgWriter.fs
+++ src/Paket.Core/Packaging/NupkgWriter.fs
@@ -161,6 +161,13 @@ module internal NupkgWriter =
         (!!?) "title" optional.Title
         !! "authors" (core.Authors |> String.concat ", ")
         if optional.Owners <> [] then !! "owners" (String.Join(", ",optional.Owners))
+        match optional.LicenseExpression with
+        | Some licenseExpression ->
+            let el = XElement(ns + "license")
+            el.SetAttributeValue(XName.Get "type", "expression")
+            el.SetValue(licenseExpression)
+            metadataNode.Add el
+        | _ -> ()
         (!!?) "licenseUrl" optional.LicenseUrl
         match optional.RepositoryType, optional.RepositoryUrl with
         | Some t, Some url ->
