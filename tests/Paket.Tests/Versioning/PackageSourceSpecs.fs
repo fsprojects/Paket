8b133005fd0e8d72e35da142fb54ed7991f7861a
TeBeCo <tebeco@gmail.com>
2020-05-17 16:00:34 +0200

    Adding test to check that even enforcing protocolVersion on known source respect the explicit the specified protocolVersion (this will probably raise error during install/restore but it will be "logic")

--- tests/Paket.Tests/Versioning/PackageSourceSpecs.fs
+++ tests/Paket.Tests/Versioning/PackageSourceSpecs.fs
@@ -46,3 +46,46 @@ let ``should parse known nuget3 source``(feed : string) =
     | NuGet { Url = source; Authentication = _; ProtocolVersion = ProtocolVersion2 } ->
         failwithf "%s should be parsed as a v3 protocol" feed
     | _ -> failwith feed
+
+[<TestCase("https://nuget.org/api/v2", 3)>]
+[<TestCase("https://nuget.org/api/v2/", 3)>]
+[<TestCase("https://www.myget.org/F/roslyn-tools/", 3)>]
+[<TestCase("http://my.domain/artifactory/api/nuget/nugetsource/", 3)>]
+[<TestCase("http://my.domain/artifactory/api/nuget/nuget-local/", 3)>]
+[<TestCase("http://my.domain/artifactory/api/nuget/nuget_proxy/", 3)>]
+let ``should parse protocol v3 even on wellknown nuget v2 source when protocolVersion is explicit`` (feed: string, protocolVersion: int) =
+    let line = sprintf "source %s protocolVersion: %d" feed protocolVersion
+
+    match PackageSource.Parse(line) with
+    | NuGet { Url = source; Authentication = _; ProtocolVersion = ProtocolVersion3 } ->
+        let quoted = sprintf "source  \"%s\" protocolVersion: %d" feed protocolVersion
+        match PackageSource.Parse(quoted) with
+        | NuGet { Url = qsource; Authentication = _; ProtocolVersion = ProtocolVersion3 } ->
+            source |> shouldEqual qsource
+        | NuGet { Url = _; Authentication = _; ProtocolVersion = ProtocolVersion2 } ->
+            failwithf "%s should be parsed as a v3 protocol when quoted and when protocolVersion explicitly specify v" feed
+        | _ -> failwith quoted
+    | NuGet { Url = _; Authentication = _; ProtocolVersion = ProtocolVersion2 } ->
+        failwithf "%s should be parsed as a v3 protocol when protocolVersion explicitly specify v3" feed
+    | e -> failwithf "%s %A" feed e
+
+[<TestCase("https://api.nuget.org/v3/index.json", 2)>]
+[<TestCase("https://dotnet.myget.org/F/roslyn-tools/api/v3/index.json", 2)>]
+[<TestCase("http://my.domain/artifactory/api/nuget/v3/nugetsource/index.json", 2)>]
+[<TestCase("http://my.domain/artifactory/api/nuget/v3/nuget-local/index.json", 2)>]
+[<TestCase("http://my.domain/artifactory/api/nuget/v3/nuget_proxy/index.json", 2)>]
+let ``should parse protocol v2 even on wellknown nuget v3 source when protocolVersion is explicit`` (feed: string, protocolVersion: int) =
+    let line = sprintf "source %s protocolVersion: %d" feed protocolVersion
+
+    match PackageSource.Parse(line) with
+    | NuGet { Url = source; Authentication = _; ProtocolVersion = ProtocolVersion2 } ->
+        let quoted = sprintf "source  \"%s\" protocolVersion: %d" feed protocolVersion
+        match PackageSource.Parse(quoted) with
+        | NuGet { Url = qsource; Authentication = _; ProtocolVersion = ProtocolVersion2 } ->
+            source |> shouldEqual qsource
+        | NuGet { Url = _; Authentication = _; ProtocolVersion = ProtocolVersion3 } ->
+            failwithf "%s should be parsed as a v2 protocol when quoted and when protocolVersion explicitly specify v2" feed
+        | _ -> failwith quoted
+    | NuGet { Url = _; Authentication = _; ProtocolVersion = ProtocolVersion3 } ->
+        failwithf "%s should be parsed as a v2 protocol when protocolVersion explicitly specify v2" feed
+    | e -> failwithf "%s %A" feed e
