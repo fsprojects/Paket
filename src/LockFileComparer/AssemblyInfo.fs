1c822531bfbe9e4c3c511f1eac879534a908520a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2020-07-23 20:13:45 +0200

    Bump version to 6.0.0-alpha037

--- src/LockFileComparer/AssemblyInfo.fs
+++ src/LockFileComparer/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Paket")>]
 [<assembly: AssemblyCompanyAttribute("Paket team")>]
 [<assembly: AssemblyDescriptionAttribute("A dependency manager for .NET with support for NuGet packages and git repositories.")>]
-[<assembly: AssemblyVersionAttribute("5.247.4")>]
-[<assembly: AssemblyFileVersionAttribute("5.247.4")>]
-[<assembly: AssemblyInformationalVersionAttribute("5.247.4")>]
+[<assembly: AssemblyVersionAttribute("6.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("6.0.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("6.0.0-alpha037")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -16,6 +16,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Paket"
     let [<Literal>] AssemblyCompany = "Paket team"
     let [<Literal>] AssemblyDescription = "A dependency manager for .NET with support for NuGet packages and git repositories."
-    let [<Literal>] AssemblyVersion = "5.247.4"
-    let [<Literal>] AssemblyFileVersion = "5.247.4"
-    let [<Literal>] AssemblyInformationalVersion = "5.247.4"
+    let [<Literal>] AssemblyVersion = "6.0.0"
+    let [<Literal>] AssemblyFileVersion = "6.0.0"
+    let [<Literal>] AssemblyInformationalVersion = "6.0.0-alpha037"
