62d765fa76ba416539a504a9dd6044f275e7a40d
Vassili Geronimos <vassili.geronimos@deswik.com>
2024-03-07 14:58:02 +1000

    Add integration test suite for group conditions

---
+++ integrationtests/scenarios/i002394-group-conditions/before/Packages/PaketTest2394.PackageA/PackageDescription.cs
@@ -0,0 +1,10 @@
+﻿namespace PaketTest2394.PackageA;
+
+public static class PackageDescription
+{
+    public static string GetDescription()
+    {
+        var assemblyName = typeof(PackageDescription).Assembly.GetName();
+        return $"PackageA {assemblyName.Version!.ToString(2)}";
+    }
+}
